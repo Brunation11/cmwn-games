a8be21746133ad02f8d47b4a834ea17ffb69562b
Gustavo Romero <gustavoromero@Gustavos-MacBook-Pro.local>
2015-08-27 13:44:25 -0400

    first try at added seaturtle to new platform

--- seaturtle/javascript/action_items/ai1.js
+++ seaturtle/javascript/action_items/ai1.js
@@ -1,42 +1,10 @@
-var bgMusic = $("#background")[0],
-playing = true;
+ $(document).ready(function () {
+        title.load();
+        title.play();
 
-bgMusic.addEventListener('ended', function () {
-    this.currentTime = 0;
-    if (playing) {
-        this.play();
-    }
-}, false);
 
-background.play();
-
-head.play();
-
-$(document).ready(function () {
-
-    $("#close").hide();
-
-    $("#next-button2").hide();
-
-    $('#next').click(function () {
-        click.load();
-        click.play();
-        $("#title").fadeOut(1000);
-        $("#close").fadeIn(1000);
-        $("#video-modal").modal('show');
-        $("#next-button2").fadeIn(10000);
-    });
-
-    $('#close').click(function () {
-        $('#game-close').modal('show');
-    });
-
-    $('.No-Btns').click(function () {
-        $('#game-close').modal('hide');
-    });
-
-   $('#next-button2').click(function () {
-        $("#putcontenthere").load("/action_items/ai2.html");
-    });
-
-});
+        $('#btn').click(function () {
+            $("#putcontenthere").load("/action_items/ai2.html");
+            click.play();
+        });
+    })
