06178c9b052adc5abdf0f4e08ae4c75165b6a504
Arron Kallenberg <arronkallenberg@Arrons-MacBook-Pro.local>
2015-08-28 17:02:05 -0400

    canit template rework

--- canit/javascript/modals.js
+++ canit/javascript/modals.js
@@ -1,26 +1,56 @@
-$(document).ready(function () {
+    $(document).ready(function () {
+        
 
-$('#pop-up').modal('show');
+    
+    $('#pop-up').modal('show');
 
-//to use the close modal, use this function
 
-$('.close-Btn').click(function () {
-    $('#game-info').modal('show');
+
+    //to use the close modal, use this function
+
+    $('.close-Btn').click(function () {
+        $('#game-info').modal('show');
+    });
+
+    /* Exp: <div id="close">
+    <div class="col-sm-4 content-box">
+    <img src="~/Content/images/btn_close.png" class="img-responsive" alt="Responsive image">
+    </div>*/
+
+    $('.A').click(function () {
+
+        $("#level-complete").modal('show');
+
+
+
+    });
+
+    $('.B').click(function () {
+
+        $("#retry-level").modal('show');
+
+
+
+    });
+
+    $('.purple-button').click(function () {
+        $('#retry-level').modal('hide')
+    });
+
+    $('.close-Btn').click(function () {
+
+        $("#retry-level").modal('show');
+
+
+
+    });
+
+    $('.close-Btn').click(function () {
+
+        $("#game-info").modal('hide');
+
+
+
+    });
+
 });
-
-$('.close-Btn').click(function () {
-
-    $("#retry-level").modal('show');
-
-});
-
-$('.close-Btn').click(function () {
-
-    $("#game-info").modal('hide');
-
-});
-
-
-
-});
-
