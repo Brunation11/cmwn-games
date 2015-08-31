103912a442394aa369a392d8df26457f35eb8cc6
Gustavo Romero <gustavoromero@Gustavos-MacBook-Pro.local>
2015-08-31 14:51:35 -0400

    first attempt at pushing the enitre AI

---
+++ sendAsmile/javascript/action_items/ai6.js
@@ -0,0 +1,16 @@
+ $(document).ready(function () {
+        $('#slide_2 img').hide();
+        $('#slide_2 h2').hide();
+        $('.nextBtn').click(function () {
+            $('#slide_1 img').fadeOut(2000);
+            $('#slide_2 img').fadeIn(2000);
+            $('#slide_2 h2').fadeIn(2000);
+
+        });
+        $('.nextBtn1').click(function () {
+            $('.nextBtn1').delay(1000).delay(1000, function () {
+                document.location.href = "@Url.Content("~/ActionItem/AI1")";
+            });
+
+        });
+    });
---
+++ sendAsmile/zSendASmile/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SendASmile.Startup))]
+namespace SendASmile
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
