0f0d446d1727212b7639fe6f4ebc9b4fc025bf88
Victor Atencio <victor@ginasink.com>
2015-08-31 16:20:19 -0400

    added ai4.

---
+++ FruitVegVeg/javascript/action_items/ai4.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $('.vegetables').hide().delay(500).fadeIn(2000);
+
+    $('.Next').hide().delay(2000).fadeIn(2000);
+
+    $(".body-copy").hide();
+    setTimeout(function () {
+        $('.body-copy').hide().delay(1000).fadeIn(600);
+    }, 1000);
+});
+
+
+
