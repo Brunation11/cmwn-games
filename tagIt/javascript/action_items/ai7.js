69386c3592b4e46105eb46d8d040d313c593f3db
Gustavo Romero <gustavoromero@Gustavos-MacBook-Pro.local>
2015-09-01 12:39:23 -0400

    first try at pushing for finnished product

--- tagIt/javascript/action_items/ai7.js
+++ tagIt/javascript/action_items/ai7.js
@@ -1,44 +1,48 @@
-        $(document).ready(function () {
-        steps.load();
-        steps.play();
+$(document).ready(function () {           
         $('#headline2 img').hide();
+        $('#headline2 h1').hide();
         $('.forward2').hide();
         $('.back2').hide();
-        $('#headline1 img').hide();
+        $('.hint img').hide();
         $('.forward').hide();
         $('.forward1').click(function () {
-            $('#headline1 img').fadeIn(3000);
+            $('.hint img').fadeIn(3000);
             $('.forward').show();
             $('.hint').addClass('fadIn')
             hinty.play();
         });
         $('.forward').click(function () {
-            $('#headline1 img').fadeOut(3000);
+            $('#headline1 img').fadeOut(1000);
+            $('#headline1 ul').fadeOut(1000);
+            $('.here').addClass('bounce');
             $('#headline2 img').fadeIn(2000);
+            $('#headline2 h1').fadeIn(2000);
             $('.forward').hide();
             $('.forward2').show();
             $('.back').hide();
             $('.back2').show();
-            hinty.play();
+            me5.play();
         });
         $('.forward2').click(function () {
             $('.forward2').delay(1000).delay(1000, function () {
-                $("#putcontenthere").load("/action_items/ai7.html");
+                $("#putcontenthere").load("/action_items/ai8.html");
             });
-            click.play();
+            nex.play();
         });
         $('.back').click(function () {
             $('.back').delay(1000).delay(1000, function () {
-                $("#putcontenthere").load("/action_items/ai5.html");
+                $("#putcontenthere").load("/action_items/ai6.html");
             });
-            click.play();
+            nex.play();
         });
         $('.back2').click(function () {
-            $('#headline2 img').hide();
-            $('#headline1 img').show();
+            $('#headline1 img').fadeIn(3000);
+            $('#headline1 ul').fadeIn(3000);
+            $('#headline2 img').fadeOut(1000);
+            $('#headline2 h1').fadeOut(1000);
             $('.forward2').hide();
             $('.forward').show();
             $('.back2').hide();
             $('.back').show();
         });
-    });    
+    });            
