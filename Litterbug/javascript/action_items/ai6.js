cffc1588fba1383fdc2db453d07e80c2af92383b
Victor Atencio <victor@ginasink.com>
2015-09-02 12:41:10 -0400

    edited ai6.js

--- Litterbug/javascript/action_items/ai6.js
+++ Litterbug/javascript/action_items/ai6.js
@@ -2,9 +2,9 @@ $(document).ready(function () {
 
     $('.bkg-image').css('background-image','url(/content/images/background/BKG_1.png)');
 
-    $(".No").hide().delay(2000).fadeIn(100);
+    $(".No-3").hide().delay(1000).fadeIn(100);
 
-    $('.No').click(function(){
+    $('.No-3').click(function(){
        $("#putcontenthere").load("action_items/ai7.html");
     });
 });
