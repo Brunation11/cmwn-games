9fc16381a641c93e76d5f21b4f5cf8223056d038
Victor Atencio <victor@ginasink.com>
2015-09-03 09:30:03 -0400

    updated ai8.html, ai8.js, ai7.js and ai8.less file.

--- HomeSweetHabitat/javascript/action_items/ai7.js
+++ HomeSweetHabitat/javascript/action_items/ai7.js
@@ -9,4 +9,12 @@ $(document).ready(function () {
         $('#quit-game').modal('hide');
 
     });
+
+    $('.replay').click(function(){
+        $("#putcontenthere").load("action_items/ai6.html");
+    });
+
+    $('.Next-5').click(function(){
+        $("#putcontenthere").load("action_items/ai8.html");
+    });
 });
