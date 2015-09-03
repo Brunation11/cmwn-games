f758168c5224e5ae3fc979e3df18d0ff1d0e97db
Gustavo Romero <gustavoromero@Gustavos-MacBook-Pro.local>
2015-09-03 18:02:45 -0400

    issues with css

--- BikeBits/javascript/action_items/ai2.js
+++ BikeBits/javascript/action_items/ai2.js
@@ -1,7 +1,8 @@
 
       $(document).ready(function () {
         $('#game-complete').modal('hide');
-$('.next').click(function () {
+		
+		$('.next').click(function () {
             $('.next').delay(1000).delay(1000, function () {
                 $("#putcontenthere").load("/action_items/ai7.html");
             });
