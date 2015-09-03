8cf6311a006fe9a3bcc6123d0d2714c632857afb
Victor Atencio <victor@ginasink.com>
2015-09-03 10:44:23 -0400

    Water cycle game

---
+++ WaterCycle/javascript/modals.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+
+    $('.A').click(function () {
+
+        $("#level-complete").modal('show');
+    });
+
+    $('.B').click(function () {
+
+        $("#retry-level").modal('show');
+    });
+
+    $('.No').click(function () {
+        $('#retry-level').modal('hide')
+    });
+
+    $('.close-Btn').click(function () {
+
+        $("#retry-level").modal('show');
+
+    });
+});
