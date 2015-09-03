d53727b551a0dcdf9d3bbc2f9cbb82d4ae635025
Gustavo Romero <gustavoromero@Gustavos-MacBook-Pro.local>
2015-09-03 17:14:08 -0400

    first qttept at pushing in bike bits

---
+++ BikeBits/javascript/action_items/ai2.js
@@ -0,0 +1,9 @@
+
+      $(document).ready(function () {
+        $('#game-complete').modal('hide');
+$('.next').click(function () {
+            $('.next').delay(1000).delay(1000, function () {
+                $("#putcontenthere").load("/action_items/ai7.html");
+            });
+
+    });
