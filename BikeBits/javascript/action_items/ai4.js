7d2c1e72368e579ecd1dc6bcf2c2d1b1f3b6efcb
Gustavo Romero <gustavoromero@Gustavos-MacBook-Pro.local>
2015-09-04 17:05:33 -0400

    Bike Bits is good to go !!!!!

--- BikeBits/javascript/action_items/ai4.js
+++ BikeBits/javascript/action_items/ai4.js
@@ -1,7 +1,18 @@
 var complete = document.getElementById("complete3");
 
 $(document).ready(function () {
-    //complete.play();
+    complete3.play();
+
+    $('#game-complete').modal('hide');
+
+$('#close4').click(function () {
+		$('#game-complete4').modal('show');
+	});
+
+$("#no4").click(function () {
+                $('#game-complete4').modal('hide');
+            });
+
 
     $('.next4').click(function () {
         $('.next4').delay(1000).delay(1000, function () {
