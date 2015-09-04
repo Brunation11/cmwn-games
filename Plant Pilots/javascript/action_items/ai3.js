193dca822cea84a8f327af10859e5f3ca244e87b
Victor Atencio <victor@ginasink.com>
2015-09-04 16:40:57 -0400

    firxed timer.

--- Plant Pilots/javascript/action_items/ai3.js
+++ Plant Pilots/javascript/action_items/ai3.js
@@ -15,23 +15,21 @@ var incrementTime = 1000;
 var currentTime;
 
 
-$('#button-replay').click(function () {
-    $("#putcontenthere").load("/action_items/ai3.html");
-});
-
-$('#button-start').click(function () {
-    $("#putcontenthere").load("/action_items/ai4.html");
-});
-
 //set the game functions up once the screen loads
 $(document).ready(function () { //  Here is when "Next" button is clicked, timer starts to count down
 
+     //initalize the backdrop for the modal windows
+    $('#game-info').modal("show");
+
+    $('#button-start').click(function () {
+        $('#game-info').modal("hide");
         //start the game functions
         startGame();
+    });
+
 
         //initalize the drag and drop features for the game items
         init();
-    
 
     //Provide Hint when Clicked
     $('.hint').click(function () {
@@ -58,98 +56,8 @@ $(document).ready(function () { //  Here is when "Next" button is clicked, timer
 
     });
 
-})
-
-//initalize the backdrop for the modal windows
-$('#game-info').modal({
-    backdrop: true
 });
 
-//start all of the game functions
-function startGame() {
-    startTimer();
-    start.loop = true;
-    start.play();
-
-}
-
-//create a new time object
-var timer1 = new (function () {
-    currentTime = '30000'; // 20 seconds (in milliseconds)
-});
-
-//start the timer and assign the counter location
-function startTimer() {
-    $countdown = $('#counter');
-    $countdown.show();
-
-    timer1.Timer = $.timer(updateTimer, incrementTime, true);
-
-}
-
-//update the display of the counter in the proper interval
-function updateTimer() {
-
-    // Output timer position
-    var timeString = currentTime.toString();
-    $countdown.html(displayTime(timeString));
-
-    // If timer is complete, trigger correct action
-    if (currentTime == 0) {
-        timer1.Timer.stop();
-        //alert('Times up!');
-        start.pause();
-        start.currentTime = 0;
-
-        runout.play();
-
-        //end game modal with fail message
-        setTimeout(function () {
-
-            $('#game-info').modal('show');
-            $('.text-modal-start').hide();
-            $('.main-title').hide();
-            $('.text-modal').append("<h3>Try again!</h3>");
-            $("h3").css({ "font-size": "160%", "margin-left": "40px" });
-            $(".popup-image").css({ "margin-top": "79px", "margin-left": "118px" });
-            $("#button-start").css({ "margin-left": "100px" });
-            runout.load();
-            runout.play();
-            background.load();
-            background.pause();
-            $("#button-start").click(function () {
-                $('#game-info').modal('hide');
-                setTimeout(function () { location.reload(); }, 10);
-                startGame.load();
-                startGame();
-
-            });
-
-        });
-    }
-
-    // Increment timer position
-    currentTime -= incrementTime;
-    if (currentTime < 0) currentTime = 0;
-}
-
-//format the time so that is display correctly in the counter display
-function displayTime(timeString) {
-    var seconds = ~~((timeString / 1000) % 60);
-    var minutes = ~~((timeString / (1000 * 60)) % 60);
-    var hours = ~~((timeString / (1000 * 60 * 60)) % 24);
-
-    //alert('hours: ' + hours + ' minutes: ' + minutes + ' seconds: ' + seconds);
-
-    if (seconds < 10) seconds = "0" + seconds;
-    if (minutes < 10) minutes = "0" + minutes;
-    if (hours < 10) hours = "0" + hours;
-
-    //if (hours = 0) hours = "00";
-    //if (minutes = 0) minutes = "00";
-
-    return minutes.toString() + seconds.toString();
-}
 
 //finds the hint by randomizing the potential items available
 function findHint() {
@@ -253,16 +161,18 @@ function init() {
             background.pause();
             //show the popup screen for game complete and send to bloom boom game
             $('#game-success').modal('show');
-            $('#game-info').modal('hide');
+            //$('#game-info').modal('hide');
             $('.main-title').hide();
             $('.text-modal-start').hide();// HIDE Remember your hints from Level one.  Drag your food choices and feed each pollinator!  You can only feed each pollinator once
+
             $("#button-next").click(function () {
                 $('#game-complete').modal('hide');
                 //document.location.href = "/ActionItem/ImageQuiz";
             });
 
             $('#button-replay').click(function () {
-                $('#game-complete').modal('hide');
+                //$('#game-complete').modal('hide');
+                $('#game-info').modal('hide');
 
                 $("#putcontenthere").load("/action_items/ai3.html");
             })
---
+++ Plant Pilots/javascript/gametimer.js
@@ -0,0 +1,85 @@
+//start all of the game functions
+function startGame() {
+    startTimer();
+    start.loop = true;
+    start.play();
+
+}
+
+//create a new time object
+var timer1 = new (function () {
+    currentTime = '30000'; // 20 seconds (in milliseconds)
+});
+
+//start the timer and assign the counter location
+function startTimer() {
+    $countdown = $('#counter');
+    $countdown.show();
+
+    timer1.Timer = $.timer(updateTimer, incrementTime, true);
+
+}
+
+//update the display of the counter in the proper interval
+function updateTimer() {
+
+    // Output timer position
+    var timeString = currentTime.toString();
+    $countdown.html(displayTime(timeString));
+
+    // If timer is complete, trigger correct action
+    if (currentTime == 0) {
+        timer1.Timer.stop();
+        //alert('Times up!');
+        start.pause();
+        start.currentTime = 0;
+
+        runout.play();
+
+        //end game modal with fail message
+        setTimeout(function () {
+
+            $('#game-info').modal('show');
+            $('.text-modal-start').hide();
+            $('.main-title').hide();
+            $('.text-modal').append("<h3>Try again!</h3>");
+            $("h3").css({ "font-size": "160%", "margin-left": "40px" });
+            $(".popup-image").css({ "margin-top": "79px", "margin-left": "118px" });
+            $("#button-start").css({ "margin-left": "100px" });
+            runout.load();
+            runout.play();
+            background.load();
+            background.pause();
+            $("#button-start").click(function () {
+                $('#game-info').modal('hide');
+                setTimeout(function () { location.reload(); }, 10);
+                startGame.load();
+                startGame();
+
+            });
+
+        });
+    }
+
+    // Increment timer position
+    currentTime -= incrementTime;
+    if (currentTime < 0) currentTime = 0;
+}
+
+//format the time so that is display correctly in the counter display
+function displayTime(timeString) {
+    var seconds = ~~((timeString / 1000) % 60);
+    var minutes = ~~((timeString / (1000 * 60)) % 60);
+    var hours = ~~((timeString / (1000 * 60 * 60)) % 24);
+
+    //alert('hours: ' + hours + ' minutes: ' + minutes + ' seconds: ' + seconds);
+
+    if (seconds < 10) seconds = "0" + seconds;
+    if (minutes < 10) minutes = "0" + minutes;
+    if (hours < 10) hours = "0" + hours;
+
+    //if (hours = 0) hours = "00";
+    //if (minutes = 0) minutes = "00";
+
+    return minutes.toString() + seconds.toString();
+}
