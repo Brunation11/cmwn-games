fb5ae625864c264f8b381a07eb4e41b4f784cc42
LuisQ7 <luis@ginasink.com>
2015-09-08 16:23:30 -0400

    honeyBee updates

--- honeybee/javascript/action_items/ai5.js
+++ honeybee/javascript/action_items/ai5.js
@@ -18,10 +18,10 @@ $(document).ready(function () {
         background.play();
     });
 
-    $('#next-button2').click(function () {
-        $('#video-modal').modal('hide');
-        $('#game-info2').modal('show');
-    });
+    // $('#next-button2').click(function () {
+    //     $('#video-modal').modal('hide');
+    //     $('#game-info2').modal('show');
+    // });
 
     $('#close').click(function () {
         $('#game-close').modal('show');
@@ -31,34 +31,38 @@ $(document).ready(function () {
         $('#game-close').modal('hide');
     });
 
-    $('#next-button3').click(function () {
+    // $('#next-button3').click(function () {
+    //     $("#putcontenthere").load("/action_items/ai6.html");
+    // });
+
+    $('#next-button2').click(function () {
         $("#putcontenthere").load("/action_items/ai6.html");
     });
 
 });
 
-function readURL(input) {
-    if (input.files && input.files[0]) {
+// function readURL(input) {
+//     if (input.files && input.files[0]) {
 
-        var reader = new FileReader();
+//         var reader = new FileReader();
 
-        reader.onload = function (e) {
-            $('.file-upload-image').attr('src', e.target.result);
-            $('.image-upload-wrap').show();
-            $('.image-title').html(input.files[0].name);
-        };
+//         reader.onload = function (e) {
+//             $('.file-upload-image').attr('src', e.target.result);
+//             $('.image-upload-wrap').show();
+//             $('.image-title').html(input.files[0].name);
+//         };
 
-        //create an AJAX call that writes the file to the file system then stored the file location in session to be read later
+//         //create an AJAX call that writes the file to the file system then stored the file location in session to be read later
 
 
-        reader.readAsDataURL(input.files[0]);
+//         reader.readAsDataURL(input.files[0]);
 
-    } else {
-        $('.image-upload-wrap').hide();
-    }
-}
+//     } else {
+//         $('.image-upload-wrap').hide();
+//     }
+// }
 
-function removeUpload() {
-    $('.file-upload-input').replaceWith($('.file-upload-input').clone());
-    $('.image-upload-wrap').hide();
-}
+// function removeUpload() {
+//     $('.file-upload-input').replaceWith($('.file-upload-input').clone());
+//     $('.image-upload-wrap').hide();
+// }
