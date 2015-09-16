985fb5b493f42b13c783c5b144d026807485bffe
Gustavo Romero <gustavoromero@Gustavos-MacBook-Pro.local>
2015-09-16 15:08:57 -0400

    second try at fixing background images

--- litter-bug/javascript/action_items/ai10.js
+++ litter-bug/javascript/action_items/ai10.js
@@ -1,6 +1,6 @@
 // $(document).ready(function () {
 
-//     $('.bkg-image').css('background-image','url(/content/images/background/BKG_1.png)');
+//     $('.bkg-image').css('background-image','url(/content/images/background/bkg_1.png)');
 
 //     $(".Upload-again").hide().delay(900).fadeIn(100);
 //     $(".post").hide().delay(1000).fadeIn(100);
