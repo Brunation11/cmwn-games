7adcfbd1e0b3f1fde4a24c03bbb9b2fe088bf823
LuisQ7 <luis@ginasink.com>
2015-09-17 12:32:48 -0400

    MeerkatMania updates

--- meerkat-mania/javascript/action_items/ai4.js
+++ meerkat-mania/javascript/action_items/ai4.js
@@ -1,32 +1,32 @@
 $(document).ready(function () {
-        flip.load();
-        flip.play();
+    flip.load();
+    flip.play();
 
-        $('#frame').hide();
-        $('#video').hide();
+    $('#frame').hide();
+    $('#video').hide();
+    $('#btn').hide();
+    $('#btn1').hide();
+    $('#btn').fadeIn(3000);
+    $('#btn').click(function () {
+        $('#content').hide();
         $('#btn').hide();
-        $('#btn1').hide();
-        $('#btn').fadeIn(3000);
-        $('#btn').click(function () {
-            $('#content').hide();
-            $('#btn').hide();
-            $('#frame').show();
-            $('#video').show();
-            btn_click.play();
-            $('#btn1').fadeIn(74000);
-           
-        });
-        $('#btn1').click(function () {
-            
-                $("#putcontenthere").load("action_items/ai5.html");
-            
-            btn_click.play();
-        });
-        $("#Btn-hide1").click(function () {
-            $('#retry-level').modal('hide');
+        $('#frame').show();
+        $('#video').show();
+        btn_click.play();
+        $('#btn1').fadeIn(74000);
+       
+    });
+    $('#btn1').click(function () {
+        
+            $("#putcontenthere").load("action_items/ai5.html");
+        
+        btn_click.play();
+    });
+    $("#Btn-hide1").click(function () {
+        $('#retry-level').modal('hide');
 
-        });
-        $('.close-Btn').click(function () {
-            $('#retry-level').modal('show');
-        });
-    })
+    });
+    $('.close-Btn').click(function () {
+        $('#retry-level').modal('show');
+    });
+})
