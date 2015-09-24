ff0ca3b6a896bcaa02419d18c70bcb8945c4016e
Victor Atencio <victor@ginasink.com>
2015-09-24 17:40:51 -0400

    fixed gitch

--- done/litter-bug/javascript/action_items/ai5.js
+++ done/litter-bug/javascript/action_items/ai5.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
     school.load();
     school.play();
+
     $('.bkg-image').css('background-image','url(content/images/background/bakg_1.png)');
 
     $(".no-2").hide().delay(1000).fadeIn(100);
