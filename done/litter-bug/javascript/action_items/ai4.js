bb60d5a0bf39090a0977c32727f50a63f9fdd156
Victor Atencio <victor@ginasink.com>
2015-09-24 11:15:05 -0400

    added files

--- done/litter-bug/javascript/action_items/ai4.js
+++ done/litter-bug/javascript/action_items/ai4.js
@@ -1,12 +1,14 @@
 $(document).ready(function () {
+    trashroom.load();
+    trashroom.play();
 
     $('.bkg-image').css('background-image','url(content/images/background/bakg_1.png)');
 
     $(".no-1").hide().delay(1000).fadeIn(100);
 
     $('.no-1').click(function(){
-        click.load();
-        click.play();
+        no.load();
+        no.play();
         setTimeout(function(){
             $("#putcontenthere").load("action_items/ai5.html");
         },1000);
--- done/litter-bug/javascript/action_items/ai6.js
+++ done/litter-bug/javascript/action_items/ai6.js
@@ -1,12 +1,13 @@
 $(document).ready(function () {
-
+    ground.load();
+    ground.play();
     $('.bkg-image').css('background-image','url(content/images/background/bakg_1.png)');
 
     $(".no-3").hide().delay(1000).fadeIn(100);
 
     $('.no-3').click(function(){
-        click.load();
-        click.play();
+        no.load();
+        no.play();
         setTimeout(function(){
             $("#putcontenthere").load("action_items/ai7.html");
         },1000);
