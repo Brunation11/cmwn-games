54b288b3c92b3e9c51b2f3860f7584c762585452
Gustavo Romero <gustavoromero@Gustavos-MacBook-Pro.local>
2015-09-28 10:19:17 -0400

    finished witht the edits on Cloud in a jar

--- done/cloud-in-a-jar/javascript/action_items/ai4.js
+++ done/cloud-in-a-jar/javascript/action_items/ai4.js
@@ -1,8 +1,11 @@
 $(document).ready(function () {
-        $('.s1').hide().fadeIn(2000);
-        $('.s2').hide().fadeIn(4000);
-        $('.s3').hide().fadeIn(6000);
-        $('.s4').hide().fadeIn(8000);
+        $('.s1').fadeIn(2000);
+        $('.s2').fadeIn(4000);
+        $('.s3').fadeIn(6000);
+        $('.s4').fadeIn(8000);
+        $('#btn').fadeIn(9000);
+
+
         $('#btn').click(function () {
             $('#btn').delay(1000).delay(1000, function () {
                 $("#putcontenthere").load("action_items/ai5.html");
