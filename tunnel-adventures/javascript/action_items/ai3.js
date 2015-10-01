3eff0b122546b92f23013040aa86116fd430c4eb
System Administrator <root@Gustavos-MacBook-Pro.local>
2015-10-01 15:40:59 -0400

    renamed tunnel adventures and took the old one out

--- tunnel-adventures/javascript/action_items/ai3.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-    //change the background class
-    $("#main-background").removeClass('intro').addClass('page');
-    
-    //remove the intro title
-    $("#main-title").removeClass('intro-title');
-})
