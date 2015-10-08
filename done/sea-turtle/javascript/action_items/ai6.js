3e3633f94a671fd27bd956593c8794edce136782
Luis Quispe <Luis@GIIs-MacBook-Pro.local>
2015-10-08 16:22:52 -0400

    seaTurtle updates

--- done/sea-turtle/javascript/action_items/ai6.js
+++ done/sea-turtle/javascript/action_items/ai6.js
@@ -3,7 +3,6 @@
     $('#btn').click(function () {
         $('#btn').delay(2000).delay(1000, function () {
            $("#putcontenthere").load("action_items/ai7.html");
-           completeFlip(24318);
         });
         click.play();
     });
