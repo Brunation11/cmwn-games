8a2f62f6653d8aa930fb5996fdbc3ca94529493b
Victor Atencio <victor@ginasink.com>
2015-10-08 16:16:11 -0400

    moved gamed id

--- done/tag-it/javascript/action_items/ai11.js
+++ done/tag-it/javascript/action_items/ai11.js
@@ -59,7 +59,6 @@ $(document).ready(function () {
     $('.no').click(function () {
         $('.no').delay(1000).delay(1000, function () {
             $("#putcontenthere").load("action_items/ai12.html");
-            completeFlip(12935);
         });
         nex.play();
     });
--- done/tag-it/javascript/action_items/ai12.js
+++ done/tag-it/javascript/action_items/ai12.js
@@ -1,14 +1,13 @@
 $(document).ready(function () {
+
+  completeFlip(12935);
+
     try {
            flipper.play();
         }
         catch (err) {
            //no sound - log error
         }
-
-
-
-
 
     $('.forward').click(function () {
         $('.forward').delay(1000).delay(1000, function () {
