3c336aa3b0d84773e8d56ea4c5ced135438d3921
Gustavo Romero <gustavoromero@Gustavos-MacBook-Pro.local>
2015-10-23 18:07:15 -0400

    working on waste pro

---
+++ done/new-waste-pro/javascript/main.js
@@ -0,0 +1,78 @@
+
+var game = {
+    currentSlide: 1,
+    loadScreen : function(slide) {
+        this.currentSlide = slide;
+
+         $('.replace').hide();
+
+        var divId = "#div" + slide;
+
+        $(divId).show();
+        console.log("current slide: " + this.currentSlide);
+
+        this.loadScreenCallback(this.currentSlide);
+    },
+
+    loadNextScreen: function() {
+        this.currentSlide++;
+
+        this.loadScreen(this.currentSlide);
+
+    },
+
+    loadPrevScreen: function() {
+        this.currentSlide--;
+
+        this.loadScreen(this.currentSlide);
+    }
+
+};
+
+game.loadScreenCallback = function (currentSlide) {
+    if (currentSlide == 3) {  
+            $('.next').show();    
+    }
+    if (currentSlide == 2) {
+            $('.arrows').fadeIn(500);
+            $('.next').hide();
+            $('.card').removeClass('flipped');
+    }
+    if (currentSlide == 1) {
+            $('#play, .next').fadeIn(500);  
+            $('.arrows').fadeOut(100);    
+    }
+
+}
+
+
+
+game.loadScreen(1);
+
+$(document).ready(function () {
+$('.next').on('click', function(){
+    game.loadNextScreen();
+});
+
+$('.prev').on('click', function(){
+    game.loadPrevScreen();
+});
+
+$("#start-over").on('click', function() {
+    game.loadScreen(1);
+});
+
+
+$('.close-Btn').click(function () {
+    $('#retry-level').modal('show');
+});
+$('#Btn-hide1').click(function () {
+    $('#retry-level').modal('hide')
+});
+
+$('.card').click(function () {
+        $(this).addClass('flipped');
+        setTimeout(function () { $('.next').fadeIn(500); }, 1000);    
+    });
+      
+});
