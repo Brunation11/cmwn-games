57bd1a9f3050264dad000d90fd9800f52bbcbebc
Micah Rolon <functionmicah@gmail.com>
2015-12-16 13:28:43 -0500

    Little better font weight handling. Enable frame component to take a title attribute.

--- library/polar-bear/source/js/components/frame/behavior.js
+++ library/polar-bear/source/js/components/frame/behavior.js
@@ -1,3 +1,14 @@
 pl.game.component('frame', function () {
-	// body...
+	
+	this.handleProperty({
+		title: function (_node, _name, _value) {
+			if (this.is(_node)) {
+				this.find('.frame').addClass('title');
+				this.game.defineRule('.experiment:nth-of-type('+(this.screen.index()+1)+') .frame-component .frame.title::before', {
+					backgroundImage: 'url('+ _value +')'
+				});
+			}
+		}
+	});
+
 });
--- library/polar-bear/source/js/index.js
+++ library/polar-bear/source/js/index.js
@@ -252,4 +252,30 @@ pl.game('polar-bear', function () {
 
 	});
 
+	this.defineRule = function (_selector_scope, _selector_def, _definition) {
+		var _scope, _selector, source, prop, value;
+		// Resolve arguments.
+		_selector_scope.$els ? // (A) if we are a scope
+			(_scope = _selector_scope, // assign scope arg...
+			typeof _selector_def === 'string' ? // ...also, (B) if arg 2 is a string
+				_selector = _scope.address() + _selector_def: // assing selector arg with scope address:
+				(_selector = _scope.address(), _definition = _selector_def)): // (B) otherwise, assign selector arg to scope address, also assing definition arg
+			(_selector = _selector_scope, _definition = _selector_def); // (A) otherwise, assing selector and definition args.
+
+		source = _selector+' {';
+
+		for (prop in _definition) {
+			if (!_definition.hasOwnProperty(prop)) continue;
+			value = _definition[prop];
+			source += prop.replace(/([A-Z]+)/g, '-$1').toLowerCase()+': '+value+';'
+		}
+
+		source += '}'
+
+		$('<style type="text/css" class="dynanic-styles">'+source+'</style>')
+			.appendTo(document.body);
+
+		return source;
+	};
+
 });
