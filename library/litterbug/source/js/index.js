4885c8184b53fa91c8fa4e3c5d832c883fecca61
Adam Walzer <adamwalzer@gmail.com>
2016-01-12 18:24:49 -0500

    init commit for litterbug

---
+++ library/litterbug/source/js/index.js
@@ -0,0 +1,65 @@
+/**
+ * Index script
+ * @module
+ */
+import './testPlatformIntegration';
+import 'js-interactive-library';
+import './config.game';
+
+import './components/screen-basic/behavior';
+import './components/screen-quit/behavior';
+import './components/title/behavior';
+import './components/frame/behavior';
+import './components/score/behavior';
+import './components/reveal/behavior';
+import './components/multiple-choice/behavior';
+import './components/selectable/behavior';
+import './components/selectable-all/behavior';
+import './components/selectable-reveal/behavior';
+
+pl.game('litterbug', function () {
+
+	this.screen('title', function () {
+		
+		this.ready = function () {
+			this.open();
+			this.close($('#loader'));
+		};
+
+		this.on('ui-open', function (_event) {
+			if (this === _event.targetScope) {
+				this.title.start();
+			}
+		});
+
+		this.on('ready', function (_event) {
+			// Screens are display:none then when READY get display:block.
+			// When a screen is OPEN then it transitions a transform,
+			// the delay is to prevent the transition failing to play
+			// because of collision of these styles.
+			// 
+			if (this.is(_event.target)) this.delay(0, this.open);
+		});
+
+	});
+
+	this.screen('lets-clean-up', function () {
+		
+		this.on('ui-open', function() {
+			$('body').addClass('lets-clean-up');
+		});
+
+		this.on('ui-close', function() {
+			$('body').removeClass('lets-clean-up');
+		});
+
+	});
+
+	pl.game.component('no', function () {
+
+		console.log("here");
+
+	});
+
+
+});
