732983911008e0ff9d27a5bab1964de5a6421195
Adam Walzer <adamwalzer@gmail.com>
2016-02-04 13:43:30 -0500

    Revert "changes to prevent toilets from swinging in with screen"
    This reverts commit a590286797944a52fd9f5233dc6b620205347949.

--- library/twirl-n-swirl/source/js/screens/flush-it.js
+++ library/twirl-n-swirl/source/js/screens/flush-it.js
@@ -29,8 +29,8 @@ export default function flushIt () {
 		this.item = function (_id) {
 			this
 				.removeClass('LAYER')
-				.open();
-			this.reveal.item(_id);
+				.open()
+				.reveal.item(_id);
 		};
 		/**
 		 * Close the modal and play the standard button sound.
