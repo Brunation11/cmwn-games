be3515265490aab463d42543b6b1ac0e846811c5
Micah Rolon <functionmicah@gmail.com>
2016-02-08 17:30:19 -0500

    Remove unused components, Add missing.

--- library/polar-bear/source/js/index.js
+++ library/polar-bear/source/js/index.js
@@ -2,22 +2,22 @@
  * Index script
  * @module
  */
-import 'testPlatformIntegration';
+import './testPlatformIntegration';
 import 'js-interactive-library';
-import 'config.game';
+import './config.game';
 
-import 'components/screen-basic/behavior';
-import 'components/screen-quit/behavior';
-import 'components/title/behavior';
-import 'components/frame/behavior';
-import 'components/slides/behavior';
-import 'components/carousel/behavior';
-import 'components/score/behavior';
-import 'components/reveal/behavior';
-import 'components/multiple-choice/behavior';
-import 'components/selectable/behavior';
-import 'components/selectable-reveal/behavior';
-import 'components/cannon/behavior';
+import './components/screen-basic/behavior';
+import './components/screen-quit/behavior';
+import './components/title/behavior';
+import './components/frame/behavior';
+import './components/slides/behavior';
+import './components/carousel/behavior';
+import './components/score/behavior';
+import './components/reveal/behavior';
+import './components/multiple-choice/behavior';
+import './components/selectable/behavior';
+import './components/selectable-reveal/behavior';
+import './components/cannon/behavior';
 
 pl.game('polar-bear', function () {
 
