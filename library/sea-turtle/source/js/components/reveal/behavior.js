d15ee12b05d172d8f11b2958427a0d23e80804fc
Adam Walzer <adamwalzer@gmail.com>
2016-02-16 17:34:54 -0500

    globe update

--- library/sea-turtle/source/js/components/reveal/behavior.js
+++ library/sea-turtle/source/js/components/reveal/behavior.js
@@ -39,7 +39,7 @@ pl.game.component('reveal', function () {
 		return true;
 	};
 
-	this.deselect = function(_$target) {
+	this.deselectTarget = function(_$target) {
 		if(!this.screen.state(this.screen.STATE.VOICE_OVER) || this.game.demoMode) {
 			this.screen.deselect(_$target);
 		}
