912a6bf7f27ae8d856cfefd3cff1b421c6d48cd5
Adam Walzer <adamwalzer@gmail.com>
2016-02-26 12:26:47 -0500

    game-121 happy fish face boilerplate

---
+++ library/happy-fish-face/source/js/index.js
@@ -0,0 +1,63 @@
+/**
+ * Index script
+ * @module
+ */
+import './testPlatformIntegration';
+import 'js-interactive-library';
+// import '../../../../../js-interactive-library';
+import './config.game';
+
+// SCREENS
+
+import './components/screen-basic/behavior';
+import './components/screen-quit/behavior';
+import './components/title/behavior';
+import './components/frame/behavior';
+import './components/score/behavior';
+import './components/reveal/behavior';
+import './components/multiple-choice/behavior';
+import './components/selectable/behavior';
+import './components/selectable-reveal/behavior';
+import './components/audio-sequence/behavior';
+import './components/modal/behavior';
+
+pl.game('happy-fish-face', function () {
+
+	this.screen('title', function () {
+
+		this.on('ready', function (_event) {
+			// Screens are display:none then when READY get display:block.
+			// When a screen is OPEN then it transitions a transform,
+			// the delay is to prevent the transition failing to play
+			// because of collision of these styles.
+			// 
+			if (this.is(_event.target)) {
+				this.delay(0, function() {
+					this.open();
+					this.close($('#loader'));
+				});
+			}
+		});
+
+	});
+
+	this.screen('flip', function () {
+		this.next = function () {
+			this.game.quit.okay();
+		};
+
+		this.on('ready', function(_event) {
+			if(!this.is(_event.target)) return;
+			this.require('shake');
+		});
+
+		this.entity('.flip', function() {
+			this.on('animationend', function(_event) {
+				if(!this.is(_event.target) || !this.screen.allowAction()) return;
+				this.screen.requiredQueue.ready('shake');
+			});
+		});
+	});
+
+
+});
