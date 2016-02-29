84651f2661cca65c5461299d0da4d7e8d92a868e
Adam Walzer <adamwalzer@gmail.com>
2016-02-29 13:55:44 -0500

    interim animal id firefox drag and drop fix game-343

--- library/animal-id/source/js/components/dropzone/behavior.js
+++ library/animal-id/source/js/components/dropzone/behavior.js
@@ -13,7 +13,18 @@ pl.game.component('dropzone', function () {
 		});
 
 		this.respond('release', function (_event) {
-			if (_event.state.progress.point && this.isPointInBounds(_event.state.progress.point)) {
+			var point, scale;
+
+			if((scale = this.game.transformScale().x) !== 1) {
+				point = [
+							_event.state.start.point[0] + scale * _event.state.progress.distance[0],
+							_event.state.start.point[1] + scale * _event.state.progress.distance[1]
+						];
+			} else {
+				point = _event.state.progress.point;
+			}
+
+			if (point && this.isPointInBounds(point)) {
 				if (this.takes(_event.state.$draggable.id())) {
 					_event.state.$draggable.removeClass('PLUCKED');
 					_event.state.$helper.addClass('DROPED');
@@ -49,7 +60,6 @@ pl.game.component('dropzone', function () {
 		var point, scale;
 
 		point = pl.Point.create(arguments);
-		if((scale = this.game.transformScale().x) !== 1) point = point.scale(1/scale);
 
 		if (point.x >= this.cache.position.x && point.x <= this.cache.position.x+this.cache.size.width) {
 			if (point.y >= this.cache.position.y && point.y <= this.cache.position.y+this.cache.size.height) {
