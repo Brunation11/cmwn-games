2e514dfa1c1e5d75a2862bead1801ed572111475
Adam Walzer <adamwalzer@gmail.com>
2016-03-22 11:50:00 -0400

    game-517 fire sprites and modal close

--- library/fire/source/js/components/screen-basic/behavior.js
+++ library/fire/source/js/components/screen-basic/behavior.js
@@ -1,21 +1,8 @@
 pl.game.component('screen-basic', function () {
 
-	var characters;
-	/**
-	 * Nodes, including the node of this screen, with a
-	 * attribute of pl-bg will get a background-image style
-	 * and the resource preloaded and collected for watching.
-	 */
-	this.handleProperty({
-		bg: function (_node, _name, _value) {
-			var img = new Image();
-			characters = characters || [];
-
-			img.src = _value;
-			characters.push(img);
-			$(_node).css('background-image', 'url('+_value+')');
-		}
-	});
+	this.shouldProceed = function() {
+		return (!this.state(this.STATE.PLAYING) && !this.state(this.STATE.VOICE_OVER)) || this.game.demoMode;
+	};
 
 	this.ready = function () {
 		if (this.isMemberSafe('requiredQueue') && this.requiredQueue) {
--- library/fire/source/js/index.js
+++ library/fire/source/js/index.js
@@ -304,23 +304,6 @@ pl.game('fire', function () {
 	});
 
 	this.screen('dress', function() {
-
-		var characters = [];
-		/**
-		 * Nodes, including the node of this screen, with a
-		 * attribute of pl-bg will get a background-image style
-		 * and the resource preloaded and collected for watching.
-		 */
-		this.handleProperty({
-			bg: function (_node, _name, _value) {
-				var img = new Image();
-
-				img.src = _value;
-				characters.push(img);
-				$(_node).css('background-image', 'url('+_value+')');
-			}
-		});
-
 		this.respond('select', function(_event) {
 			// this removes any screen class that starts with the same thing as the event message
 			// and then adds the event message as a class to the screen
