29cb1e4e1ecee0417277cbb340ee3887d3e644bf
Adam Walzer <adamwalzer@gmail.com>
2016-04-12 17:25:54 -0400

    GAME-664 iOS splash sea-turtle

--- library/sea-turtle/source/js/index.js
+++ library/sea-turtle/source/js/index.js
@@ -7,6 +7,7 @@ import '../../../../../js-interactive-library';
 import './config.game';
 
 // INCLUDE USED COMPONENT BEHAVIORS HERE
+import '../../../shared/js/screen-ios-splash';
 import './components/screen-basic/behavior';
 import './components/screen-quit/behavior';
 import './components/title/behavior';
@@ -29,14 +30,10 @@ pl.game('sea-turtle', function () {
 	 */
 	this.screen('title', function () {
 		
-		this.on('ready', function (_event) {
-			// Screens are display:none then when READY get display:block.
-			// When a screen is OPEN then it transitions a transform,
-			// the delay is to prevent the transition failing to play
-			// because of collision of these styles.
-			// 
-			if (this.is(_event.target)) this.delay(0, this.open);
-			this.close(this.game.loader);
+		this.on('ready', function(_event) {
+			if(!this.is(_event.target)) return;
+
+			if(this.game.iosSplash.state(this.STATE.READY)) this.game.iosSplash.splash();
 		});
 
 		this.startAudio = function () {
