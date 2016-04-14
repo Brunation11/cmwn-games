ccb55223e8e7bf332f5c531a488e6c71b533bad4
Adam Walzer <adamwalzer@gmail.com>
2016-04-14 12:19:10 -0400

    GF-38 pause screen monarch

--- library/monarch/source/js/components/video/behavior.js
+++ library/monarch/source/js/components/video/behavior.js
@@ -13,6 +13,11 @@ pl.game.component('video', function () {
 		this.video[0].pause();
 	};
 
+	this.resume = function() {
+		if(this.isComplete) return;
+		this.video[0].play();
+	};
+
 	this.screen.on('ui-close', this.pause.bind(this));
 
 });
