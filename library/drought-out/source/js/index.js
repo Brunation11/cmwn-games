4c81b32513eb3af3f187bd657826f05b71a936a7
Adam Walzer <adamwalzer@gmail.com>
2016-04-26 16:42:35 -0400

    GAME-348 drought-out mobile title screen fix

--- library/drought-out/source/js/index.js
+++ library/drought-out/source/js/index.js
@@ -2,8 +2,6 @@
  * Index script
  * @module
  */
-// import 'js-interactive-library';
-// import '../../../../../js-interactive-library';
 import './config.game';
 
 import '../../../shared/js/screen-ios-splash';
@@ -16,7 +14,7 @@ import './components/selectable-reveal/behavior';
 import './components/reveal/behavior';
 import './components/audio-sequence/behavior';
 
-import '../../../shared/js/test-platform-integration';
+// import '../../../shared/js/test-platform-integration';
 import '../../../shared/js/google-analytics';
 
 pl.game('drought-out', function () {
@@ -45,25 +43,13 @@ pl.game('drought-out', function () {
 			if(this.game.iosSplash.state(this.STATE.READY)) this.game.iosSplash.splash();
 		});
 
-		this.startAudio = function () {
-			this.title.audio.background.play();
-		};
+		this.entity('cacti', function() {
+			this.on('animationend', function(_e) {
+				if(!this.is(_e.target)) return;
 
-		this.on('ui-open', function (_event) {
-			if (this === _event.targetScope) {
-				this.title.start();
-			}
+				this.complete();
+			});
 		});
-
-		this.on('ready', function (_event) {
-			// Screens are display:none then when READY get display:block.
-			// When a screen is OPEN then it transitions a transform,
-			// the delay is to prevent the transition failing to play
-			// because of collision of these styles.
-			// 
-			if (this.is(_event.target)) this.delay(0, this.open);
-		});
-
 	});
 
 	this.screen('think', selectScreen);
