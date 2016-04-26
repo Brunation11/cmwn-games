b7bb5808d4cea0beff12c33f1796c7456ad986ab
Adam Walzer <adamwalzer@gmail.com>
2016-04-26 16:50:14 -0400

    GAME-652 drought-out VO fixes

--- library/drought-out/source/js/components/audio-sequence/behavior.js
+++ library/drought-out/source/js/components/audio-sequence/behavior.js
@@ -2,19 +2,20 @@ pl.game.component('audio-sequence', function () {
 	var sounds;
 
 	this.on('ready', function (_event) {
-		var i = 1;
+		var self = this;
 
 		if (!this.is(_event.target)) return;
 
 		sounds = this.audio.find('.audio');
 
 		this.audio.on('ended', function (_event) {
-			var next = sounds[i++];
+			var next = sounds[self.i++];
 			if (next) next.play();
-		}.bind(this));
+		});
 	});
 
 	this.start = function() {
+		this.i = 1;
 		if(sounds.length) sounds.play();
 	};
 });
