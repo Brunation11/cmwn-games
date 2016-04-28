72352781eb02e3cddefe71d967a8514e26175680
Adam Walzer <adamwalzer@gmail.com>
2016-04-28 15:30:22 -0400

    be-bright removing unused components

--- library/be-bright/source/js/index.js
+++ library/be-bright/source/js/index.js
@@ -7,13 +7,13 @@
 import './config.game';
 
 import '../../..//shared/js/screen-ios-splash';
-import './components/title/behavior';
+import './components/multiple-choice/behavior';
+import './components/reveal/behavior';
 import './components/screen-basic/behavior';
 import './components/screen-quit/behavior';
-import './components/selectable/behavior';
 import './components/selectable-reveal/behavior';
-import './components/reveal/behavior';
-import './components/multiple-choice/behavior';
+import './components/selectable/behavior';
+import './components/title/behavior';
 import './components/video/behavior';
 
 import '../../../shared/js/test-platform-integration';
