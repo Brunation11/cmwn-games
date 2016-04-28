f7d54000f483a22aa5351f3e8cbb41d13f846ff2
Adam Walzer <adamwalzer@gmail.com>
2016-04-28 17:04:05 -0400

    happy-fish-face replacing tabs

--- library/happy-fish-face/source/js/components/multiple-choice/behavior.js
+++ library/happy-fish-face/source/js/components/multiple-choice/behavior.js
@@ -1,27 +1,27 @@
 pl.game.component('multiple-choice', function () {
 
-	this.validateAnswer = function () {
-		var answers;
+  this.validateAnswer = function () {
+    var answers;
 
-		if (this.properties.correct) {
-			answers = this.properties.correct.split(/\s*,\s*/);
+    if (this.properties.correct) {
+      answers = this.properties.correct.split(/\s*,\s*/);
 
-			if (~answers.indexOf(String(this.getSelected().id()))) {
-				this.complete();
-			}
-		}
+      if (~answers.indexOf(String(this.getSelected().id()))) {
+        this.complete();
+      }
+    }
 
-		return false;
-	}
+    return false;
+  }
 
-	this.answer = function () {
-		if (this.event) {
-			$li = $(this.event.target).closest('li');
+  this.answer = function () {
+    if (this.event) {
+      $li = $(this.event.target).closest('li');
 
-			if (this.select($li)) {
-				this.validateAnswer();
-			}
-		}
-	};
+      if (this.select($li)) {
+        this.validateAnswer();
+      }
+    }
+  };
 
 });
--- library/happy-fish-face/source/js/components/score/behavior.js
+++ library/happy-fish-face/source/js/components/score/behavior.js
@@ -1,76 +1,76 @@
 pl.game.component('score', function () {
 
-	this.value = 0;
+  this.value = 0;
 
-	this.entity('board', function () {
-		
-		this.template = null;
-		this.images = null;
+  this.entity('board', function () {
+    
+    this.template = null;
+    this.images = null;
 
-		this.ready = function () {
-			this.template = this.counter.html();
-			this.images = this.find('img');
-		};
+    this.ready = function () {
+      this.template = this.counter.html();
+      this.images = this.find('img');
+    };
 
-		this.render = function () {
-			var image;
+    this.render = function () {
+      var image;
 
-			if (this.images.length) {
-				image = this.images[this.value];
-				this.select(image);
-			}
-			
-			this.counter.html(this.template.replace('{{score}}', this.value));
+      if (this.images.length) {
+        image = this.images[this.value];
+        this.select(image);
+      }
+      
+      this.counter.html(this.template.replace('{{score}}', this.value));
 
-			return this;
-		};
+      return this;
+    };
 
-	});
+  });
 
-	this.start = () => {
-		this.value = 0;
-		this.board.render();
-	};
+  this.start = () => {
+    this.value = 0;
+    this.board.render();
+  };
 
-	this.ready = function () {
-		this.board.render();
-	};
+  this.ready = function () {
+    this.board.render();
+  };
 
-	this.up = function (_count) {
-		this.value+= _count || 1;
+  this.up = function (_count) {
+    this.value+= _count || 1;
 
-		this.board.render();
+    this.board.render();
 
-		console.log('score', this.value, this.properties.max)
+    console.log('score', this.value, this.properties.max)
 
-		if (this.value >= this.properties.max) {
-			console.log('oh word');
-			this.complete();
-		}
+    if (this.value >= this.properties.max) {
+      console.log('oh word');
+      this.complete();
+    }
 
-		return this;
-	};
+    return this;
+  };
 
-	this.down = function (_count) {
-		this.value-= _count || 1;
+  this.down = function (_count) {
+    this.value-= _count || 1;
 
-		this.board.render();
-		
-		if (this.value >= this.properties.max) {
-			this.complete();
-		}
+    this.board.render();
+    
+    if (this.value >= this.properties.max) {
+      this.complete();
+    }
 
-		return this;
-	};
+    return this;
+  };
 
-	this.state('incomplete','-COMPLETE', {
-		willSet: function (_target) {
-			this.isComplete = false;
+  this.state('incomplete','-COMPLETE', {
+    willSet: function (_target) {
+      this.isComplete = false;
 
-			if(this.value >= this.properties.max) {
-				this.value = this.properties.max - 1;
-			}
-		}
-	});
+      if(this.value >= this.properties.max) {
+        this.value = this.properties.max - 1;
+      }
+    }
+  });
 
 });
