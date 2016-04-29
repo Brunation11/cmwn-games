fc72d5dea7b8f8a8a2b43ecbf625a200779fe077
Adam Walzer <adamwalzer@gmail.com>
2016-04-29 11:28:48 -0400

    printmaster replace tabs

--- library/printmaster/source/js/components/audio-sequence/behavior.js
+++ library/printmaster/source/js/components/audio-sequence/behavior.js
@@ -1,21 +1,21 @@
 pl.game.component('audio-sequence', function () {
-	var i, sounds;
+  var i, sounds;
 
-	this.on('ready', function (_event) {
-		if (!this.is(_event.target)) return;
+  this.on('ready', function (_event) {
+    if (!this.is(_event.target)) return;
 
-		sounds = this.find('audio').map(function () {
-			return $(this).data('context');
-		}).toArray();
+    sounds = this.find('audio').map(function () {
+      return $(this).data('context');
+    }).toArray();
 
-		this.audio.on('ended', function (_event) {
-			var next = sounds[i++];
-			if (next && this.screen.state(this.screen.STATE.OPEN)) next.play();
-		}.bind(this));
-	});
+    this.audio.on('ended', function (_event) {
+      var next = sounds[i++];
+      if (next && this.screen.state(this.screen.STATE.OPEN)) next.play();
+    }.bind(this));
+  });
 
-	this.start = function() {
-		i = 1;
-		if(sounds[0] && this.screen.state(this.screen.STATE.OPEN)) sounds[0].play();
-	};
+  this.start = function() {
+    i = 1;
+    if(sounds[0] && this.screen.state(this.screen.STATE.OPEN)) sounds[0].play();
+  };
 });
--- library/printmaster/source/js/screens/identify.js
+++ library/printmaster/source/js/screens/identify.js
@@ -2,73 +2,73 @@
  * Printmaster - Identify Screen
  */
 export default function identify () {
-	this.on('ui-open', function(_event) {
-		if(!this.is(_event.target)) return;
+  this.on('ui-open', function(_event) {
+    if(!this.is(_event.target)) return;
 
-		this.start();
+    this.start();
 
-		if(this.state(this.STATE.COMPLETE)) {
-			this.selectable.removeClass('COMPLETE').isComplete = false;
-			this.setItem();
-		}
-	});
+    if(this.state(this.STATE.COMPLETE)) {
+      this.selectable.removeClass('COMPLETE').isComplete = false;
+      this.setItem();
+    }
+  });
 
-	this.start = function() {
-		var correct;
+  this.start = function() {
+    var correct;
 
-		correct = pl.Queue.create();
+    correct = pl.Queue.create();
 
-		correct.on('complete', this.bind(function () {
-			this.delay('1.5s', function() {
-				this.audio.sfx.confirmed.play();
-				this.selectable.complete();
-			});
-		}));
+    correct.on('complete', this.bind(function () {
+      this.delay('1.5s', function() {
+        this.audio.sfx.confirmed.play();
+        this.selectable.complete();
+      });
+    }));
 
-		this.items = this
-			.find('.header img')
-			.map(function (_index, _node) {
-				correct.add($(_node).id());
-				return _node;
-			})
-			.toArray();
+    this.items = this
+      .find('.header img')
+      .map(function (_index, _node) {
+        correct.add($(_node).id());
+        return _node;
+      })
+      .toArray();
 
-		this.items.correct = correct;
+    this.items.correct = correct;
 
-		this.headers = this.find('.header img');
-		this.answers = this.find('.items li');
-	};
+    this.headers = this.find('.header img');
+    this.answers = this.find('.items li');
+  };
 
-	this.setItem = function(_idx) {
-		if(this.item && this.items[this.item]) this.items[this.item].ready();
-		this.idx = _idx || 0;
-		this.item = $(this.items[this.idx]).id();
+  this.setItem = function(_idx) {
+    if(this.item && this.items[this.item]) this.items[this.item].ready();
+    this.idx = _idx || 0;
+    this.item = $(this.items[this.idx]).id();
 
-		if(this.item) {
-			this.select(this.headers.filter('[pl-id='+this.item+']'));
-			if(this.selectable) this.selectable.deselectAll();
+    if(this.item) {
+      this.select(this.headers.filter('[pl-id='+this.item+']'));
+      if(this.selectable) this.selectable.deselectAll();
 
-			for(var i = 0; i < 5; i++) {
-				this.delay(i*150, function() {
-					this.answers.each(function (_index, _node) {
-						_node.style.order = Math.round(20*Math.random());
-					});
-				});
-			}
-		}
-	};
+      for(var i = 0; i < 5; i++) {
+        this.delay(i*150, function() {
+          this.answers.each(function (_index, _node) {
+            _node.style.order = Math.round(20*Math.random());
+          });
+        });
+      }
+    }
+  };
 
-	this.respond('select', function(_event) {
-		if(_event.message === this.item) {
-			this.audio.sfx.correct.play();
-			this.audio.sfx.granted.play();
-			this.select(_event.behaviorTarget);
-			this.items.correct.ready(this.item);
-			this.delay('1s', this.setItem.bind(this,++this.idx));
-		} else {
-			this.audio.sfx.incorrect.play();
-			this.audio.sfx.denied.play();
-			this.highlight(_event.behaviorTarget);
-		}
-	});
+  this.respond('select', function(_event) {
+    if(_event.message === this.item) {
+      this.audio.sfx.correct.play();
+      this.audio.sfx.granted.play();
+      this.select(_event.behaviorTarget);
+      this.items.correct.ready(this.item);
+      this.delay('1s', this.setItem.bind(this,++this.idx));
+    } else {
+      this.audio.sfx.incorrect.play();
+      this.audio.sfx.denied.play();
+      this.highlight(_event.behaviorTarget);
+    }
+  });
 }
