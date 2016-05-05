4723678d68b5b73dec5d42f7417a1e0b1bd5e030
Adam Walzer <adamwalzer@gmail.com>
2016-05-05 18:42:22 -0400

    meerkat vo updates

--- library/meerkat-mania/source/js/components/screen-quit/behavior.js
+++ library/meerkat-mania/source/js/components/screen-quit/behavior.js
@@ -1,7 +1,7 @@
 pl.game.component('screen-quit', function () {
 
   this.buttonSound = function () {
-    if (this.game.audio.sfx.button) this.game.audio.sfx.button.play();
+    if (this.audio.sfx.button) this.audio.sfx.button.play();
   };
 
   // TODO: Make an automated way to handle this
@@ -12,8 +12,19 @@ pl.game.component('screen-quit', function () {
     }
   });
 
+  this.on('ready', function (_e) {
+    var ctx;
+
+    if (!this.is(_e.target)) return;
+
+    ctx = new (window.AudioContext || window.webkitAudioContext);
+    if (this.audio.voiceOver.sure) this.audio.voiceOver.sure.setContext(ctx);
+    if (this.audio.sfx.button) this.audio.sfx.button.setContext(ctx);
+  });
+
   this.on('ui-open', function (_event) {
     if (!this.is(_event.target)) return;
+    if (this.audio.voiceOver.sure) this.audio.voiceOver.sure.play();
     this.buttonSound();
     this.game.addClass('QUIT-SCREEN');
     this.removeClass('LEAVE-END');
@@ -21,6 +32,7 @@ pl.game.component('screen-quit', function () {
   });
 
   this.on('ui-leave', function () {
+    if (this.audio.voiceOver.sure) this.audio.voiceOver.sure.stop();
     this.game.removeClass('QUIT-SCREEN');
     this.game.resume();
   });
