a0a0f2de424250676757fca65371bc7e8a542acd
Adam Walzer <adamwalzer@gmail.com>
2016-05-11 12:27:50 -0400

    GAME-708 video and background fix

--- library/monarch/source/js/components/video/behavior.js
+++ library/monarch/source/js/components/video/behavior.js
@@ -1,12 +1,19 @@
 pl.game.component('video', function () {
+  var bg;
 
   this.start = function () {
     var self = this;
     this.video.on('ended', function () {
       self.complete();
     });
-    if (this.game.bgSound) this.game.bgSound.pause();
+    this.stopBackground();
     this.video[0].play();
+  };
+
+  this.stopBackground = function () {
+    if (!this.properties.playBackground && (bg = this.game.media.playing('.background'))) {
+      bg.stop('@ALL');
+    }
   };
 
   this.pause = function () {
@@ -15,6 +22,7 @@ pl.game.component('video', function () {
 
   this.resume = function () {
     if (this.isComplete) return;
+    this.stopBackground();
     this.video[0].play();
   };
 
