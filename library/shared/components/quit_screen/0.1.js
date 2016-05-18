2e99d3e784375620778cd1545fb89b87ef954c8f
Adam Walzer <adamwalzer@gmail.com>
2016-05-18 14:41:52 -0400

    be bright react port

---
+++ library/shared/components/quit_screen/0.1.js
@@ -0,0 +1,38 @@
+class QuitScreen extends play.Screen {
+  constructor() {
+    super();
+
+    this.state = {
+      id: 'quit'
+    };
+
+  }
+
+  okay() {
+
+  }
+
+  cancel() {
+    this.close();
+    play.trigger('menuClose', {
+      id: this.state.id,
+    });
+  }
+
+  render() {
+    return (
+      <div id={this.state.id} className={'screen'+this.getClassNames()}>
+        <div className="center">
+          <div className="frame">
+            <h2>Are you sure you<br/>want to quit?</h2>
+            <h3>Your game progress will be saved</h3>
+            <button className="quit-yes" onClick={this.okay.bind(this)}></button>
+            <button className="quit-no" onClick={this.cancel.bind(this)}></button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default QuitScreen;
