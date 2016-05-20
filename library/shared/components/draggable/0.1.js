81139941576178d35d11c9b8bf7d2d18ac02a886
Adam Walzer <adamwalzer@gmail.com>
2016-05-20 18:33:13 -0400

    draggables

--- library/shared/components/draggable/0.1.js
+++ library/shared/components/draggable/0.1.js
@@ -5,29 +5,71 @@ class Draggable extends play.Component {
     super();
 
     this.boundMouseDown = this.mouseDown.bind(this);
-    this.boundMouseMove = this.mouseMove.bind(this);
     this.boundMouseUp = this.mouseUp.bind(this);
+
+    this.boundMoveEvent = this.moveEvent.bind(this);
+
+    this.boundTouchStart = this.touchStart.bind(this);
+    this.boundTouchEnd = this.touchEnd.bind(this);
   }
 
-  mouseDown(e) {
+  startEvent(e) {
+    var top, left, el;
+
+    if (typeof this.state.top !== 'number') {
+      left = 0;
+      top = 0;
+      el = this.refs['el'];
+
+      while (el) {
+          if (el.className.indexOf('screen') !== -1) {
+            break;
+          }
+
+          left += el.offsetLeft || 0;
+          top += el.offsetTop  || 0;
+          el = el.offsetParent;
+      }
+    }
+
     this.setState({
       dragging: true,
       return: false,
       offsetX: e.offsetX,
       offsetY: e.offsetY,
+      left,
+      top,
     });
-    window.addEventListener('mousemove', this.boundMouseMove);
+  }
+
+  attachMouseEvents() {
+    window.addEventListener('mousemove', this.boundMoveEvent);
     window.addEventListener('mouseup', this.boundMouseUp);
   }
 
-  mouseMove(e) {
+  attachTouchEvents() {
+    window.addEventListener('touchmove', this.boundMoveEvent);
+    window.addEventListener('touchend', this.boundTouchEnd);
+  }
+
+  mouseDown(e) {
+    this.startEvent(e);
+    this.attachMouseEvents();
+  }
+
+  touchStart(e) {
+    this.startEvent(e);
+    this.attachTouchEvents();
+  }
+
+  moveEvent(e) {
     this.setState({
-      x: (e.x-this.state.offsetX)/this.state.scale,
-      y: (e.y-this.state.offsetY)/this.state.scale,
+      x: (e.x-this.state.offsetX)/this.state.scale-this.state.left,
+      y: (e.y-this.state.offsetY)/this.state.scale-this.state.top,
     })
   }
 
-  mouseUp(e) {
+  endEvent() {
     var x, y;
 
     if (this.props.return) {
@@ -41,15 +83,39 @@ class Draggable extends play.Component {
       x,
       y,
     });
-    window.removeEventListener('mousemove', this.boundMouseMove);
+  }
+
+  dettachMouseEvents() {
+    window.removeEventListener('mousemove', this.boundMoveEvent);
     window.removeEventListener('mouseup', this.boundMouseUp);
+  }
+
+  dettachTouchEvents() {
+    window.removeEventListener('touchmove', this.boundMoveEvent);
+    window.removeEventListener('touchend', this.boundTouchEnd);
+  }
+
+  mouseUp(e) {
+    this.endEvent();
+    this.dettachMouseEvents();
+  }
+
+  touchEnd(e) {
+    this.endEvent();
+    this.dettachTouchEvents();
   }
 
   componentDidMount() {
     play.Component.prototype.componentDidMount.call(this);
+    this.setScale();
 
     this.refs['el'].addEventListener('mousedown', this.boundMouseDown);
+    this.refs['el'].addEventListener('touchstart', this.boundTouchStart);
 
+    window.addEventListener('resize', this.setScale.bind(this));
+  }
+
+  setScale() {
     this.setState({
       scale: play.trigger('getState').scale,
     });
