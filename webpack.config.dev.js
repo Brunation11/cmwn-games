2f09cf0ec95fc4bfbddec8cf8b64ca6bce2ab6cc
Adam Walzer <adamwalzer@gmail.com>
2016-06-09 17:30:31 -0400

    updating be bright to newest version of framework

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,32 +1,30 @@
 var path = require('path'),
     webpack = require('webpack');
 
-delete require.cache[require.resolve("js-interactive-library")];
-
 module.exports = {
-    context: __dirname + '/library',
-    entry: null,
-    devtool: 'source-map',
-    resolve: {
-        extensions: ['', '.js'],
-        modulesDirectories: ['node_modules']
-    },
-    plugins: [],
-    output: {
-        path: __dirname+'/build',
-        filename: '[name]/ai.js',
-        publicPath: '/build/'
-    },
-    module: {
-        loaders: [{
-            test: /\.js$/,
-            loaders: ['babel'],
-            include: path.join(__dirname, 'library/'),
-            exclude: [/bower_components/, /node_modules/]
-        }]
-    },
-    postcss: function () {
-        return [autoprefixer];
-    },
-    cache: false
+  context: __dirname + '/library',
+  entry: null,
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['', '.js'],
+    modulesDirectories: ['node_modules']
+  },
+  plugins: [],
+  output: {
+    path: __dirname+'/build',
+    filename: '[name]/ai.js',
+    publicPath: '/build/'
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loaders: ['babel'],
+      include: path.join(__dirname, 'library/'),
+      exclude: [/bower_components/, /node_modules/]
+    }]
+  },
+  postcss: function () {
+    return [autoprefixer];
+  },
+  cache: false
 };
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,50 +1,48 @@
 var path = require('path'),
     webpack = require('webpack');
 
-delete require.cache[require.resolve("js-interactive-library")];
-
 module.exports = {
-    context: __dirname + '/library',
-    entry: null,
-    devtool: 'source-map',
-    resolve: {
-        extensions: ['', '.js'],
-        modulesDirectories: ['node_modules']
-    },
-    plugins: [
-        new webpack.DefinePlugin({
-            'process.env': {
-                'BABEL_ENV': JSON.stringify('production'),
-                'NODE_ENV': JSON.stringify('production')
-            }
-        }),
-        new webpack.optimize.OccurenceOrderPlugin(),
-        new webpack.optimize.DedupePlugin(),
-        new webpack.optimize.UglifyJsPlugin({
-            mangle: false,
-            sourceMap: true,
-            compress: {
-                unused: false,
-                warnings: false
-            }
-        }),
-        new webpack.optimize.AggressiveMergingPlugin()
-    ],
-    output: {
-        path: __dirname+'/build',
-        filename: '[name]/ai.js',
-        publicPath: '/build/'
-    },
-    module: {
-        loaders: [{
-            test: /\.js$/,
-            loaders: ['babel'],
-            include: path.join(__dirname, 'library/'),
-            exclude: [/bower_components/, /node_modules/]
-        }]
-    },
-    postcss: function () {
-        return [autoprefixer];
-    },
-    cache: false
+  context: __dirname + '/library',
+  entry: null,
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['', '.js'],
+    modulesDirectories: ['node_modules']
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+        'process.env': {
+            'BABEL_ENV': JSON.stringify('production'),
+            'NODE_ENV': JSON.stringify('production')
+        }
+    }),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      mangle: false,
+      sourceMap: true,
+      compress: {
+        unused: false,
+        warnings: false
+      }
+    }),
+    new webpack.optimize.AggressiveMergingPlugin()
+  ],
+  output: {
+    path: __dirname+'/build',
+    filename: '[name]/ai.js',
+    publicPath: '/build/'
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loaders: ['babel'],
+      include: path.join(__dirname, 'library/'),
+      exclude: [/bower_components/, /node_modules/]
+    }]
+  },
+  postcss: function () {
+    return [autoprefixer];
+  },
+  cache: false
 };
