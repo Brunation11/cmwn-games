a68e15af49e3f2a4ac892d5046e8015973be8f71
bunation11 <bzatta@gmail.com>
2016-07-22 11:14:56 -0400

    refactor title screen

--- library/survey/components/title_screen.js
+++ library/survey/components/title_screen.js
@@ -2,10 +2,8 @@ var TitleScreen = (
   <skoash.Screen
     id="title"
     checkComplete={false}
-    playOnStart="buzz"
     completeDelay={3000}
   >
-    <skoash.Image ref="background" className="hidden" src="media/assets/_images/_BKG/BKG_1-01.png" />
 
     <skoash.Image ref="ice-cube" className="ice-cube animated" src="media/assets/_images/_BKG/BKG_02-title-01.png" />
     <skoash.Image ref="magnifying-glass" className="magnifying-glass animated" src="media/assets/_images/_title_page/magnifying%20glass.png" />
@@ -17,10 +15,5 @@ var TitleScreen = (
 
 export default TitleScreen;
 
-
-// ask about play on start
 // what is ref buzz controlling
-// add play button audio
-// get confirmation on animate.css
-// add "tada" animation
 // animate play button
