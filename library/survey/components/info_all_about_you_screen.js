e64422d09e7525649b8a3eab129409cc741c2add
bunation11 <bzatta@gmail.com>
2016-07-22 11:14:40 -0400

    refactor all about you screen

--- library/survey/components/info_all_about_you_screen.js
+++ library/survey/components/info_all_about_you_screen.js
@@ -2,17 +2,19 @@ var InfoScreen = (
   <skoash.Screen
     id="info-all-about-you"
   >
+    <skoash.Image ref="penguin-megaphone" className="penguin-megaphone animated" src="media/assets/_images/s_2/img_s2_penguin-01.png" />
+    <skoash.Image ref="spotlight" className="spotlight animated" src="media/assets/_images/s_2/img-spotlight-01.png" />
+    <skoash.Image ref="speech-bubble" className='speech-bubble animated' src="media/assets/_images/s_2/img_s2_speechballoon.png" />
+    <skoash.Image ref="you" className="you animated" src="media/assets/_images/s_2/text-you-01.png" />
     <skoash.Audio ref="vo" type="voiceOver" src="media/assets/_audio/S_HiThere/S_2.1.mp3" />
-    <skoash.Image ref="info" className="animated" src="media/S_6/img_6.png" />
   </skoash.Screen>
 );
 
 export default InfoScreen;
 
-    // <skoash.Image ref="you" className="you animated" src="media/assets/_images/" />
-    // <skoash.Image ref="penguine" className="penguine animated" src="media/assets/_images/" />
-    // <skoash.Image ref="thought-bubble" className="thought-bubble animated" src="media/assets/_images/" />
-    // <skoash.Image ref="stage" className="stage animated" src="media/assets/_images/" />
-// how to set asset to appear after vo
-// assets not included
-//
+// add vo
+// add second animation on you
+// refactor audio S_2.1 to play only after "you" has appeared
+// refactor animation of speech bubble
+// set speech bubble to appear after vo
+// set you to appear after speech bubble
