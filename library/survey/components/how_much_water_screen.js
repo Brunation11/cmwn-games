8a116562533bbc2bd25e0713897d32a956330a68
bunation11 <bzatta@gmail.com>
2016-07-27 12:59:55 -0400

    update reveal assets to use audio sequence

--- library/survey/components/how_much_water_screen.js
+++ library/survey/components/how_much_water_screen.js
@@ -1,11 +1,19 @@
+import AudioSequence from 'shared/components/audio_sequence/0.1'
 import SelectableReveal from 'shared/components/selectable_reveal/0.1';
 
 var HowMuchWaterScreen = (
   <skoash.Screen
     id="how-much-water"
   >
-    <skoash.Audio ref="vo" type="voiceOver" src="media/assets/_audio/VOs/VO_HowMuch.mp3" />
-    <skoash.Audio ref="vo" type="voiceOver" src="media/assets/_audio/VOs/VO_WaterCon.mp3" />
+
+    <AudioSequence
+      ref="vo"
+      checkComplete={true}
+    >
+      <skoash.Audio ref="vo-how-much" type="voiceOver" src="media/assets/_audio/VOs/VO_HowMuch.mp3" />
+      <skoash.Audio ref="vo-water-con" type="voiceOver" src="media/assets/_audio/VOs/VO_WaterCon.mp3" />
+    </AudioSequence>
+
     <skoash.Image ref="banner" className="banner animated" src="media/assets/_images/S_13/Text_13_How%20much%20do%20you%20know.png" />
     <skoash.Image ref="penguin" className="penguin animated" src="media/assets/_images/S_13/IMG_13_Penguins.png" />
     <div ref="frame" className="frame animated"></div>
@@ -30,12 +38,27 @@ var HowMuchWaterScreen = (
         <skoash.ListItem className="not-at-all animated" data-ref="not-at-all" correct />
       ]}
       revealAssets={[
-        <skoash.Audio ref="a-lot" type="voiceOver" src="media/assets/_audio/_Expressions/S_ALot.mp3" />,
-        <skoash.Audio ref="a-lot" type="voiceOver" src="media/assets/_audio/VOs/VO_ALot.mp3" />,
-        <skoash.Audio ref="a-little" type="voiceOver" src="media/assets/_audio/_Expressions/S_ALittle.mp3" />,
-        <skoash.Audio ref="a-little" type="voiceOver" src="media/assets/_audio/VOs/VO_ALittle.mp3" />,
-        <skoash.Audio ref="not-at-all" type="voiceOver" src="media/assets/_audio/_Expressions/S_NotAtAll.mp3" />,
-        <skoash.Audio ref="not-at-all" type="voiceOver" src="media/assets/_audio/VOs/VO_NotAt.mp3" />
+        // <AudioSequence
+        //   ref="a-lot"
+        //   checkComplete={true}
+        // >
+          <skoash.Audio ref="a-lot-expression" type="voiceOver" src="media/assets/_audio/_Expressions/S_ALot.mp3" delay={1000} />,
+          <skoash.Audio ref="a-lot-vo" type="voiceOver" src="media/assets/_audio/VOs/VO_ALot.mp3" delay={1000} />,
+        // </AudioSequence>,
+        // <AudioSequence
+          // ref="a-little"
+          // checkComplete={true}
+        // >
+          <skoash.Audio ref="a-little-expression" type="voiceOver" src="media/assets/_audio/_Expressions/S_ALittle.mp3" delay={1000} />,
+          <skoash.Audio ref="a-little-vo" type="voiceOver" src="media/assets/_audio/VOs/VO_ALittle.mp3" delay={1000} />,
+        // </AudioSequence>,
+        // <AudioSequence
+          // ref="a-not-at-all"
+          // checkComplete={true}
+        // >
+          <skoash.Audio ref="not-at-all-expression" type="voiceOver" src="media/assets/_audio/_Expressions/S_NotAtAll.mp3" delay={1000} />,
+          <skoash.Audio ref="not-at-all-vo" type="voiceOver" src="media/assets/_audio/VOs/VO_NotAt.mp3" delay={1000} />
+        // </AudioSequence>
       ]}
     />
 
