5508c1bf9a39aa8215df26adbfe3b0de83407dff
bunation11 <bzatta@gmail.com>
2016-07-29 09:39:04 -0400

    remove unused sfx

--- library/survey/components/flip_screen.js
+++ library/survey/components/flip_screen.js
@@ -6,7 +6,6 @@ var FlipScreen = (
     }}
   >
     <skoash.Audio ref="vo" type="voiceOver" src="media/assets/_audio/VOs/VO_Finished.mp3" />
-    <skoash.Audio ref="start" type="sfx" src="media/S_10/S_10.2.mp3" delay={4000} />
     <skoash.Image ref="frame" className="frame animated" src="media/assets/_images/S_17/IMG_17_Frame.png" />
     <skoash.Image ref="banner" className="banner animated" src="media/assets/_images/S_17/Text_17_Youfinished.png" />
     <skoash.Image ref="confetti" className="confetti animated" src="media/assets/_images/S_17/IMG_17_Confetti.png" />
