6abb0adc0da744b121bb45a7abb547db65e6e7b2
Adam Walzer <adamwalzer@gmail.com>
2016-08-03 14:49:56 -0400

    skoash update and some step screens

--- library/printmaster/components/info_step_3_screen.js
+++ library/printmaster/components/info_step_3_screen.js
@@ -2,6 +2,19 @@ var InfoStep3Screen = (
   <skoash.Screen
     id="info-step-3"
   >
+    <skoash.Audio type="voiceOver" src="media/S_15/VO_15.1.mp3" />
+    <skoash.Image className="hidden" src="media/_Frame/Fr_1.png" />
+    <skoash.Component className="frame">
+      <skoash.Image className="illustration animated" src="media/S_15/img_15.2.gif" />
+      <skoash.Component>
+        <skoash.Image className="title animated" src="media/S_15/img_15.1.png" />
+        <p>
+          Sprinkle a LITTLE powder<br/>
+          over the surface<br/>
+          that you touched.
+        </p>
+      </skoash.Component>
+    </skoash.Component>
   </skoash.Screen>
 );
 
