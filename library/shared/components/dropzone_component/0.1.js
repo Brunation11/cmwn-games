5fdd50eea24280cb5ff9607db7e2ea664dabb9a8
bunation11 <bzatta@gmail.com>
2016-08-03 15:43:49 -0400

    update dropzone component to account for multiple answers on dropzone

--- library/shared/components/dropzone_component/0.1.js
+++ library/shared/components/dropzone_component/0.1.js
@@ -2,7 +2,7 @@ import classNames from 'classnames';
 
 class DropzoneComponent extends skoash.Component {
   getClassNames() {
-    var message = this.state.content ? this.state.content.props.message : '';
+    var message = this.state.content && this.state.content.props ? this.state.content.props.message : '';
     return classNames(
       message,
       skoash.Component.prototype.getClassNames.call(this)
