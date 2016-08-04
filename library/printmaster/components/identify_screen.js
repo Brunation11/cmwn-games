2097c43b5d3deed1ae3a497bb31cdf5142a24f8d
Adam Walzer <adamwalzer@gmail.com>
2016-08-04 17:37:56 -0400

    identify screen finishing up

--- library/printmaster/components/identify_screen.js
+++ library/printmaster/components/identify_screen.js
@@ -3,18 +3,27 @@ import TargetSelectable from 'shared/components/target_selectable/0.1';
 var IdentityScreen = (
   <skoash.Screen
     id="identify"
+    silentComplete
   >
-    <skoash.Audio ref="correct" type="sfx" src="media/S_9/S_9.1.mp3" />
-    <skoash.Audio ref="incorrect" type="sfx" src="media/S_9/S_9.2.mp3" />
-    <skoash.Audio ref="granted" type="sfx" src="media/S_9/S_9.3.mp3" />
-    <skoash.Audio ref="denied" type="sfx" src="media/S_9/S_9.4.mp3" />
-    <skoash.Audio ref="confirmed" type="sfx" src="media/S_9/S_9.5.mp3" />
+    <skoash.Audio
+      ref="screen-complete"
+      complete
+      type="sfx"
+      src="media/S_9/S_9.5.mp3"
+      delay={1000}
+    />
     <skoash.Image className="hidden" src="media/S_9/img_9.1.png" />
     <skoash.Image className="hidden" src="media/S_9/img_9.8.png" />
     <skoash.Image className="hidden" src="media/S_9/img_9.6.png" />
     <skoash.Image className="hidden" src="media/S_9/img_9.7.png" />
     <skoash.Image className="hidden" src="media/S_9/img_9.9.png" />
     <TargetSelectable className="group"
+      assets={[
+        <skoash.Audio ref="correct" type="sfx" src="media/S_9/S_9.1.mp3" />,
+        <skoash.Audio ref="correct-sound" type="sfx" src="media/S_9/S_9.3.mp3" />,
+        <skoash.Audio ref="incorrect" complete type="sfx" src="media/S_9/S_9.2.mp3" />,
+        <skoash.Audio ref="incorrect-sound" complete type="sfx" src="media/S_9/S_9.4.mp3" />,
+      ]}
       targets={[
         <skoash.Image ref="arch" className="arch animated" src="media/S_9/img_9.2.png" />,
         <skoash.Image ref="loops" className="loops animated" src="media/S_9/img_9.3.png" />,
@@ -22,10 +31,26 @@ var IdentityScreen = (
         <skoash.Image ref="doubleloop" className="doubleloop animated" src="media/S_9/img_9.5.png" />,
       ]}
       selectableList={[
-        <li data-ref="arch" />,
-        <li data-ref="loops" />,
-        <li data-ref="whorl" />,
-        <li data-ref="doubleloop" />,
+        <skoash.Component
+          type="li"
+          checkComplete={false}
+          data-ref="arch"
+        />,
+        <skoash.Component
+          type="li"
+          checkComplete={false}
+          data-ref="loops"
+        />,
+        <skoash.Component
+          type="li"
+          checkComplete={false}
+          data-ref="whorl"
+        />,
+        <skoash.Component
+          type="li"
+          checkComplete={false}
+          data-ref="doubleloop"
+        />,
       ]}
       selectableSelectClass="HIGHLIGHTED"
     />
