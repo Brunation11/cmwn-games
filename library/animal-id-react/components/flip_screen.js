0d9f3bef81380e81ef21f9f8b8da7b3556dc0d14
bunation11 <bzatta@gmail.com>
2016-08-10 12:09:43 -0400

    refactor flip screen

--- library/animal-id-react/components/flip_screen.js
+++ library/animal-id-react/components/flip_screen.js
@@ -5,11 +5,11 @@ var FlipScreen = (
       name: 'flip',
     }}
   >
-    <skoash.Audio ref="vo" type="voiceOver" src="media/assets/_audio/VOs/VO_Finished.mp3" />
-    <skoash.Image ref="frame" className="frame animated" src="media/assets/_images/S_17/IMG_17_Frame.png" />
-    <skoash.Image ref="banner" className="banner animated" src="media/assets/_images/S_17/Text_17_Youfinished.png" />
-    <skoash.Image ref="confetti" className="confetti animated" src="media/assets/_images/S_17/IMG_17_Confetti.png" />
-    <skoash.Image ref="you" className="you animated" src="media/assets/_images/S_17/Text_17_You.png" />
+    <skoash.Audio ref="vo" type="voiceOver" src="media/audio/VO_25-1.mp3" />
+    <skoash.Component>
+      <h3>Thank you for spending time<br/>with us in the Animal Kingdom.<br/>The more you know, the more you care!</h3>
+      <skoash.Image ref="flip" className="flip animated" src="media/images/flip.png" />
+    </skoash.Component>
   </skoash.Screen>
 );
 
