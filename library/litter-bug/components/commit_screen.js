249e55147932bcc8d783a77f15939a54ba92324c
bunation11 <bzatta@gmail.com>
2016-08-11 12:13:09 -0400

    merge

---
+++ library/litter-bug/components/commit_screen.js
@@ -0,0 +1,21 @@
+var CommitScreen = (
+  <skoash.Screen
+    id="commit"
+  >
+    <skoash.Audio ref="vo" type="voiceOver" src="media/S_13/VO_13.1.mp3" />
+    <skoash.Component ref="center" className="center">
+      <skoash.Component ref="frame" className="frame">
+        <skoash.Image ref="background" className="background" src="media/_Frames/FR_3.png" />
+        <p>
+          I promise to NEVER litter<br/>
+          and to pick up the litter<br/>
+          I see whenever I safely can.<br/>
+          I will dispose of it in a<br/>
+          trash can or a recycle bin.
+        </p>
+      </skoash.Component>
+    </skoash.Component>
+  </skoash.Screen>
+);
+
+export default CommitScreen;
