a8f65bd8645bb1f1ae959648f74e78405fdaaa33
Adam Walzer <adamwalzer@gmail.com>
2016-08-15 12:21:00 -0400

    starting to prepare for skoash 1.0.1

--- library/happy-fish-face/components/title_screen.js
+++ library/happy-fish-face/components/title_screen.js
@@ -1,30 +1,32 @@
-var TitleScreen = (
-  <skoash.Screen
-    ref="title"
-    id="title"
-    className="screen"
-    checkComplete={false}
-    completeDelay={2000}
-  >
-    <skoash.Image ref="background" className="fish animated" src="media/_images/_title/img_1.1.png" />
-    <skoash.Image ref="title" className="title animated" src="media/_images/_title/img_1.2.png" />
-    <div ref="bubbles" className='bubbles'>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-    </div>
-  </skoash.Screen>
-);
-
-export default TitleScreen;
+export default function (props, ref, key) {
+  return (
+    <skoash.Screen
+      {...props}
+      ref={ref}
+      key={key}
+      id="title"
+      className="screen"
+      checkComplete={false}
+      completeDelay={2000}
+    >
+      <skoash.Image ref="background" className="fish animated" src="media/_images/_title/img_1.1.png" />
+      <skoash.Image ref="title" className="title animated" src="media/_images/_title/img_1.2.png" />
+      <div ref="bubbles" className="bubbles">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </skoash.Screen>
+  );
+}
--- library/happy-fish-face/index.js
+++ library/happy-fish-face/index.js
@@ -1,3 +1,5 @@
+import _ from 'lodash';
+
 import config from './config.game';
 
 import Loader from 'shared/components/loader/0.1';
@@ -25,20 +27,22 @@ class HappyFishFace extends skoash.Game {
     this.screens = {
       0: iOSScreen,
       1: TitleScreen,
-      2: YouFeelScreen,
-      3: WaterPollutionScreen,
-      4: HealthyWaterScreen,
-      5: CleanWaterScreen,
-      6: BubbleUpScreen,
-      7: MultiBubblesScreen,
-      8: PollutesWaterScreen,
-      9: TrashScreen,
-      10: FlipScreen,
+      // 2: YouFeelScreen,
+      // 3: WaterPollutionScreen,
+      // 4: HealthyWaterScreen,
+      // 5: CleanWaterScreen,
+      // 6: BubbleUpScreen,
+      // 7: MultiBubblesScreen,
+      // 8: PollutesWaterScreen,
+      // 9: TrashScreen,
+      // 10: FlipScreen,
     };
 
     this.menus = {
       quit: QuitScreen,
     };
+
+    this.state.data.screens = _.map(this.screens, () => ({}));
   }
 
   getBackgroundIndex(index) {
