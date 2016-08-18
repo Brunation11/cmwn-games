facbdfc86e61596a806438fdceb0d140d379b02f
Adam Walzer <adamwalzer@gmail.com>
2016-08-18 15:31:56 -0400

    meerkat mania all screens

--- library/meerkat-mania/index.js
+++ library/meerkat-mania/index.js
@@ -8,6 +8,12 @@ import iOSScreen from 'shared/components/ios_splash_screen/0.1';
 import TitleScreen from './components/title_screen';
 import ExcelScreen from './components/excel_screen';
 import RolesScreen from './components/roles_screen';
+import LookOutScreen from './components/look_out_screen';
+import VideoScreen from './components/video_screen';
+import FeelScreen from './components/feel_screen';
+import MoveScreen from './components/move_screen';
+import Video2Screen from './components/video_2_screen';
+import FlipScreen from './components/flip_screen';
 
 import QuitScreen from 'shared/components/quit_screen/0.1';
 
@@ -22,6 +28,12 @@ class MeerkatMania extends skoash.Game {
       1: TitleScreen,
       2: ExcelScreen,
       3: RolesScreen,
+      4: LookOutScreen,
+      5: VideoScreen,
+      6: FeelScreen,
+      7: MoveScreen,
+      8: Video2Screen,
+      9: FlipScreen,
     };
 
     this.menus = {
@@ -42,6 +54,7 @@ class MeerkatMania extends skoash.Game {
       <div>
         <skoash.Audio ref="bkg-1" type="background" src="media/_BKG/S_BKG_1.mp3" loop />
         <skoash.Audio ref="bkg-2" type="background" src="media/_BKG/S_BKG_2.mp3" loop />
+        <skoash.Audio ref="bkg-3" type="background" src="media/S_13/S_13.1.mp3" />
         <skoash.Audio ref="button" type="sfx" src="media/_Buttons/S_BU_1.mp3" />
         <skoash.Audio ref="close" type="sfx" src="media/_Buttons/S_Close_1.mp3" />
         <skoash.Image className="hidden" src="media/_BKG/BKG_1.png" />
