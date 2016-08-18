f63dcb95a86c7954fa68394d0a3f1480bdd6f8a6
Adam Walzer <adamwalzer@gmail.com>
2016-08-18 11:19:35 -0400

    meerkat bootstrap

---
+++ library/meerkat-mania/components/title_screen.js
@@ -0,0 +1,15 @@
+export default function (props, ref, key) {
+  return (
+    <skoash.Screen
+      {...props}
+      ref={ref}
+      key={key}
+      id="title"
+      checkComplete={false}
+      completeDelay={3000}
+      completeOnStart
+    >
+        <skoash.Image class="animated" src="media/S_1/img_1.1.png" />
+    </skoash.Screen>
+  );
+}
