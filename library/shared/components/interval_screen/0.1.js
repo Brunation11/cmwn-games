0921b57999e5e36696f192e4edf1b09efd5e4975
Annalise Mariottini <annaliseirene@msn.com>
2016-08-19 16:00:53 -0400

    info fuel oxygen, plus changes to info checmical, plus new interval screen component

---
+++ library/shared/components/interval_screen/0.1.js
@@ -0,0 +1,58 @@
+import _ from 'lodash';
+
+class IntervalScreen extends skoash.Screen {
+  open(opts) {
+    super.open(opts);
+
+    this.setState(this.props.states);
+  }
+
+  startNextAnimation() {
+    var states = Object.keys(this.props.states);
+    var nextAnimation = states.find(key => {
+      return !this.state[key];
+    });
+    console.log(nextAnimation);
+    var temp = {};
+    temp[nextAnimation] = true;
+    this.setState(temp);
+  }
+
+  getClassNames() {
+    var classNames = super.getClassNames();
+    _.each(this.props.states, (value, key) => {
+      value = this.state[key];
+      if (value) classNames += ' ' + key.toUpperCase();
+    });
+    return classNames;
+  }
+
+  renderContentList(listName = 'children') {
+    var self = this;
+    var children = [].concat(this.props[listName]);
+    return children.map((component, key) => {
+      if (!component) return;
+      var ref = component.ref || component.props['data-ref'] || listName + '-' + key;
+      if (component.type.name === 'AudioSequence') { // does not work for media sequence yet
+        return (
+          <component.type
+            {...component.props}
+            ref={ref}
+            key={key}
+            playInterval={self.props.interval}
+            onPlay={self.startNextAnimation.bind(this)}
+          />
+        );
+      }
+      return (
+        <component.type
+          {...component.props}
+          ref={ref}
+          key={key}
+        />
+      );
+    });
+  }
+}
+
+export default IntervalScreen;
