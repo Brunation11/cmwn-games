e5fb4f66197bea8e863f9de37c2beec72dd1663b
bunation11 <bzatta@gmail.com>
2016-08-22 15:59:36 -0400

    add new carausel screen

---
+++ library/polar-bear-react/components/choose_carousel_screen.js
@@ -0,0 +1,43 @@
+import CarouselCannon from 'shared/components/carousel_cannon/0.1';
+import Randomizer from 'shared/components/randomizer/0.1';
+
+function onSelect(target) {
+  console.log(target.props.name); // eslint-disable-line no-console
+}
+
+export default function (props, ref, key) {
+  return (
+    <skoash.Screen
+      {...props}
+      ref={ref}
+      key={key}
+      id="choose-carousel"
+    >
+      <CarouselCannon
+        ref="carousel-cannon"
+        carouselBin={
+          <Randomizer
+            ref="randomizer"
+            bin={[
+              <skoash.Image name="fox" src="media/images/carousel-bears/carousel-slide-1.png" />,
+              <skoash.Image name="dog" src="media/images/carousel-bears/carousel-slide-2.png" />,
+              <skoash.Image name="polar" src="media/images/carousel-bears/carousel-slide-7.png" />,
+              <skoash.Image name="cat" src="media/images/carousel-bears/carousel-slide-3.png" />,
+              <skoash.Image name="ermine" src="media/images/carousel-bears/carousel-slide-4.png" />,
+              <skoash.Image name="goat" src="media/images/carousel-bears/carousel-slide-5.png" />,
+              <skoash.Image name="peacock" src="media/images/carousel-bears/carousel-slide-6.png" />,
+              <skoash.Image name="polar" src="media/images/carousel-bears/carousel-slide-7.png" />,
+              <skoash.Image name="seal" src="media/images/carousel-bears/carousel-slide-8.png" />,
+              <skoash.Image name="hare" src="media/images/carousel-bears/carousel-slide-9.png" />,
+              <skoash.Image name="polar" src="media/images/carousel-bears/carousel-slide-7.png" />,
+            ]}
+          />
+        }
+        cannonBall={
+          <skoash.Image src="media/images/cannon-bears/ball.png" />
+        }
+        onSelect={onSelect}
+      />
+    </skoash.Screen>
+  );
+}
