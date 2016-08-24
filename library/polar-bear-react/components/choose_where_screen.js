59499de36ac41297143fe5e80a9ed85bd18b868c
bunation11 <bzatta@gmail.com>
2016-08-24 09:49:33 -0400

    refactor map screen to allow proper reveals when clicking on countries

--- library/polar-bear-react/components/choose_where_screen.js
+++ library/polar-bear-react/components/choose_where_screen.js
@@ -11,47 +11,49 @@ export default function (props, ref, key) {
       <skoash.Component className="map">
         <SelectableCanvasReveal
           ref="selectable-canvas-reveal"
+          selectableSelectClass="HIGHLIGHTED"
+          answers={["0", "1", "2", "3", "4", "5"]}
           assets={[
             <skoash.Audio ref="correct" type="sfx" src="media/audio/answer-correct.mp3" />,
             <skoash.Audio ref="incorrect" type="sfx" src="media/audio/answer-incorrect.mp3" complete />
           ]}
           selectableList={[
             <skoash.Component>
-              <skoash.Image ref="img" className="border animated" src="media/images/map/img_9.13.png" />
+              <skoash.Image ref="img" data-ref="greenland" className="greenland animated" src="media/images/map/img_9.3.png" />
             </skoash.Component>,
             <skoash.Component>
-              <skoash.Image ref="img" className="labels animated" src="media/images/map/img_9.2.png" />
-            </skoash.Component>,
-            <skoash.Component>
-              <skoash.Image ref="img" className="greenland animated" src="media/images/map/img_9.3.png" />
-            </skoash.Component>,
-            <skoash.Component>
-              <skoash.Image ref="img" className="alaska animated" src="media/images/map/img_9.4.png" />
+              <skoash.Image ref="img" data-ref="alaska" className="alaska animated" src="media/images/map/img_9.4.png" />
            </skoash.Component>,
             <skoash.Component>
-              <skoash.Image ref="img" className="canada animated" src="media/images/map/img_9.5.png" />
+              <skoash.Image ref="img" data-ref="canada" className="canada animated" src="media/images/map/img_9.5.png" />
             </skoash.Component>,
             <skoash.Component>
-              <skoash.Image ref="img" className="norway animated" src="media/images/map/img_9.8.png" />
+              <skoash.Image ref="img" data-ref="norway" className="norway animated" src="media/images/map/img_9.8.png" />
             </skoash.Component>,
             <skoash.Component>
-              <skoash.Image ref="img" className="north-pole animated" src="media/images/map/img_9.10.png" />
+              <skoash.Image ref="img" data-ref="north-pole" className="north-pole animated" src="media/images/map/img_9.10.png" />
             </skoash.Component>,
             <skoash.Component>
-              <skoash.Image ref="img" className="russia animated" src="media/images/map/img_9.11.png" />
+              <skoash.Image ref="img" data-ref="russia" className="russia animated" src="media/images/map/img_9.11.png" />
             </skoash.Component>,
             <skoash.Component>
-              <skoash.Image ref="img" className="finland animated" src="media/images/map/img_9.6.png" />
+              <skoash.Image ref="img" data-ref="finland" className="finland animated" src="media/images/map/img_9.6.png" />
             </skoash.Component>,
             <skoash.Component>
-              <skoash.Image ref="img" className="sweden animated" src="media/images/map/img_9.7.png" />
+              <skoash.Image ref="img" data-ref="sweedan" className="sweden animated" src="media/images/map/img_9.7.png" />
             </skoash.Component>,
             <skoash.Component>
-              <skoash.Image ref="img" className="denmark animated" src="media/images/map/img_9.9.png" />
+              <skoash.Image ref="img" data-ref="denmark" className="denmark animated" src="media/images/map/img_9.9.png" />
             </skoash.Component>,
             <skoash.Component>
-              <skoash.Image ref="img" className="iceland animated" src="media/images/map/img_9.12.png" />
-            </skoash.Component>
+              <skoash.Image ref="img" data-ref="iceland" className="iceland animated" src="media/images/map/img_9.12.png" />
+            </skoash.Component>,
+            <skoash.Component>
+              <skoash.Image className="border animated" src="media/images/map/img_9.13.png" />
+            </skoash.Component>,
+            <skoash.Component>
+              <skoash.Image className="labels animated" src="media/images/map/img_9.2.png" />
+            </skoash.Component>,
           ]}
           revealAssets={[
             <skoash.Audio ref="vo-1" type="voiceOver" src="media/audio/map/VO_9.2.mp3" delay={500} />,
