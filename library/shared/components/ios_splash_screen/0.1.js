109255972981324931cadf0a37452488a0f858d0
Adam Walzer <adamwalzer@gmail.com>
2016-08-24 17:54:39 -0400

    ios css update CORE-1739

--- library/shared/components/ios_splash_screen/0.1.js
+++ library/shared/components/ios_splash_screen/0.1.js
@@ -7,6 +7,7 @@ export default function (props, ref, key) {
       id="ios-splash"
       checkComplete={false}
       completeDelay={6000}
+      completeOnStart
       hidePrev
     >
       <skoash.Image className="hidden" src="../shared/images/ios_start_ball.png" />
