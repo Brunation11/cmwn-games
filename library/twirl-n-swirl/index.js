8f290cf0bfcdaf2b02a0bf962d6adee22ac676a8
bunation11 <bzatta@gmail.com>
2016-08-26 17:39:29 -0400

    refactor game index to support new framework

--- library/twirl-n-swirl/index.js
+++ library/twirl-n-swirl/index.js
@@ -5,9 +5,9 @@ import Loader from 'shared/components/loader/0.1';
 import iOSScreen from 'shared/components/ios_splash_screen/0.1';
 import TitleScreen from './components/title_screen';
 import InfoQuestionScreen from './components/info_question_screen';
-import PlantScreen from './components/plant_screen';
+import InfoPlantScreen from './components/info_plant_screen';
 import FlushItScreen from './components/flush_it_screen';
-import OwnershipScreen from './components/ownership_screen';
+import ResponsibleFlusherScreen from './components/responsible_flusher_screen';
 import FlipScreen from './components/flip_screen';
 
 import QuitScreen from 'shared/components/quit_screen/0.1';
@@ -20,17 +20,19 @@ class TwirlNSwirl extends skoash.Game {
 
     this.screens = {
       0: iOSScreen,
-      /*1: TitleScreen,
+      1: TitleScreen,
       2: InfoQuestionScreen,
-      3: PlantScreen,
-      4: OwnershipScreen,
-      5: FlipScreen,*/
-        1: FlushItScreen,
+      3: InfoPlantScreen,
+      4: FlushItScreen,
+      5: ResponsibleFlusherScreen,
+      6: FlipScreen,
     };
 
     this.menus = {
-      quit: QuitScreen,
+      quit: <QuitScreen />,
     };
+
+    this.state.data.screens = _.map(this.screens, () => ({}));
   }
 
   renderLoader() {
@@ -39,11 +41,25 @@ class TwirlNSwirl extends skoash.Game {
     );
   }
 
+  getBackgroundIndex(currentScreenIndex) {
+    switch (currentScreenIndex) {
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+      return 0;
+    default:
+      return;
+    }
+  }
+
   renderAssets() {
     return (
       <div>
-        <skoash.Audio ref="bkg" type="background" src="media/audio/title/background.mp3" />
-        
+        <skoash.Audio ref="bkg-1" type="background" src="media/audio/title/background.mp3" />
+
         <skoash.Audio ref="button" type="sfx" src="media/audio/button.mp3" />
         <skoash.Audio ref="screen-complete" type="sfx" src="media/audio/screen-complete.mp3" />
       </div>
