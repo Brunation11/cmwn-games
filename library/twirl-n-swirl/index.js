387b0368831fc0ba9427ffccfd34e55b494dad75
bunation11 <bzatta@gmail.com>
2016-08-31 16:58:18 -0400

    uncomment shared platform integration

--- library/twirl-n-swirl/index.js
+++ library/twirl-n-swirl/index.js
@@ -12,7 +12,7 @@ import FlipScreen from './components/flip_screen';
 
 import QuitScreen from 'shared/components/quit_screen/0.1';
 
-//import 'shared/js/test-platform-integration';
+import 'shared/js/test-platform-integration';
 
 class TwirlNSwirl extends skoash.Game {
   constructor() {
