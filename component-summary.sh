ca73ec036c8b32432de740a0be89be4d78cc7317
Adam Walzer <adamwalzer@gmail.com>
2016-09-06 13:15:41 -0400

    bash updates

--- component-summary.sh
+++ component-summary.sh
@@ -1,7 +1,31 @@
 #!/bin/bash
-for f in ./library/shared/components/**/*.js;
+for file in ./library/shared/components/**/*.js;
 do
-  echo ${f:28}};
+  # echo ${file:28} | sed 's/\.js//g'
+  component=$(echo ${file:28} | sed 's/\.js//g')
+  echo $component
+  files=$(grep -oR "$component" library/**/components/*.js)
+  files=$(echo $files | sed 's/library\///g' | sed 's/\/components\/[^ ]*//g')
+  # files=$(echo $files | sed 's/\/components\/[^ ]*//g')
+  files=$(echo $files | xargs -n1 | sort -u)
+  # files=$(echo $files | sed 's/\/components\///g')
+  # files=$(echo $files | sed 's/\/components\/.//g')
+  # games=$(echo $files | sed -e '/library\/[^/]*/g')
+  # games=$(grep '\(library\/[^/]*\)' "$files")
+  # games=$(echo `expr "$files" : '\(library\/[^/]*\)'`)
+  echo $files
+  echo
+  echo
+  # for g in $(expr "$files" : '\(library\/[^/]*\)');
+  # do
+  #   echo g
+  #   echo $g
+  # done
+  # echo $files
+  # echo
+  # echo $games
+  # echo
+  # echo
   # for i in $(echo $f | tr "shared/components/" "\n");
   # do
   #   echo $i
