29979ad6b0ef698f6d5c19e1026be062fec3396a
Adam Walzer <adamwalzer@gmail.com>
2016-09-09 18:47:32 -0400

    deleting unnecessary comments

--- delete-invalid-files.sh
+++ delete-invalid-files.sh
@@ -1,13 +1,6 @@
 #!/usr/local/bin/bash
 for file in ./build/**/* ./build/**/**/* ./build/**/**/**/* ./build/**/**/**/**/*; do
-  # pat=^[[:alnum:]]
-  # pat=^\s$
-  # pat="[^\s]"
-  # pat=[^\w\/\.\-]
   pat=([^A-Za-z0-9\/\._\\\-])
-  # pat=^[A-Za-z\d\/\.\-_]$
-  # pat=[.\s.]
-  # pat="^.*[[:space:]]+.*$"
   # pat="^.*[^A-Za-z0-9\/\._\\\-]+.*$"
   if [[ $file =~ $pat ]]; then
     echo $file
