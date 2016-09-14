7547005b7c03e2fba36a6fa7c7e5adb972cef0ce
Annalise Mariottini <annaliseirene@msn.com>
2016-09-14 16:37:04 -0400

    rearranging files, initial basic changes

---
+++ library/sea-turtle-ll/source/js/config.game.js
@@ -0,0 +1,16 @@
+pl.game.config({
+  screenSelector: '.screen',
+  componentDirectory: 'components/',
+
+  dimensions: {
+    width: 960,
+    ratio: 16 / 9
+  },
+
+  bgVolume: {
+    drop: 0.2,
+    max: 1
+  },
+
+  shouldLoadComponentStyles: false
+});
---
+++ library/sea-turtle/index.js
@@ -0,0 +1,47 @@
+
+import _ from 'lodash';
+
+import config from './config.game';
+
+import Loader from 'shared/components/loader/0.1';
+
+import iOSScreen from 'shared/components/ios_splash_screen/0.1';
+//import TitleScreen from './components/title_screen';
+import QuitScreen from 'shared/components/quit_screen/0.1';
+
+import 'shared/js/test-platform-integration';
+
+class SeaTurtle extends skoash.Game {
+  constructor() {
+    super(config);
+
+    this.screens = {
+      0: iOSScreen,
+//      1: TitleScreen,
+    };
+
+    this.menus = {
+      quit: QuitScreen,
+    };
+
+    this.state.data.screens = _.map(this.screens, () => ({}));
+  }
+
+  renderLoader() {
+    return (
+      <Loader />
+    );
+  }
+
+  renderAssets() {
+    return (
+      <div>
+      </div>
+    );
+  }
+
+}
+
+skoash.start(SeaTurtle, config.id);
+
+import 'shared/js/google-analytics';
