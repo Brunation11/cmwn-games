66faec9538e101d200c38fe6a2a9603494f955a3
Annalise Mariottini <annaliseirene@msn.com>
2016-09-14 18:07:08 -0400

    info globe screen

--- library/sea-turtle/index.js
+++ library/sea-turtle/index.js
@@ -8,6 +8,7 @@ import Loader from 'shared/components/loader/0.1';
 import iOSScreen from 'shared/components/ios_splash_screen/0.1';
 import TitleScreen from './components/title_screen';
 import VideoScreen from './components/video_screen';
+import InfoGlobeScreen from './components/info_globe_screen';
 import QuitScreen from 'shared/components/quit_screen/0.1';
 
 //import 'shared/js/test-platform-integration';
@@ -19,7 +20,8 @@ class SeaTurtle extends skoash.Game {
     this.screens = {
       0: iOSScreen,
       1: TitleScreen,
-      2: VideoScreen,
+      //2: VideoScreen,
+      2: InfoGlobeScreen,
     };
 
     this.menus = {
@@ -35,10 +37,17 @@ class SeaTurtle extends skoash.Game {
     );
   }
 
+  getBackgroundIndex(index) {
+    if (index < 2) return 0;
+    if (index > 2) return 1;
+    return -1;
+  }
+
   renderAssets() {
     return (
       <div>
         <skoash.Audio ref="bkg-1" type="background" src="media/audio/SO_1.1.mp3" />
+        <skoash.Audio ref="bkg-2" type="background" src="media/audio/background/1.mp3" loop />
         <skoash.Audio ref="button" type="sfx" src="media/audio/button.mp3" />
         <skoash.Audio ref="screen-complete" type="sfx" src="media/audio/button-next-activated.mp3" />
       </div>
