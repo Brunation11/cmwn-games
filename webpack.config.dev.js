a17a014b379291594a6ea9f2db20ee74438cdc70
Annalise Mariottini <annaliseirene@msn.com>
2016-09-21 16:39:10 -0400

    webpack fixes

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -18,9 +18,11 @@ module.exports = {
   module: {
     loaders: [{
       test: /\.js$/,
-      loaders: ['babel'],
-      include: path.join(__dirname, 'library/'),
-      exclude: [/bower_components/, /node_modules/]
+      loader: ['babel'],
+      exclude: [/bower_components/, /node_modules/],
+      query: {
+        presets: ['react', 'es2015', 'stage-0']
+      }
     }]
   },
   postcss: function () {
