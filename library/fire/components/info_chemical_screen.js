e7b67e9b90390535c4ababc9d30d6441deab883b
Annalise Mariottini <annaliseirene@msn.com>
2016-09-28 18:03:17 -0400

    interval screen removed from info chemical screen

--- library/fire/components/info_chemical_screen.js
+++ library/fire/components/info_chemical_screen.js
@@ -1,34 +1,76 @@
-import AudioSequence from 'shared/components/audio_sequence/0.1';
+import _ from 'lodash';
 
-import IntervalScreen from 'shared/components/interval_screen/0.1';
+import MediaCollection from 'shared/components/media_collection/0.1';
+
+const STATES = [
+  { 
+    name: 'VO',
+    audio: 'children-0',
+    interval: 2000,
+  },
+  {
+    name: 'FLAME',
+    audio: 'children-1',
+    interval: 1000
+  },
+  {
+    name: 'EQUAL',
+    audio: 'children-2',
+    interval: 700
+  },
+  {
+    name: 'TEXT',
+    audio: 'children-3',
+  },
+];
 
 export default function (props, ref, key) {
+
+  function play(open, i) {
+    var state = STATES[i];
+
+    open += ' ' + state.name;
+    skoash.trigger('updateState', {
+      path: 'states',
+      data: {
+        open,
+        play: state.audio,
+      }
+    });
+
+    if (state.interval) {
+      setTimeout(() => {
+        play(open, ++i);
+      }, state.interval);
+    }
+  }
+
+  function init() {
+    play('', 0);
+  }
+
   return (
-    <IntervalScreen
+    <skoash.Screen
       {...props}
       ref={ref}
       key={key}
       id="info-chemical"
-      states={{
-        'voice-over': false,
-        flame: false,
-        equal: false,
-        text: false
-      }}
-      interval={[2000, 1000, 700]}
+      onOpen={init}
+      className={_.get(props, 'data.states.open', null)}
     >
-      <AudioSequence ref="media-sequence">
+      <MediaCollection ref="media-collection" play={_.get(props, 'data.states.play', null)}>
         <skoash.Audio type="voiceOver" src="media/S_2/vo_ChemicalReaction.mp3" />  
         <skoash.Audio type="sfx" src="media/S_2/S_2.2.mp3" />
         <skoash.Audio type="sfx" src="media/S_2/S_2.3.mp3" />
         <skoash.Audio type="sfx" src="media/S_2/S_2.4.mp3" />
-      </AudioSequence>
+      </MediaCollection>
       <skoash.Image className="animated" src="media/S_2/img_2.1.png" />
       <skoash.Image className="animated flame" src="media/S_2/img_2.2.png" />
       <skoash.Image className="animated equal" src="media/S_2/img_2.3.png" />
       <h2 className="animated text">
         Chemical<br />Reaction
       </h2>
-    </IntervalScreen>
+    </skoash.Screen>
   );
 }
+
