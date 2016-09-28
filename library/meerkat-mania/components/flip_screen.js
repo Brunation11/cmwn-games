d8018a113bbfeae472db9ef4c9943d3ace315814
bunation11 <bzatta@gmail.com>
2016-09-28 16:12:44 -0400

    add emit on save to flip page

--- library/meerkat-mania/components/flip_screen.js
+++ library/meerkat-mania/components/flip_screen.js
@@ -7,6 +7,9 @@ export default function (props, ref, key) {
       ref={ref}
       key={key}
       id="flip"
+      emitOnComplete={{
+        name: 'flip',
+      }}
     >
       <skoash.MediaSequence>
         <skoash.Audio type="voiceOver" src="media/S_13/VO_13-1.mp3" />
