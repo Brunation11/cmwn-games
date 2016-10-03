9c62d46a69ccc20be50af42261ad51021baddc5d
Adam Walzer <adamwalzer@gmail.com>
2016-10-03 12:09:55 -0400

    updating LB to skoash 1.0.3 to resolve issues

--- library/litter-bug/components/room_screen.js
+++ library/litter-bug/components/room_screen.js
@@ -1,12 +1,15 @@
-var RoomScreen = (
-  <skoash.Screen
-    id="room"
-  >
-    <skoash.Audio ref="vo" type="voiceOver" src="media/S_4/VO_4.1.mp3" />
-    <skoash.Audio ref="button" complete type="sfx" src="media/S_4/VO_4.2.mp3" />
-    <skoash.Image ref="image-1" className="animated" src="media/S_4/img_4.1.png" />
-    <skoash.Image ref="image-2" className="animated" src="media/S_4/img_4.2.png" />
-  </skoash.Screen>
-);
-
-export default RoomScreen;
+export default function (props, ref, key) {
+  return (
+    <skoash.Screen
+      {...props}
+      ref={ref}
+      key={key}
+      id="room"
+    >
+      <skoash.Audio ref="vo" type="voiceOver" src="media/S_4/VO_4.1.mp3" />
+      <skoash.Audio ref="button" complete type="sfx" src="media/S_4/VO_4.2.mp3" />
+      <skoash.Image ref="image-1" className="animated" src="media/S_4/img_4.1.png" />
+      <skoash.Image ref="image-2" className="animated" src="media/S_4/img_4.2.png" />
+    </skoash.Screen>
+  );
+}
--- library/litter-bug/config.game.js
+++ library/litter-bug/config.game.js
@@ -1,7 +1,7 @@
 var config = {
   id: 'litter-bug',
   version: 2,
-  skoash: '1.0.0',
+  skoash: '1.0.3',
   dimensions: {
     width: 960,
     ratio: 16 / 9
