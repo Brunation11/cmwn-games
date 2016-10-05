b033993e8c108fb9a3f847e2767d251fc7c9b28b
Annalise Mariottini <annaliseirene@msn.com>
2016-10-05 17:04:22 -0400

    info jelly screen complete

--- library/sea-turtle/index.js
+++ library/sea-turtle/index.js
@@ -12,6 +12,7 @@ import GlobeScreen from './components/globe_screen';
 import InfoTrashScreen from './components/info_trash_screen';
 import TrashScreen from './components/trash_screen';
 import InfoKeepGoingScreen from './components/info_keep_going_screen';
+import InfoJellyScreen from './components/info_jelly_screen';
 import QuitScreen from 'shared/components/quit_screen/0.1';
 
 //import 'shared/js/test-platform-integration';
@@ -28,7 +29,8 @@ class SeaTurtle extends skoash.Game {
       //4: GlobeScreen,
       //5: InfoTrashScreen,
       //6: TrashScreen,
-      2: InfoKeepGoingScreen,
+      //7: InfoKeepGoingScreen,
+      2: InfoJellyScreen,
     };
 
     this.menus = {
@@ -48,7 +50,8 @@ class SeaTurtle extends skoash.Game {
     if (index < 2) return 0;
     if (index > 2 && index < 5) return 1;
     if (index >= 5 && index < 7) return 2;
-    if (index >= 7) return 3;
+    if (index == 7) return 3;
+    if (index == 8) return 4;
     
     return -1;
   }
@@ -59,7 +62,8 @@ class SeaTurtle extends skoash.Game {
         <skoash.Audio ref="bkg-1" type="background" src="media/audio/SO_1.1.mp3" />
         <skoash.Audio ref="bkg-2" type="background" src="media/audio/background/1.mp3" loop />
         <skoash.Audio ref="bkg-3" type="background" src="media/audio/background/3.mp3" loop />
-        <skoash.Audio ref="bkg-4"type="background" src="media/audio/SO_7.1.mp3" />
+        <skoash.Audio ref="bkg-4" type="background" src="media/audio/SO_7.1.mp3" loop />
+        <skoash.Audio ref="bkg-5" type="background" src="media/audio/background/3.mp3" loop />
         <skoash.Audio ref="button" type="sfx" src="media/audio/button.mp3" />
         <skoash.Audio ref="screen-complete" type="sfx" src="media/audio/button-next-activated.mp3" />
       </div>
