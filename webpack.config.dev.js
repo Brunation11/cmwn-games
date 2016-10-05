d84f5c68e213fb62956d926e88e81eb4b938bd60
Annalise Mariottini <annaliseirene@msn.com>
2016-10-05 16:32:29 -0400

    removed fact

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -18,9 +18,12 @@ module.exports = {
   module: {
     loaders: [{
       test: /\.js$/,
-      loaders: ['babel'],
-      include: path.join(__dirname, 'library/'),
-      exclude: [/bower_components/, /node_modules/]
+      loader: ['babel'],
+//      include: path.join(__dirname, 'library/'),
+      exclude: [/bower_components/, /node_modules/],
+      query: {
+          presets: ['es2015', 'react', 'stage-0']
+      },
     }]
   },
   postcss: function () {
