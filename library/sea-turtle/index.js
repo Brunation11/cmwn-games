06b299290857b751009d3b8ed6dd319b56659303
Annalise Mariottini <annaliseirene@msn.com>
2016-10-06 18:54:02 -0400

    uncommented js-integration

--- library/sea-turtle/index.js
+++ library/sea-turtle/index.js
@@ -17,7 +17,7 @@ import JellyfishScreen from './components/jellyfish_screen';
 import FlipScreen from './components/flip_screen';
 import QuitScreen from 'shared/components/quit_screen/0.1';
 
-//import 'shared/js/test-platform-integration';
+import 'shared/js/test-platform-integration';
 
 class SeaTurtle extends skoash.Game {
   constructor() {
