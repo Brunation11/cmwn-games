174fe461f2328f3b56d95b22af716a9e514e8f71
Adam Walzer <adamwalzer@gmail.com>
2016-10-06 15:04:06 -0400

    update dpad methods CORE-2348

--- library/shared/components/d_pad/0.1.js
+++ library/shared/components/d_pad/0.1.js
@@ -6,41 +6,48 @@ const LEFT = 'left';
 const RIGHT = 'right';
 
 class DPad extends skoash.Component {
-  bootstrap() {
-    var mousedown, mouseup, keydown;
+  constructor() {
+    super();
 
+    this.mousedown = this.mousedown.bind(this);
+    this.clear = this.clear.bind(this);
+    this.keydown = this.keydown.bind(this);
+  }
+
+  mousedown(e) {
+    this.updateRef(e.target.getAttribute('data-ref'));
+  }
+
+  clear() {
+    this.updateRef(null);
+  }
+
+  keydown(e) {
+    var ref = null;
+    if (e.keyCode === 37) {
+      ref = 'left';
+    } else if (e.keyCode === 38) {
+      ref = 'up';
+    } else if (e.keyCode === 39) {
+      ref = 'right';
+    } else if (e.keyCode === 40) {
+      ref = 'down';
+    }
+    this.updateRef(ref);
+  }
+
+  bootstrap() {
     super.bootstrap();
 
     this.DOM = ReactDOM.findDOMNode(this);
 
-    mousedown = e => {
-      this.updateRef(e.target.getAttribute('data-ref'));
-    };
+    this.DOM.addEventListener('mousedown', this.mousedown);
+    this.DOM.addEventListener('clear', this.clear);
+    this.DOM.addEventListener('mouseout', this.clear);
 
-    mouseup = () => {
-      this.updateRef(null);
-    };
 
-    this.DOM.addEventListener('mousedown', mousedown);
-    this.DOM.addEventListener('mouseup', mouseup);
-    this.DOM.addEventListener('mouseout', mouseup);
-
-    keydown = (e) => {
-      var ref = null;
-      if (e.keyCode === 37) {
-        ref = 'left';
-      } else if (e.keyCode === 38) {
-        ref = 'up';
-      } else if (e.keyCode === 39) {
-        ref = 'right';
-      } else if (e.keyCode === 40) {
-        ref = 'down';
-      }
-      this.updateRef(ref);
-    };
-
-    window.addEventListener('keydown', keydown);
-    window.addEventListener('keyup', mouseup);
+    window.addEventListener('keydown', this.keydown);
+    window.addEventListener('keyup', this.clear);
   }
 
   updateRef(ref) {
