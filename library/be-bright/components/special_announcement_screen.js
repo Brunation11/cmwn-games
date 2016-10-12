50ed7c688cfca4df581a8d194362e74782e40fc8
Adam Walzer <adamwalzer@gmail.com>
2016-10-12 09:49:33 -0400

    adding special announcement screen

---
+++ library/be-bright/components/special_announcement_screen.js
@@ -0,0 +1,21 @@
+export default function (props, ref, key) {
+  return (
+    <skoash.Screen
+      {...props}
+      ref={ref}
+      key={key}
+      id="special-announcement-screen"
+    >
+      <skoash.Image className="hidden" src="media/_images/frame.specialannouncement.png" />
+      <skoash.Component className="frame">
+        <skoash.Image className="eco" src="media/_images/mr.eco.png" />
+        <div className="copy">
+          <div>Special</div>
+          <div>Announcement!</div>
+          <div>Calling all</div>
+          <div>Eco-Heroes!!</div>
+        </div>
+      </skoash.Component>
+    </skoash.Screen>
+  );
+}
--- library/be-bright/index.js
+++ library/be-bright/index.js
@@ -3,13 +3,14 @@ import config from './config.game';
 import Loader from 'shared/components/loader/0.1';
 
 import iOSScreen from 'shared/components/ios_splash_screen/0.1';
-import TitleScreen from './components/title_screen';
-import BulbsScreen from './components/bulbs_screen';
-import PigScreen from './components/pig_screen';
-import SwitchesScreen from './components/switches_screen';
-import InfoScreen from './components/info_screen';
-import VideoScreen from './components/video_screen';
-import FlipScreen from './components/flip_screen';
+// import TitleScreen from './components/title_screen';
+// import BulbsScreen from './components/bulbs_screen';
+// import PigScreen from './components/pig_screen';
+// import SwitchesScreen from './components/switches_screen';
+// import InfoScreen from './components/info_screen';
+// import VideoScreen from './components/video_screen';
+import SpecialAnnouncementScreen from './components/special_announcement_screen';
+// import FlipScreen from './components/flip_screen';
 
 import QuitScreen from 'shared/components/quit_screen/0.1';
 
@@ -18,13 +19,14 @@ var BeBright = (
     config={config}
     screens={{
       0: iOSScreen,
-      1: TitleScreen,
-      2: BulbsScreen,
-      3: PigScreen,
-      4: SwitchesScreen,
-      5: InfoScreen,
-      6: VideoScreen,
-      7: FlipScreen,
+      1: SpecialAnnouncementScreen,
+      // 1: TitleScreen,
+      // 2: BulbsScreen,
+      // 3: PigScreen,
+      // 4: SwitchesScreen,
+      // 5: InfoScreen,
+      // 6: VideoScreen,
+      // 7: FlipScreen,
     }}
     menus={{
       quit: QuitScreen,
