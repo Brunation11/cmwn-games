02437391e9047c6ba1d96544f367d4a6c5adfcf7
bunation11 <bzatta@gmail.com>
2016-10-13 12:20:26 -0400

    refactor function to properly collect data per screens

--- library/all-about-you/components/load_data.js
+++ library/all-about-you/components/load_data.js
@@ -1,6 +1,7 @@
 export default function () {
   var loadData = {};
   if (!this.refs || !this.metaData) return;
+
   if (this.refs.selectable) {
     _.forEach(this.metaData, (ref) => {
       loadData[ref] = this.refs.selectable.props.selectClass || this.refs.selectable.state.selectClass;
