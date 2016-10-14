f0eff110bb88f42960901170d5c4c58587e3a70c
Adam Walzer <adamwalzer@gmail.com>
2016-10-14 18:29:10 -0400

    putting back screens

--- library/be-bright/index.js
+++ library/be-bright/index.js
@@ -3,13 +3,13 @@ import config from './config.game';
 import Loader from 'shared/components/loader/0.1';
 
 import iOSScreen from 'shared/components/ios_splash_screen/0.1';
-// import TitleScreen from './components/title_screen';
-// import BulbsScreen from './components/bulbs_screen';
-// import PigScreen from './components/pig_screen';
-// import SwitchesScreen from './components/switches_screen';
-// import InfoScreen from './components/info_screen';
-// import VideoScreen from './components/video_screen';
-// import SpecialAnnouncementScreen from './components/special_announcement_screen';
+import TitleScreen from './components/title_screen';
+import BulbsScreen from './components/bulbs_screen';
+import PigScreen from './components/pig_screen';
+import SwitchesScreen from './components/switches_screen';
+import InfoScreen from './components/info_screen';
+import VideoScreen from './components/video_screen';
+import SpecialAnnouncementScreen from './components/special_announcement_screen';
 import LevelOneScreen from './components/level_one_screen';
 import LevelTwoScreen from './components/level_two_screen';
 import LevelThreeScreen from './components/level_three_screen';
@@ -22,17 +22,17 @@ var BeBright = (
     config={config}
     screens={{
       0: iOSScreen,
-      // 1: SpecialAnnouncementScreen,
-      // 1: TitleScreen,
-      // 2: BulbsScreen,
-      // 3: PigScreen,
-      // 4: SwitchesScreen,
-      // 5: InfoScreen,
-      // 6: VideoScreen,
-      1: LevelOneScreen,
-      2: LevelTwoScreen,
-      3: LevelThreeScreen,
-      4: FlipScreen,
+      1: TitleScreen,
+      2: BulbsScreen,
+      3: PigScreen,
+      4: SwitchesScreen,
+      5: InfoScreen,
+      6: VideoScreen,
+      7: SpecialAnnouncementScreen,
+      8: LevelOneScreen,
+      9: LevelTwoScreen,
+      10: LevelThreeScreen,
+      11: FlipScreen,
     }}
     menus={{
       quit: QuitScreen,
@@ -41,7 +41,7 @@ var BeBright = (
     assets={[
       <skoash.Image className="background bkg-1" src="media/_BKG/BKG_1.png" />,
       <skoash.Image className="background bkg-2" src="media/_images/bkg02.png" />,
-      // <skoash.Audio ref="bkg-1" type="background" src="media/_BKG/S_BKG_1.mp3" loop />,
+      <skoash.Audio ref="bkg-1" type="background" src="media/_BKG/S_BKG_1.mp3" loop />,
       <skoash.Audio ref="button" type="sfx" src="media/_Buttons/S_BU_1.mp3" />,
       <skoash.Audio ref="screen-complete" type="sfx" src="media/_Buttons/S_BU_2.mp3" />,
       <skoash.Audio ref="correct" type="sfx" src="media/_Buttons/S_BU_3.mp3" />,
