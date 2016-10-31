f1a66a4fba78aea49f6fff588fc65da8932f663e
Adam Walzer <adamwalzer@gmail.com>
2016-10-31 11:02:56 -0400

    jobs reveal

--- library/3d-world/components/help_the_world_screen.js
+++ library/3d-world/components/help_the_world_screen.js
@@ -3,6 +3,26 @@ import Reveal from 'shared/components/reveal/0.1';
 import MediaCollection from 'shared/components/media_collection/0.1';
 
 export default function (props, ref, key) {
+  var nextPhoto;
+
+  const jobs = [
+    'designer',
+    'architect',
+    'surgeon',
+    'engineer',
+    'dentist',
+    'artist',
+  ];
+
+  nextPhoto = function () {
+    skoash.trigger('updateState', {
+      path: 'selectable',
+      data: {
+        select: jobs[(jobs.indexOf(_.get(props, 'data.selectable.target.props.data-ref')) + 1) % jobs.length]
+      }
+    });
+  };
+
   return (
     <skoash.Screen
       {...props}
@@ -27,32 +47,32 @@ export default function (props, ref, key) {
       >
         <skoash.Audio
           type="voiceOver"
-          ref="designer"
+          ref={jobs[0]}
           src={ENVIRONMENT.MEDIA + 'SoundAssets/vos/VO_Product_Designers.mp3'}
         />
         <skoash.Audio
           type="voiceOver"
-          ref="architect"
+          ref={jobs[1]}
           src={ENVIRONMENT.MEDIA + 'SoundAssets/vos/VO_Architects.mp3'}
         />
         <skoash.Audio
           type="voiceOver"
-          ref="surgeon"
+          ref={jobs[2]}
           src={ENVIRONMENT.MEDIA + 'SoundAssets/vos/VO_Surgeons.mp3'}
         />
         <skoash.Audio
           type="voiceOver"
-          ref="engineer"
+          ref={jobs[3]}
           src={ENVIRONMENT.MEDIA + 'SoundAssets/vos/VO_Engineers.mp3'}
         />
         <skoash.Audio
           type="voiceOver"
-          ref="dentist"
+          ref={jobs[4]}
           src={ENVIRONMENT.MEDIA + 'SoundAssets/vos/VO_Dentists.mp3'}
         />
         <skoash.Audio
           type="voiceOver"
-          ref="artist"
+          ref={jobs[5]}
           src={ENVIRONMENT.MEDIA + 'SoundAssets/vos/VO_Artists.mp3'}
         />
       </MediaCollection>
@@ -64,30 +84,31 @@ export default function (props, ref, key) {
       <Selectable
         dataTarget="selectable"
         selectClass="HIGHLIGHTED"
+        select={_.get(props, 'data.selectable.select')}
         list={[
           <skoash.Component
             type="li"
-            data-ref="designer"
+            data-ref={jobs[0]}
           />,
           <skoash.Component
             type="li"
-            data-ref="architect"
+            data-ref={jobs[1]}
           />,
           <skoash.Component
             type="li"
-            data-ref="surgeon"
+            data-ref={jobs[2]}
           />,
           <skoash.Component
             type="li"
-            data-ref="engineer"
+            data-ref={jobs[3]}
           />,
           <skoash.Component
             type="li"
-            data-ref="dentist"
+            data-ref={jobs[4]}
           />,
           <skoash.Component
             type="li"
-            data-ref="artist"
+            data-ref={jobs[5]}
           />,
         ]}
       />
@@ -97,7 +118,7 @@ export default function (props, ref, key) {
         list={[
           <skoash.Component
             type="li"
-            data-ref="designer"
+            data-ref={jobs[0]}
           >
             <h3>
               Product designers
@@ -105,10 +126,11 @@ export default function (props, ref, key) {
             <div>
               design and print useful objects for others!
             </div>
+            <button className="next-photo" onClick={nextPhoto} />
           </skoash.Component>,
           <skoash.Component
             type="li"
-            data-ref="architect"
+            data-ref={jobs[1]}
           >
             <h3>
               Architects
@@ -116,10 +138,11 @@ export default function (props, ref, key) {
             <div>
               create plans for housing that will be 3D printed!
             </div>
+            <button className="next-photo" onClick={nextPhoto} />
           </skoash.Component>,
           <skoash.Component
             type="li"
-            data-ref="surgeon"
+            data-ref={jobs[2]}
           >
             <h3>
               Surgeon
@@ -127,10 +150,11 @@ export default function (props, ref, key) {
             <div>
               turn x-rays into 3D models and repair injured body parts!
             </div>
+            <button className="next-photo" onClick={nextPhoto} />
           </skoash.Component>,
           <skoash.Component
             type="li"
-            data-ref="engineer"
+            data-ref={jobs[3]}
           >
             <h3>
               Engineers
@@ -139,10 +163,11 @@ export default function (props, ref, key) {
               make 3D models of you creations,<br/>
               and then print the real thing!
             </div>
+            <button className="next-photo" onClick={nextPhoto} />
           </skoash.Component>,
           <skoash.Component
             type="li"
-            data-ref="dentist"
+            data-ref={jobs[4]}
           >
             <h3>
               Dentists
@@ -150,10 +175,11 @@ export default function (props, ref, key) {
             <div>
               print replacement teeth for your patients!
             </div>
+            <button className="next-photo" onClick={nextPhoto} />
           </skoash.Component>,
           <skoash.Component
             type="li"
-            data-ref="artist"
+            data-ref={jobs[5]}
           >
             <h3>
               Artists
@@ -161,6 +187,7 @@ export default function (props, ref, key) {
             <div>
               express themselves through the magic of 3D printing!
             </div>
+            <button className="next-photo" onClick={nextPhoto} />
           </skoash.Component>,
         ]}
       />
