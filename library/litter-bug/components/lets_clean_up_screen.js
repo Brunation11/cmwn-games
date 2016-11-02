90507938ce0f92c995682433759e6092fe880b92
Max Rafferty <maxprafferty@gmail.com>
2016-11-02 16:53:47 -0400

    updated 'no' screens to be initially invisible

--- library/litter-bug/components/lets_clean_up_screen.js
+++ library/litter-bug/components/lets_clean_up_screen.js
@@ -7,9 +7,9 @@ export default function (props, ref, key) {
       id="lets-clean-up"
     >
       <skoash.MediaSequence ref="media-sequence">
-        <skoash.Audio ref="vo-2" type="voiceOver" src="media/_assets/_sounds/_vos/LitterIsTrash.mp3" />
-        <skoash.Audio ref="vo-1" type="voiceOver" src="media/_assets/_sounds/_vos/PaperCansBottles.mp3" />
-        <skoash.Audio ref="vo-2" type="voiceOver" src="media/_assets/_sounds/_vos/LetsCleanUp.mp3" />
+        <skoash.Audio ref="vo-1" type="voiceOver" src="media/_assets/_sounds/_vos/LitterIsTrash.mp3" />
+        <skoash.Audio ref="vo-2" type="voiceOver" src="media/_assets/_sounds/_vos/PaperCansBottles.mp3" />
+        <skoash.Audio ref="vo-3" type="voiceOver" src="media/_assets/_sounds/_vos/LetsCleanUp.mp3" />
       </skoash.MediaSequence>
       <div className="copy animated">
           Litter is trash<br />Paper, cans, and bottles on the ground<br/>make a mess and can hurt wildlife<br/>and the environment.
