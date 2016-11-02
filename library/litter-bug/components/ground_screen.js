dbaa42b1e5a95ab3aa7b8a285ecebb5fe998f7bb
bunation11 <bzatta@gmail.com>
2016-11-02 14:53:40 -0400

    refactor and swap assets

--- library/litter-bug/components/ground_screen.js
+++ library/litter-bug/components/ground_screen.js
@@ -5,11 +5,12 @@ export default function (props, ref, key) {
       ref={ref}
       key={key}
       id="ground"
+      startDelay={2000}
     >
-      <skoash.Audio ref="vo" type="voiceOver" src="media/S_6/VO_6.1.mp3" />
-      <skoash.Audio ref="button" complete type="sfx" src="media/S_4/VO_4.2.mp3" />
-      <skoash.Image ref="image-1" class="animated" src="media/S_6/img_6.1.png" />
-      <skoash.Image ref="image-2" src="media/S_6/img_6.2.png" />
+      <skoash.Audio ref="vo" type="voiceOver" src="media/_assets/_sounds/_vos/ShouldYouThrow.mp3" />
+      <skoash.Audio ref="button" complete type="sfx" src="media/_assets/_sounds/_effects/VO_4.2.mp3" />
+      <div className="banner animated" />
+      <div className="avatar animated" />
     </skoash.Screen>
   );
 }
