537796ba3fa7e18301c2285c7309f843aa05df74
Max Rafferty <maxprafferty@gmail.com>
2016-11-02 19:25:51 -0400

    converted thank you screen

--- library/litter-bug/components/good_for_you_screen.js
+++ library/litter-bug/components/good_for_you_screen.js
@@ -1,3 +1,4 @@
+import Repeater from 'shared/components/repeater/0.1';
 export default function (props, ref, key) {
   return (
     <skoash.Screen
@@ -6,17 +7,13 @@ export default function (props, ref, key) {
       key={key}
       id="good-for-you"
     >
-      <skoash.Audio ref="vo" type="voiceOver" src="media/S_11/VO_11.1.mp3" />
-      <skoash.Component ref="frame" className="frame">
-        <skoash.Image className="background" src="media/_Frames/FR_1.png" />
-        <skoash.Component ref="content-group" className="content-group">
-          <skoash.Image ref="image-1" className="animated" src="media/S_11/img_11.1.png" />
-          <skoash.Image ref="image-2" className="animated" src="media/S_11/img_11.2.png" />
-          <p className="animated">
-            You learned about littering<br/>
-            and showed leadership!
-          </p>
-        </skoash.Component>
+      <skoash.Audio ref="vo" type="voiceOver" src="media/_assets/_sounds/_vos/ThankYou.mp3" />
+      <div className="mr-eco animated" />
+      <div className="banner animated" />
+      <div className="banner2 animated" />
+
+      <skoash.Component className="sparkles">
+        <Repeater className="stars" count={70} ><div></div></Repeater>
       </skoash.Component>
     </skoash.Screen>
   );
