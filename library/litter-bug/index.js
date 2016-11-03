0e8baad39d56e49076c13ebeb5ba6d812048fbfb
Adam Walzer <adamwalzer@gmail.com>
2016-11-03 16:03:56 -0400

    adding back in other screens

--- library/litter-bug/index.js
+++ library/litter-bug/index.js
@@ -9,16 +9,16 @@ import TitleScreen from './components/title_screen';
 import CatchGameLevelOne from './components/catch_game_level_one_screen';
 import CatchGameLevelTwo from './components/catch_game_level_two_screen';
 import CatchGameLevelThree from './components/catch_game_level_three_screen';
-// import LetsCleanUpScreen from './components/lets_clean_up_screen';
-// import CleanUpScreen from './components/clean_up_screen';
-// import RoomScreen from './components/room_screen';
-// import SchoolScreen from './components/school_screen';
-// import GroundScreen from './components/ground_screen';
-// import SingAboutItScreen from './components/sing_about_it_screen';
-// import VideoScreen from './components/video_screen';
-// import GoodForYouScreen from './components/good_for_you_screen';
-// import TakePledgeScreen from './components/take_pledge_screen';
-// import CommitScreen from './components/commit_screen';
+import LetsCleanUpScreen from './components/lets_clean_up_screen';
+import CleanUpScreen from './components/clean_up_screen';
+import RoomScreen from './components/room_screen';
+import SchoolScreen from './components/school_screen';
+import GroundScreen from './components/ground_screen';
+import SingAboutItScreen from './components/sing_about_it_screen';
+import VideoScreen from './components/video_screen';
+import GoodForYouScreen from './components/good_for_you_screen';
+import TakePledgeScreen from './components/take_pledge_screen';
+import CommitScreen from './components/commit_screen';
 import FlipScreen from './components/flip_screen';
 
 import QuitScreen from 'shared/components/quit_screen/0.1';
@@ -32,17 +32,17 @@ var LitterBug = (
       2: CatchGameLevelOne,
       3: CatchGameLevelTwo,
       4: CatchGameLevelThree,
-      // 2: LetsCleanUpScreen,
-      // 3: CleanUpScreen,
-      // 4: RoomScreen,
-      // 5: SchoolScreen,
-      // 6: GroundScreen,
-      // 7: SingAboutItScreen,
-      // 8: VideoScreen,
-      // 9: GoodForYouScreen,
-      // 10: TakePledgeScreen,
-      // 11: CommitScreen,
-      5: FlipScreen,
+      5: LetsCleanUpScreen,
+      6: CleanUpScreen,
+      7: RoomScreen,
+      8: SchoolScreen,
+      9: GroundScreen,
+      10: SingAboutItScreen,
+      11: VideoScreen,
+      12: GoodForYouScreen,
+      13: TakePledgeScreen,
+      14: CommitScreen,
+      15: FlipScreen,
     }}
     menus={{
       quit: QuitScreen,
