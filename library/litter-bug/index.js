72c4e923bb2349bd2a7d6100fc92bb6f161ae13b
bunation11 <bzatta@gmail.com>
2016-11-03 16:24:22 -0400

    various litterbug updates

--- library/litter-bug/index.js
+++ library/litter-bug/index.js
@@ -7,6 +7,8 @@ import Loader from 'shared/components/loader/0.1';
 import iOSScreen from 'shared/components/ios_splash_screen/0.1';
 import TitleScreen from './components/title_screen';
 import LetsCleanUpScreen from './components/lets_clean_up_screen';
+import CleanUpGameLvl1Screen from './components/clean_up_game_lvl1_screen';
+import CleanUpGameLvl2Screen from './components/clean_up_game_lvl2_screen';
 import CleanUpScreen from './components/clean_up_screen';
 import RoomScreen from './components/room_screen';
 import SchoolScreen from './components/school_screen';
@@ -33,10 +35,12 @@ var LitterBug = (
       6: GroundScreen,
       7: SingAboutItScreen,
       8: VideoScreen,
-      9: GoodForYouScreen,
-      10: TakePledgeScreen,
-      11: CommitScreen,
-      12: FlipScreen,
+      9: CleanUpGameLvl1Screen,
+      10: CleanUpGameLvl1Screen,
+      11: GoodForYouScreen,
+      12: TakePledgeScreen,
+      13: CommitScreen,
+      14: FlipScreen,
     }}
     menus={{
       quit: QuitScreen,
@@ -45,14 +49,10 @@ var LitterBug = (
     assets={[
       <skoash.Audio ref="bkg-1" type="background" src="media/_BKG/S_BKG_1.mp3" loop/>,
       <skoash.Audio ref="button" type="sfx" src="media/_Buttons/S_BU_1.mp3" />,
-      // <skoash.Image ref="img-bkg-1" className="hidden" src="media/_BKG/BKG_1.png" />,
-      // <skoash.Image ref="img-bkg-2" className="hidden" src="media/_BKG/BKG_2.png" />,
-      // <skoash.Image ref="img-bkg-3" className="hidden" src="media/_BKG/BKG_3.png" />,
-      // <skoash.Image ref="img-bkg-4" className="hidden" src="media/_assets/_images/BKG.4.jpg" />,
-      // <skoash.Image ref="img-bkg-5" className="hidden" src="media/_BKG/BKG_5.png" />,
       <div className="background default" />,
       <div className="background title" />,
       <div className="background lets-clean-up" />,
+      <div className="background clean-up-game" />,
       <div className="background select" />,
       <div className="background sun" />,
       <div className="background commit" />,
---
+++ library/shared/components/cannon/0.2.js
@@ -0,0 +1,142 @@
+import classNames from 'classnames';
+
+const BALL = 'ball';
+
+class Cannon extends skoash.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      classes: {}
+    }
+
+    this.fire = this.fire.bind(this);
+    this.reload = this.reload.bind(this);
+  }
+
+  bootstrap() {
+    super.bootstrap();
+
+    var list = this.refs.bin ? this.refs.bin.get(this.props.showNum + 1) : this.props.list;
+
+    this.setState({
+      list
+    });
+  }
+
+  start() {
+    super.start();
+    this.next();
+  }
+
+  next() {
+    var classes, list;
+    classes = this.state.classes;
+    list = this.state.list;
+    if (this.props.reverseReload) {
+      list = this.refs.bin.get(1).concat(list);
+      list.pop();
+    } else {
+      list = list.concat(this.refs.bin.get(1));
+      list.shift();
+    }
+    classes[this.state.list.length - 1] = 'LOADED';
+    this.enabled = true;
+
+    this.setState({
+      classes,
+      list
+    });
+  }
+
+  fire() {
+    this.setState({
+      fire: true,
+      reload: false
+    }, () => {
+      setTimeout(() => {
+        this.reload();
+      }, 500);
+    });
+
+    this.next();
+  }
+
+  reload() {
+    this.setState({
+      fire: false,
+      reload: true
+    });
+
+    if (typeof this.props.onReload === 'function') {
+      this.props.onReload.call(this);
+    }
+  }
+
+  getClassNames() {
+    return classNames('cannon', {
+      FIRE: this.state.fire,
+      RELOAD: this.state.reload
+    }, super.getClassNames());
+  }
+
+  getClass(key, li) {
+    return classNames(
+      'ammo',
+      li.props.className,
+      this.state.classes[key]
+    );
+  }
+
+  renderBin() {
+    if (!this.props.bin) return null;
+
+    return (
+      <this.props.bin.type
+        {...this.props.bin.props}
+        ref="bin"
+      />
+    );
+  }
+
+  renderAmmo() {
+    var list = this.state.list || this.props.list;
+    return list.map((li, key) => {
+      var ref;
+      ref = li.ref || li.props['data-ref'] || key;
+      return (
+        <li.type
+          {...li.props}
+          ref={ref}
+          data-ref={ref}
+          key={key}
+          className={this.getClass(key, li)}
+          onClick={this.fire}
+        />
+      );
+    });
+  }
+
+  render() {
+    return (
+      <div className={this.getClassNames()}>
+        {this.renderBin()}
+        <div className="ammo-container" />
+        {this.renderAmmo()}
+      </div>
+    );
+  }
+}
+
+Cannon.defaultProps = _.defaults({
+  showNum: 3,
+  reverseReload: false,
+  list: [
+    <li></li>,
+    <li></li>,
+    <li></li>,
+    <li></li>
+  ]
+}, skoash.Component.defaultProps);
+
+export default Cannon;
