20a77874b822856d3f72455774e627a9b5958511
bunation11 <bzatta@gmail.com>
2016-11-04 17:57:01 -0400

    add game background music

--- library/litter-bug/index.js
+++ library/litter-bug/index.js
@@ -50,8 +50,19 @@ var LitterBug = (
       quit: QuitScreen,
     }}
     loader={<Loader />}
+    getBackgroundIndex{function (currentIndex) {
+      switch (currentScreenIndex) {
+        case 11:
+        case 12:
+        case 13:
+          return 1;
+        default:
+          return 0;
+      }
+    }}
     assets={[
       <skoash.Audio ref="bkg-1" type="background" src="media/_BKG/S_BKG_1.mp3" loop/>,
+      <skoash.Audio ref="bkg-2" type="background" src="media/_assets/_sounds/_effects/GameThemeMusic%202.mp3" loop/>,
       <skoash.Audio ref="button" type="sfx" src="media/_Buttons/S_BU_1.mp3" />,
       <skoash.Image ref="img-bkg-1" className="hidden" src="media/_BKG/BKG_1.png" />,
       <skoash.Image ref="img-bkg-2" className="hidden" src="media/_BKG/BKG_2.png" />,
