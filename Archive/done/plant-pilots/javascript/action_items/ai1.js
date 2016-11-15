62fdc9cb3d2cdf6ee3056a76088e34c53f9d5c4b
Adam Walzer <adamwalzer@gmail.com>
2016-11-15 18:17:35 -0500

    removing archive

--- Archive/done/plant-pilots/javascript/action_items/ai1.js
+++
@@ -1,2 +0,0 @@
-background.load();
-background.play();
--- Archive/done/twofer/javascript/action_items/ai3.js
+++
@@ -1,7 +0,0 @@
-$('#next-button').click(function () {
-    $("#putcontenthere").load("action_items/ai4.html");
-});
-
-$('.horse-clues').hide().fadeIn();
-
-$('#next-button').hide().delay(6000).fadeIn();
