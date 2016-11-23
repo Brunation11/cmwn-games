08915ede803dcc76a52b22356f4ac058963dda15
Adam Walzer <adamwalzer@gmail.com>
2016-11-23 12:10:32 -0500

    linting base files

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,32 +1,32 @@
 var path = require('path');
 
 module.exports = {
-  context: __dirname + '/library',
-  entry: null,
-  devtool: 'cheap-module-inline-source-map',
-  resolve: {
-    root: [path.resolve(__dirname, 'library'), path.resolve(__dirname, 'node_modules')],
-    extensions: ['', '.js'],
-    modulesDirectories: ['node_modules']
-  },
-  plugins: [],
-  output: {
-    path: __dirname + '/build',
-    filename: '[name]/ai.js',
-    publicPath: '/build/'
-  },
-  module: {
-    loaders: [{
-      test: /\.js$/,
-      loader: ['babel'],
-      exclude: [/bower_components/, /node_modules/],
-      query: {
-        presets: ['react', 'es2015', 'stage-0']
-      }
-    }]
-  },
-  postcss: function () {
-    return [autoprefixer];
-  },
-  cache: false
+    context: __dirname + '/library',
+    entry: null,
+    devtool: 'cheap-module-inline-source-map',
+    resolve: {
+        root: [path.resolve(__dirname, 'library'), path.resolve(__dirname, 'node_modules')],
+        extensions: ['', '.js'],
+        modulesDirectories: ['node_modules']
+    },
+    plugins: [],
+    output: {
+        path: __dirname + '/build',
+        filename: '[name]/ai.js',
+        publicPath: '/build/'
+    },
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            loader: ['babel'],
+            exclude: [/bower_components/, /node_modules/],
+            query: {
+                presets: ['react', 'es2015', 'stage-0']
+            }
+        }]
+    },
+    postcss: function () {
+        return [autoprefixer];
+    },
+    cache: false
 };
