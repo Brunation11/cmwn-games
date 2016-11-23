36af93541538b4cff351be5eaeceba35afa349b7
Adam Walzer <adamwalzer@gmail.com>
2016-11-23 16:40:24 -0500

    fire linting

--- library/fire/components/choose_screen.js
+++ library/fire/components/choose_screen.js
@@ -25,8 +25,9 @@ class ChooseScreenComponent extends skoash.Screen {
     }
 
     bootstrap() {
+        var ref;
+        var self = this;
         super.bootstrap();
-        var ref, self = this;
 
         ['center-1', 'center-2'].forEach(val => {
             ref = self.refs[val];
@@ -85,40 +86,40 @@ class ChooseScreenComponent extends skoash.Screen {
 
     renderFemaleMale() {
         return (
-      <skoash.Component ref="center-1" className="center female-male">
-        <skoash.Component className="group">
-          <Selectable
-            ref="selectable"
-            list={[
-                <skoash.ListItem id="gender-female" className="animated"
-                onClick={this.nextSlide.bind(this, 'female')} />,
-                <skoash.ListItem id="gender-male" className="animated"
-                onClick={this.nextSlide.bind(this, 'male')} />,
-            ]}
-          />
-        </skoash.Component>
-      </skoash.Component>
-    );
+            <skoash.Component ref="center-1" className="center female-male">
+                <skoash.Component className="group">
+                    <Selectable
+                        ref="selectable"
+                        list={[
+                            <skoash.ListItem id="gender-female" className="animated"
+                                onClick={this.nextSlide.bind(this, 'female')} />,
+                            <skoash.ListItem id="gender-male" className="animated"
+                                onClick={this.nextSlide.bind(this, 'male')} />,
+                        ]}
+                    />
+                </skoash.Component>
+            </skoash.Component>
+        );
     }
 
     renderSkinColor() {
         return (
-      <skoash.Component ref="center-2" className="center skin-color">
-        <skoash.Component className="group">
-          <Selectable
-            ref="selectable-2"
-            list={[
-                <skoash.ListItem id="skin-light" className="animated"
-                onClick={this.next.bind(this, 'light')} />,
-                <skoash.ListItem id="skin-medium" className="animated"
-                onClick={this.next.bind(this, 'medium')} />,
-                <skoash.ListItem id="skin-dark" className="animated"
-                onClick={this.next.bind(this, 'dark')} />,
-            ]}
-          />
-        </skoash.Component>
-      </skoash.Component>
-    );
+            <skoash.Component ref="center-2" className="center skin-color">
+                <skoash.Component className="group">
+                    <Selectable
+                        ref="selectable-2"
+                        list={[
+                            <skoash.ListItem id="skin-light" className="animated"
+                            onClick={this.next.bind(this, 'light')} />,
+                            <skoash.ListItem id="skin-medium" className="animated"
+                            onClick={this.next.bind(this, 'medium')} />,
+                            <skoash.ListItem id="skin-dark" className="animated"
+                            onClick={this.next.bind(this, 'dark')} />,
+                        ]}
+                    />
+                </skoash.Component>
+            </skoash.Component>
+        );
     }
 
     getClassNames() {
@@ -136,28 +137,28 @@ class ChooseScreenComponent extends skoash.Screen {
 
     renderContent() {
         return (
-      <div>
-        {this.renderContentList()}
-        {this.renderFemaleMale()}
-        {this.renderSkinColor()}
-      </div>
-    );
+            <div>
+                {this.renderContentList()}
+                {this.renderFemaleMale()}
+                {this.renderSkinColor()}
+            </div>
+        );
     }
 }
 
 export default function (props, ref, key) {
     return (
-    <ChooseScreenComponent
-      {...props}
-      ref={ref}
-      key={key}
-      id="choose"
-      hideNext
-      silentComplete
-    >
-      <skoash.Audio ref="vo" type="voiceOver" src="media/S_11/vo_ChooseYourFirefighter.mp3" />
-      <skoash.Image className="animated" src="media/S_11/img_11.1.png" />
-    </ChooseScreenComponent>
-  );
+        <ChooseScreenComponent
+            {...props}
+            ref={ref}
+            key={key}
+            id="choose"
+            hideNext
+            silentComplete
+        >
+            <skoash.Audio ref="vo" type="voiceOver" src="media/S_11/vo_ChooseYourFirefighter.mp3" />
+            <skoash.Image className="animated" src="media/S_11/img_11.1.png" />
+        </ChooseScreenComponent>
+    );
 }
 
