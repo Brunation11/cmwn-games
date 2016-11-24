bc84fe5f34357e6f06b8136feed50f4f4ec35ae5
Adam Walzer <adamwalzer@gmail.com>
2016-11-24 13:00:08 -0500

    HFF linting

--- library/happy-fish-face/components/clean_water_screen.js
+++ library/happy-fish-face/components/clean_water_screen.js
@@ -1,23 +1,23 @@
 export default function (props, ref, key) {
     return (
-    <skoash.Screen
-      {...props}
-      ref={ref}
-      key={key}
-      id="clean-water"
-    >
-      <skoash.Audio type="voiceOver" src="media/_audio/_S_CleanWater/HFF_VO_CleanWater.mp3" />
-      <skoash.Component ref="center" className="center">
-        <skoash.Component ref="group" className="group">
-          <skoash.Component ref="frame" className="frame" pl-bg>
-            <p>
-              And clean water is<br /> important for<br /> humans too!
-            </p>
-            <skoash.Image src="media/_images/_S_CleanWater/img_5.1.png" />
-          </skoash.Component>
-        </skoash.Component>
-      </skoash.Component>
-    </skoash.Screen>
-  );
+        <skoash.Screen
+            {...props}
+            ref={ref}
+            key={key}
+            id="clean-water"
+        >
+            <skoash.Audio type="voiceOver" src="media/_audio/_S_CleanWater/HFF_VO_CleanWater.mp3" />
+            <skoash.Component ref="center" className="center">
+                <skoash.Component ref="group" className="group">
+                    <skoash.Component ref="frame" className="frame" pl-bg>
+                        <p>
+                            And clean water is<br /> important for<br /> humans too!
+                        </p>
+                        <skoash.Image src="media/_images/_S_CleanWater/img_5.1.png" />
+                    </skoash.Component>
+                </skoash.Component>
+            </skoash.Component>
+        </skoash.Screen>
+    );
 }
 
--- library/happy-fish-face/components/you_feel_screen.js
+++ library/happy-fish-face/components/you_feel_screen.js
@@ -2,35 +2,44 @@ import SelectableAudio from 'shared/components/selectable_audio/0.1';
 
 export default function (props, ref, key) {
     return (
-    <skoash.Screen
-      {...props}
-      ref={ref}
-      key={key}
-      id="you-feel"
-    >
-      <skoash.Audio ref="vo" type="voiceOver" src="media/_audio/_S_YouFeel/HFF_VO_YouFeel.mp3" />
-      <skoash.Component ref="center" className="center">
-        <skoash.Component ref="group" className="group">
-         <skoash.Component ref="frame" className="frame" pl-bg>
-            <skoash.Image ref="title" src="media/_images/_S_YouFeel/img_2.1.png" />
-            <SelectableAudio
-              ref="selectable-audio"
-              completeDelay={1000}
-              selectableList={[
-                  <skoash.ListItem className="animated img-0" pl-bg />,
-                  <skoash.ListItem className="animated img-1" pl-bg />,
-                  <skoash.ListItem className="animated img-2" pl-bg />
-              ]}
-              audioAssets={[
-                  <skoash.Audio type="sfx" src="media/_audio/_S_YouFeel/HFF_SX_LeftEmoji.mp3" />,
-                  <skoash.Audio type="sfx" src="media/_audio/_S_YouFeel/HFF_SX_CenterEmoji.mp3" />,
-                  <skoash.Audio type="sfx" src="media/_audio/_S_YouFeel/HFF_SX_RightEmoji.mp3" />
-              ]}
-              chooseOne
-            />
-          </skoash.Component>
-        </skoash.Component>
-      </skoash.Component>
-    </skoash.Screen>
-  );
+        <skoash.Screen
+            {...props}
+            ref={ref}
+            key={key}
+            id="you-feel"
+        >
+            <skoash.Audio ref="vo" type="voiceOver" src="media/_audio/_S_YouFeel/HFF_VO_YouFeel.mp3" />
+            <skoash.Component ref="center" className="center">
+                <skoash.Component ref="group" className="group">
+                 <skoash.Component ref="frame" className="frame" pl-bg>
+                        <skoash.Image ref="title" src="media/_images/_S_YouFeel/img_2.1.png" />
+                        <SelectableAudio
+                            ref="selectable-audio"
+                            completeDelay={1000}
+                            selectableList={[
+                                <skoash.ListItem className="animated img-0" pl-bg />,
+                                <skoash.ListItem className="animated img-1" pl-bg />,
+                                <skoash.ListItem className="animated img-2" pl-bg />
+                            ]}
+                            audioAssets={[
+                                <skoash.Audio
+                                    type="sfx"
+                                    src="media/_audio/_S_YouFeel/HFF_SX_LeftEmoji.mp3"
+                                />,
+                                <skoash.Audio
+                                    type="sfx"
+                                    src="media/_audio/_S_YouFeel/HFF_SX_CenterEmoji.mp3"
+                                />,
+                                <skoash.Audio
+                                    type="sfx"
+                                    src="media/_audio/_S_YouFeel/HFF_SX_RightEmoji.mp3"
+                                />
+                            ]}
+                            chooseOne
+                        />
+                    </skoash.Component>
+                </skoash.Component>
+            </skoash.Component>
+        </skoash.Screen>
+    );
 }
