5b2250ebc3096065533f8665fd9c0059b3f1697b
Adam Walzer <adamwalzer@gmail.com>
2016-11-25 17:33:48 -0500

    some gulp linting love

--- gulpfile.js
+++ gulpfile.js
@@ -31,7 +31,7 @@ function lsd(_path) {
 }
 
 function defineEntries(config, game) {
-  // modify some webpack config options
+    // modify some webpack config options
     var varsPath = './shared/js/' + env + '-variables.js';
 
     config = Object.create(config);
@@ -91,7 +91,7 @@ gulp.task('webpack:build', function (callback) {
         name = 'webpack:build' + (env === 'dev' ? '-dev' : '');
         config = defineEntries(webpackConfig, game);
 
-    // run webpack
+        // run webpack
         webpack(config, function (err, stats) {
             if (err) throw new gutil.PluginError(name, err);
             gutil.log(`[${name}]`, stats.toString({
@@ -108,18 +108,18 @@ gulp.task('sass', function () {
     var varsPath = './library/shared/css/' + env + '-variables.scss';
     games.forEach(function (game) {
         gulp
-      .src([
-          './library/' + game + '/**/*.scss',
-          './library/' + game + '/**/*.css'
-      ])
-      .pipe(header(fs.readFileSync(varsPath, 'utf8')))
-      .pipe(sass().on('error', sass.logError))
-      .pipe(concat('style.css'))
-      .pipe(sourcemaps.init())
-      .pipe(postcss([autoprefixer({ browsers: ['last 5 versions'] })]))
-      .pipe(sourcemaps.write())
-      .pipe(gulp.dest('./build/' + game + '/css'))
-      .pipe(livereload());
+        .src([
+            './library/' + game + '/**/*.scss',
+            './library/' + game + '/**/*.css'
+        ])
+        .pipe(header(fs.readFileSync(varsPath, 'utf8')))
+        .pipe(sass().on('error', sass.logError))
+        .pipe(concat('style.css'))
+        .pipe(sourcemaps.init())
+        .pipe(postcss([autoprefixer({ browsers: ['last 5 versions'] })]))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('./build/' + game + '/css'))
+        .pipe(livereload());
     });
 
     gulp
@@ -141,86 +141,93 @@ gulp.task('copy-index', function () {
         fs.stat(indexPath, function (err) {
             if (err == null) {
                 gulp
-          .src(indexPath)
-          // include the following code where you want the livereload script to be injected
-          /*
-            <!-- inject:livereload -->
-            <!-- endinject -->
-          */
-          .pipe(inject(gulp.src('./library/shared/livereload.js'), {
-              starttag: '<!-- inject:livereload -->',
-              transform: function (filePath, file) {
-                  if (livereload.server) return '<script>\n' + file.contents.toString('utf8') + '\n</script>';
-              }
-          }))
-          .pipe(gulp.dest('./build/' + game));
+                .src(indexPath)
+                // include the following code where you want the livereload script to be injected
+                /*
+                  <!-- inject:livereload -->
+                  <!-- endinject -->
+                 */
+                .pipe(inject(gulp.src('./library/shared/livereload.js'), {
+                    starttag: '<!-- inject:livereload -->',
+                    transform: function (filePath, file) {
+                        if (livereload.server) {
+                            return '<script>\n' + file.contents.toString('utf8') + '\n</script>';
+                        }
+                    }
+                }))
+                .pipe(gulp.dest('./build/' + game));
             } else {
                 gulp
-          .src('./library/shared/templates/index.html')
-          .pipe(inject(gulp.src(path.join('./library', game, 'config.game.js')), {
-              starttag: '<!-- inject:head -->',
-              transform: function (filePath, file) {
-                  var s;
-                  var config;
-                  var title;
-                  s = file.contents.toString('utf8');
-                  /* eslint-disable no-eval */
-                  config = eval('(' + s.substring(s.indexOf('{'), s.indexOf(';')) + ')');
-                  /* eslint-enable no-eval */
-                  title = config.title || _.startCase(config.id);
-                  return `<title>${title}</title>`;
-              }
-          }))
-          .pipe(inject(gulp.src('./library/shared/js/test-platform-integration.js'), {
-              starttag: '<!-- inject:integration -->',
-              transform: function (filePath, file) {
-                  if (!local) return '<script>\n    ' + file.contents.toString('utf8') + '  \n</script>';
-              }
-          }))
-          .pipe(inject(gulp.src(path.join('./library', game, 'config.game.js')), {
-              starttag: '<!-- inject:body -->',
-              transform: function (filePath, file) {
-                  var s;
-                  var config;
-                  var min;
-                  s = file.contents.toString('utf8');
-                  /* eslint-disable no-eval */
-                  config = eval('(' + s.substring(s.indexOf('{'), s.indexOf(';')) + ')');
-                  /* eslint-enable no-eval */
-                  min = debug ? '' : '.min';
-                  return (
-                `<div id="${config.id}"></div>\n  ` +
-                '<script type="text/javascript" ' +
-                `src="https://cdnjs.cloudflare.com/ajax/libs/react/15.0.2/react${min}.js"></script>\n  ` +
-                '<script type="text/javascript" ' +
-                `src="https://cdnjs.cloudflare.com/ajax/libs/react/15.0.2/react-dom${min}.js"></script>\n  ` +
-                '<script type="text/javascript" ' +
-                `src="../framework/skoash.${config.skoash}.js"></script>`
-              );
-              }
-          }))
-          .pipe(inject(gulp.src('./library/shared/js/livereload.js'), {
-              starttag: '<!-- inject:livereload -->',
-              transform: function (filePath, file) {
-                  if (livereload.server) {
-                      return '<script>\n    ' + file.contents.toString('utf8') + '  \n</script>';
-                  }
-              }
-          }))
-          .pipe(inject(gulp.src('./library/shared/js/google-analytics.js'), {
-              starttag: '<!-- inject:ga -->',
-              transform: function (filePath, file) {
-                  return '<script>\n    ' + file.contents.toString('utf8') + '  \n</script>';
-              }
-          }))
-          .pipe(gulp.dest('./build/' + game));
+                .src('./library/shared/templates/index.html')
+                .pipe(inject(gulp.src(path.join('./library', game, 'config.game.js')), {
+                    starttag: '<!-- inject:head -->',
+                    transform: function (filePath, file) {
+                        var s;
+                        var config;
+                        var title;
+                        s = file.contents.toString('utf8');
+                        /* eslint-disable no-eval */
+                        config = eval('(' + s.substring(s.indexOf('{'), s.indexOf(';')) + ')');
+                        /* eslint-enable no-eval */
+                        title = config.title || _.startCase(config.id);
+                        return `<title>${title}</title>`;
+                    }
+                }))
+                .pipe(inject(gulp.src('./library/shared/js/test-platform-integration.js'), {
+                    starttag: '<!-- inject:integration -->',
+                    transform: function (filePath, file) {
+                        if (!local) {
+                            return '<script>\n    ' +
+                                file.contents.toString('utf8') + '\n</script>';
+                        }
+                    }
+                }))
+                .pipe(inject(gulp.src(path.join('./library', game, 'config.game.js')), {
+                    starttag: '<!-- inject:body -->',
+                    transform: function (filePath, file) {
+                        var s;
+                        var config;
+                        var min;
+                        s = file.contents.toString('utf8');
+                        /* eslint-disable no-eval */
+                        config = eval('(' + s.substring(s.indexOf('{'), s.indexOf(';')) + ')');
+                        /* eslint-enable no-eval */
+                        min = debug ? '' : '.min';
+                        return (
+                            `<div id="${config.id}"></div>\n  ` +
+                            '<script type="text/javascript" ' +
+                            `src="https://cdnjs.cloudflare.com/ajax/libs/react/15.0.2/react${min}.js">` +
+                            `</script>\n  ` +
+                            '<script type="text/javascript" ' +
+                            `src="https://cdnjs.cloudflare.com/ajax/libs/react/15.0.2/react-dom${min}.js">` +
+                            `</script>\n  ` +
+                            '<script type="text/javascript" ' +
+                            `src="../framework/skoash.${config.skoash}.js"></script>`
+                        );
+                    }
+                }))
+                .pipe(inject(gulp.src('./library/shared/js/livereload.js'), {
+                    starttag: '<!-- inject:livereload -->',
+                    transform: function (filePath, file) {
+                        if (livereload.server) {
+                            return '<script>\n    ' + file.contents.toString('utf8') + '  \n</script>';
+                        }
+                    }
+                }))
+                .pipe(inject(gulp.src('./library/shared/js/google-analytics.js'), {
+                    starttag: '<!-- inject:ga -->',
+                    transform: function (filePath, file) {
+                        return '<script>\n    ' + file.contents.toString('utf8') + '  \n</script>';
+                    }
+                }))
+                .pipe(gulp.dest('./build/' + game));
             }
         });
 
-    // This is only needed for LL games and can be removed once we no longer need to build any LL games.
+        // This is only needed for LL games and can be removed once we no longer need to build any LL games.
         gulp
-      .src(path.join('./library', game, 'source/screens/*'))
-      .pipe(gulp.dest('./build/' + game + '/screens'));
+        .src(path.join('./library', game, 'source/screens/*'))
+        .pipe(gulp.dest('./build/' + game + '/screens'));
     });
 });
 
@@ -232,18 +239,18 @@ gulp.task('copy-framework', function () {
 
 gulp.task('copy-media', function () {
     games.forEach(function (game) {
-    // This can be removed once media for every game is transferred to the media server.
+        // This can be removed once media for every game is transferred to the media server.
         gulp
-      .src(path.join( './library', game, 'media/**/*' ))
-      .pipe( gulp.dest(path.join( './build', game, 'media' )) );
+        .src(path.join( './library', game, 'media/**/*' ))
+        .pipe( gulp.dest(path.join( './build', game, 'media' )) );
     });
 
-  // This can be removed once fonts for every game are transferred to the media server.
+    // This can be removed once fonts for every game are transferred to the media server.
     gulp
     .src(['./library/shared/fonts/*'])
     .pipe(gulp.dest('./build/shared/fonts'));
 
-  // This can be removed once shared images games are transferred to the media server.
+    // This can be removed once shared images games are transferred to the media server.
     gulp
     .src(['./library/shared/images/*'])
     .pipe(gulp.dest('./build/shared/images'));
@@ -251,19 +258,19 @@ gulp.task('copy-media', function () {
 
 gulp.task('copy-components', function () {
     games.forEach(function (game) {
-    // This is only needed for LL games and can be removed once we no longer need to build any LL games.
+        // This is only needed for LL games and can be removed once we no longer need to build any LL games.
         gulp
-      .src(path.join( './library', game, 'source/js/components/**/*.html' ))
-      .pipe( gulp.dest(path.join( './build', game, 'components' )) );
+        .src(path.join( './library', game, 'source/js/components/**/*.html' ))
+        .pipe( gulp.dest(path.join( './build', game, 'components' )) );
     });
 });
 
 gulp.task('copy-thumbs', function () {
     games.forEach(function (game) {
-    // This can be removed once thumbs for each game are moved to the media server.
+        // This can be removed once thumbs for each game are moved to the media server.
         gulp
-      .src(path.join( './library', game, 'thumb.jpg' ))
-      .pipe( gulp.dest('./build/' + game) );
+        .src(path.join( './library', game, 'thumb.jpg' ))
+        .pipe( gulp.dest('./build/' + game) );
     });
 });
 
