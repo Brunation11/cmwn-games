d3ce212654904ad2b9e4adf7699f41e1df8890ba
bunation11 <bzatta@gmail.com>
2016-11-27 21:10:04 -0700

    add index.js for game, initial setup of assets

---
+++ library/safety-first/index.js
@@ -0,0 +1,62 @@
+import config from './config.game';
+
+import Loader from 'shared/components/loader/0.1';
+
+import iOSScreen from 'shared/components/ios_splash_screen/0.1';
+import TitleScreen from './components/title_screen';
+import MeetTheTeam from './components/meet_the_team';
+// import BulbsScreen from './components/bulbs_screen';
+// import PigScreen from './components/pig_screen';
+// import InfoScreen from './components/info_screen';
+// import VideoScreen from './components/video_screen';
+// import SpecialAnnouncementScreen from './components/special_announcement_screen';
+// import LevelOneScreen from './components/level_one_screen';
+// import LevelTwoScreen from './components/level_two_screen';
+// import LevelThreeScreen from './components/level_three_screen';
+// import FlipScreen from './components/flip_screen';
+
+import QuitScreen from 'shared/components/quit_screen/0.1';
+
+window.MEDIA_SERVER = ENVIRONMENT.MEDIA;
+ENVIRONMENT.MEDIA += 'Games/SafetyFirst/';
+
+var SafetyFirst = (
+  <skoash.Game
+    config={config}
+    loader={<Loader />}
+    screens={{
+      0: iOSScreen,
+      1: TitleScreen,
+      2: MeetTheTeam
+      // 2: BulbsScreen,
+      // 3: PigScreen,
+      // 4: InfoScreen,
+      // 5: VideoScreen,
+      // 6: SpecialAnnouncementScreen,
+      // 7: LevelOneScreen,
+      // 8: LevelTwoScreen,
+      // 9: LevelThreeScreen,
+      // 10: FlipScreen,
+    }}
+    menus={{
+      quit: QuitScreen,
+    }}
+    assets={[
+      <skoash.Font name="Source Sans Pro" />,
+      <skoash.Audio type="background" src={ENVIRONMENT.MEDIA + 'SoundAssets/effects/BKG1.mp3'} loop />,
+      <skoash.Audio ref="button" type="sfx" src={ENVIRONMENT.MEDIA + 'SoundAssets/effects/ButtonClick.mp3'} />,
+      <skoash.Audio ref="back" type="sfx" src={ENVIRONMENT.MEDIA + 'SoundAssets/effects/ButtonClick.mp3'} />,
+      <skoash.Image className="hidden" src={ENVIRONMENT.MEDIA + 'ImageAssets/bkg.2.jpg'} />,
+      <skoash.Image className="hidden" src={ENVIRONMENT.MEDIA + 'ImageAssets/bkg.3.jpg'} />,
+      <skoash.Image className="hidden" src={ENVIRONMENT.MEDIA + 'ImageAssets/bkg.home.jpg'} />,
+      <skoash.Image className="hidden" src={ENVIRONMENT.MEDIA + 'ImageAssets/bkg.dooropened.jpg'} />,
+      <skoash.Image className="hidden" src={ENVIRONMENT.MEDIA + 'ImageAssets/bkg.doorclosed.jpg'} />,
+
+      //
+      // <skoash.Audio ref="next" type="sfx" src={ENVIRONMENT.MEDIA + 'SoundAssets/effects/Next.mp3'} />,
+      // <skoash.Audio ref="screen-complete" type="sfx" src={ENVIRONMENT.MEDIA + 'SoundAssets/effects/NextAppear.mp3'} />,
+    ]}
+  />
+);
+
+skoash.start(SafetyFirst);
