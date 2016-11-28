168f790ee93de630f8ae8822f490e7d30b45c412
bunation11 <bzatta@gmail.com>
2016-11-28 11:25:38 -0700

    update screen content

--- library/safety-first/components/title_screen.js
+++ library/safety-first/components/title_screen.js
@@ -1,13 +1,15 @@
 export default function (props, ref, key) {
-  return (
-    <skoash.Screen
-      {...props}
-      ref={ref}
-      key={key}
-      id="title"
-      checkComplete={false}
-      completeOnStart
-      completeDelay={3000}
-    />
-  );
+    return (
+      <skoash.Screen
+          {...props}
+          ref={ref}
+          key={key}
+          id="title"
+          hidePrev={true}
+      >
+          {
+          //<skoash.Image src={ENVIRONMENT.MEDIA + 'SpritesAnimations/title.gif'} />}
+          }
+      </skoash.Screen>
+    );
 }
