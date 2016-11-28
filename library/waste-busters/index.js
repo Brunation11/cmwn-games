d6ff2174e0d630ef71a7e45df9269a10e971d813
Adam Walzer <adamwalzer@gmail.com>
2016-11-28 15:17:28 -0500

    starting waste busters phaser game stopping due to IB issue

--- library/waste-busters/index.js
+++ library/waste-busters/index.js
@@ -3,22 +3,20 @@ import config from './config.game';
 import Loader from 'shared/components/loader/0.1';
 
 import iOSScreen from 'shared/components/ios_splash_screen/0.1';
-import TitleScreen from './components/title_screen';
+import LevelOneScreen from './components/level_one_screen';
 
 import QuitScreen from 'shared/components/quit_screen/0.1';
 
-var wasteBusters;
-
-window.MEDIA_SERVER = ENVIRONMENT.MEDIA;
+ENVIRONMENT.MEDIA_BASE = ENVIRONMENT.MEDIA;
 ENVIRONMENT.MEDIA += 'Games/WasteBusters/';
 
-wasteBusters = (
+let wasteBusters = (
     <skoash.Game
         config={config}
         loader={<Loader />}
         screens={[
             iOSScreen,
-            TitleScreen,
+            LevelOneScreen,
         ]}
         menus={{
             quit: QuitScreen,
