6c3562db1b85af687b7fee2e2ac3778aff6bfc5f
bunation11 <bzatta@gmail.com>
2016-11-28 14:14:42 -0700

    add new screen

---
+++ library/safety-first/components/info_lets_see_screen.js
@@ -0,0 +1,18 @@
+export default function (props, ref, key) {
+  return (
+    <skoash.Screen
+      {...props}
+      ref={ref}
+      key={key}
+      id="info-lets-see"
+    >
+      <skoash.Audio ref="vo" type="voiceOver" src={`${ENVIRONMENT.MEDIA}SoundAssets/vos/LetsSee.mp3`} />
+      <skoash.Component className="frame">
+        <span>
+          You should not open the door<br />of your house to just anybody.
+        </span>
+      </skoash.Component>
+      <skoash.Image src={`${ENVIRONMENT.MEDIA}ImageAssets/dog.quit.png`} />
+    </skoash.Screen>
+  );
+}
