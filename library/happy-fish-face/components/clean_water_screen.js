6a4ccc4b9da568a09c933c78faaf97620a16e65e
Annalise Mariottini <annaliseirene@msn.com>
2016-12-01 17:25:13 -0500

    clean water screen using media server

--- library/happy-fish-face/components/clean_water_screen.js
+++ library/happy-fish-face/components/clean_water_screen.js
@@ -6,14 +6,14 @@ export default function (props, ref, key) {
             key={key}
             id="clean-water"
         >
-            <skoash.Audio type="voiceOver" src="media/_audio/_S_CleanWater/HFF_VO_CleanWater.mp3" />
+            <skoash.Audio type="voiceOver" src={`${ENVIRONMENT.MEDIA_GAME}SoundAssets/vos/CleanWater.mp3`} />
             <skoash.Component ref="center" className="center">
                 <skoash.Component ref="group" className="group">
                     <skoash.Component ref="frame" className="frame" pl-bg>
                         <p>
                             And clean water is<br /> important for<br /> humans too!
                         </p>
-                        <skoash.Image src="media/_images/_S_CleanWater/img_5.1.png" />
+                        <skoash.Image src={`${ENVIRONMENT.MEDIA_GAME}ImageAssets/img_5.1.png`} />
                     </skoash.Component>
                 </skoash.Component>
             </skoash.Component>
