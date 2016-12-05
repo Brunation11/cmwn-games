ad7f32c646f0beb3e54e212d8e66f78d9f175903
bunation11 <bzatta@gmail.com>
2016-12-04 17:15:56 -0700

    minor style updates

--- library/safety-first/components/ding_dong_screen.js
+++ library/safety-first/components/ding_dong_screen.js
@@ -6,11 +6,14 @@ export default function (props, ref, key) {
             key={key}
             id="ding-dong"
         >
-            <skoash.Audio
-                ref="ding-dong"
-                type="sfx"
-                src={`${ENVIRONMENT.MEDIA}SoundAssets/effects/DoorbellLong.mp3`}
-            />
+            <skoash.MediaSequence>
+                <skoash.Audio
+                    ref="ding-dong"
+                    type="sfx"
+                    src={`${ENVIRONMENT.MEDIA}SoundAssets/effects/DoorbellLong.mp3`}
+                    loop={true}
+                />
+            </skoash.MediaSequence>
             <skoash.Image
                 className="animated infinite"
                 src={`${ENVIRONMENT.MEDIA}ImageAssets/ding.dong.png`}
