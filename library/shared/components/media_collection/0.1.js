983da25ce5a6670812149af96ac26494023ee7b5
Adam Walzer <adamwalzer@gmail.com>
2016-12-08 12:30:40 -0500

    skoash 1.1.0

--- library/shared/components/media_collection/0.1.js
+++ library/shared/components/media_collection/0.1.js
@@ -1,3 +1,8 @@
+// As of skoash 1.1.0 this component can be found at skoash.MediaCollection
+/* eslint-disable no-console */
+console.warn('As of skoash 1.1.0 this component can be found at skoash.MediaCollection');
+/* eslint-enable no-console */
+
 class MediaCollection extends skoash.Component {
     play(ref) {
         if (this.refs[ref]) {
