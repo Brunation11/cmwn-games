8d4dbcfbd52dd4dc1209b9d256b37975f271f494
Adam Walzer <adamwalzer@gmail.com>
2016-12-13 15:00:40 -0500

    gulpfile shared scss change

--- gulpfile.js
+++ gulpfile.js
@@ -126,8 +126,7 @@ gulp.task('sass', function () {
 
     gulp
     .src([
-        './library/shared/css/**/*.scss',
-        './library/shared/css/**/*.css'
+        './library/shared/css/**/*.scss'
     ])
     .pipe(header(fs.readFileSync(varsPath, 'utf8')))
     .pipe(sass().on('error', sass.logError))
