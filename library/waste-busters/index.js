72b556b9f7a4603cb71e99f53869b2d97bc665ac
Adam Walzer <adamwalzer@gmail.com>
2016-12-14 14:30:30 -0500

    everyday screen

--- library/waste-busters/index.js
+++ library/waste-busters/index.js
@@ -3,7 +3,8 @@ import config from './config';
 import Loader from 'shared/components/loader/0.1';
 
 import iOSScreen from 'shared/components/ios_splash_screen/0.1';
-import TitleScreen from './components/title_screen';
+// import TitleScreen from './components/title_screen';
+import EverydayScreen from './components/everyday_screen';
 // import LevelOneScreen from './components/level_one_screen';
 // import LevelTwoScreen from './components/level_two_screen';
 // import LevelThreeScreen from './components/level_three_screen';
@@ -18,7 +19,8 @@ skoash.start(
         loader={<Loader />}
         screens={[
             iOSScreen,
-            TitleScreen,
+            // TitleScreen,
+            EverydayScreen,
             // LevelOneScreen,
             // LevelTwoScreen,
             // LevelThreeScreen,
@@ -54,7 +56,8 @@ skoash.start(
                 type="sfx"
                 src={MEDIA.EFFECT + 'AllClick.mp3'}
             />,
-            <div className="background title" />
+            <div className="background title" />,
+            <div className="background everyday" />
         ]}
         getBackgroundIndex={(index, id) => {
             switch (id) {
