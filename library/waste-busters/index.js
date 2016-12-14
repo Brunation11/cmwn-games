1483f91ce7c08cf41e9a29e0fc2656c7147763b4
Adam Walzer <adamwalzer@gmail.com>
2016-12-14 10:47:16 -0500

    adding first bkg

--- library/waste-busters/index.js
+++ library/waste-busters/index.js
@@ -3,6 +3,7 @@ import config from './config.game';
 import Loader from 'shared/components/loader/0.1';
 
 import iOSScreen from 'shared/components/ios_splash_screen/0.1';
+import TitleScreen from './components/title_screen';
 import LevelOneScreen from './components/level_one_screen';
 import LevelTwoScreen from './components/level_two_screen';
 import LevelThreeScreen from './components/level_three_screen';
@@ -21,6 +22,7 @@ wasteBusters = (
         loader={<Loader />}
         screens={[
             iOSScreen,
+            TitleScreen,
             LevelOneScreen,
             LevelTwoScreen,
             LevelThreeScreen,
@@ -39,8 +41,18 @@ wasteBusters = (
             <skoash.Image
                 src={ENVIRONMENT.MEDIA_GAME + 'SpritesAnimations/game1.metericons.png'}
                 className="hidden"
+            />,
+            <skoash.Audio
+                type="background"
+                src={ENVIRONMENT.MEDIA_GAME + 'SoundAssets/effects/OpeningSequence.mp3'}
             />
         ]}
+        getBackgroundIndex={(index, id) => {
+            switch (id) {
+                case 'ios-splash': return;
+                case 'title': return 0;
+            }
+        }}
     />
 );
 
