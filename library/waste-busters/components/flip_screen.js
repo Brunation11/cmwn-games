4e6a5b957102163314117400fa1e608d1dd99201
Adam Walzer <adamwalzer@gmail.com>
2016-12-21 16:37:33 -0500

    flip screen

--- library/waste-busters/components/flip_screen.js
+++ library/waste-busters/components/flip_screen.js
@@ -6,7 +6,31 @@ export default function (props, ref, key) {
             key={key}
             id="flip"
         >
-
+            <skoash.Audio
+                type="voiceOver"
+                src={MEDIA.VO + 'flip.mp3'}
+            />
+            <skoash.Image
+                className="hidden"
+                src={MEDIA.IMAGE + 'frame.square.png'}
+            />
+            <skoash.Image
+                className="hidden"
+                src={MEDIA.SPRITE + 'flip.trees.png'}
+            />
+            <div className="frame square">
+                <div className="content">
+                    <skoash.Image
+                        className="title"
+                        src={MEDIA.IMAGE + 'flip.text.png'}
+                    />
+                    Thanks for taking<br/>
+                    the time to learn<br/>
+                    about how you<br/>
+                    can help the<br/>
+                    environment!
+                </div>
+            </div>
         </skoash.Screen>
     );
 }
