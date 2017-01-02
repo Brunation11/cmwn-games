ea281151e3df3e0b91e7bd1cac3752d1ff97a960
bunation11 <bzatta@gmail.com>
2017-01-02 09:25:12 -0700

    add new property for rendering video vos

--- library/shared/js/make_media_globals.js
+++ library/shared/js/make_media_globals.js
@@ -5,6 +5,7 @@
     window.MEDIA.GAME = window.MEDIA.BASE + 'Games/' + gameName + '/';
     window.MEDIA.EFFECT = window.MEDIA.GAME + 'SoundAssets/effects/';
     window.MEDIA.VO = window.MEDIA.GAME + 'SoundAssets/vos/';
+    window.MEDIA.VIDEOVOS = window.MEDIA.GAME + 'SoundAssets/videoVOS/';
     window.MEDIA.IMAGE = window.MEDIA.GAME + 'ImageAssets/';
     window.MEDIA.SPRITE = window.MEDIA.GAME + 'SpritesAnimations/';
     window.MEDIA.FRAME = window.MEDIA.GAME + 'Frames/';
