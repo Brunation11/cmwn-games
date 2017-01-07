29ee4008e4f9d1c6321360216e48269e09607a48
bunation11 <bzatta@gmail.com>
2017-01-06 21:25:08 -0700

    import new screen

--- library/drone-zone/index.js
+++ library/drone-zone/index.js
@@ -5,6 +5,7 @@ import Loader from 'shared/components/loader/0.1';
 import iOSScreen from 'shared/components/ios_splash_screen/0.1';
 
 import TitleScreen from './components/title_screen';
+import WhyWouldYouWantADronePrtOneScreen from './components/why_would_you_want_a_drone_prt_one_screen.js';
 // import InfoVideoOneScreen from './components/info_video_one_screen';
 // import LifeStagesScreen from './components/life_stages_screen';
 // import FirstStageScreen from './components/first_stage_screen';
@@ -33,6 +34,7 @@ skoash.start(
         screens={[
             iOSScreen,
             TitleScreen,
+            WhyWouldYouWantADronePrtOneScreen,
             // InfoVideoOneScreen,
             // LifeStagesScreen,
             // FirstStageScreen,
