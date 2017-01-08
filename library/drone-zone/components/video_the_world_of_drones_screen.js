130e8ddf1d6815f48e3260489102a4437b5647e0
bunation11 <bzatta@gmail.com>
2017-01-07 17:00:50 -0700

    add new screen

---
+++ library/drone-zone/components/video_the_world_of_drones_screen.js
@@ -0,0 +1,18 @@
+export default function (props, ref, key) {
+    return (
+        <skoash.Screen
+            {...props}
+            ref={ref}
+            key={key}
+            id="video-the-world-of-drones"
+        >
+            <h1 className="header">THE WORLD OF DRONES</h1>
+
+            <skoash.Component className="frame">
+                <skoash.Video
+                    // src={`${MEDIA.VIDEO}`}
+                />
+            </skoash.Component>
+        </skoash.Screen>
+    );
+}
