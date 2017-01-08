4e117b345e276c570f7c1550715d1b90e998bac9
bunation11 <bzatta@gmail.com>
2017-01-07 17:00:27 -0700

    add new screen

---
+++ library/drone-zone/components/info_so_what_does_it_do_screen.js
@@ -0,0 +1,28 @@
+export default function (props, ref, key) {
+    return (
+        <skoash.Screen
+            {...props}
+            ref={ref}
+            key={key}
+            id="info-what-does-it-do"
+        >
+            <skoash.Audio
+                type="voiceOver"
+                // src={`${MEDIA.VO}SoWhatDoes.mp3`}
+            />
+
+            <skoash.Component className="frame">
+                <h1>SO WHAT DOES IT DO?</h1>
+                <span className="copy">
+                    It is an aircraft that does not
+                    <br />
+                    container a human pilot. Instead, the
+                    <br />
+                    drone is remote controlled by a
+                    <br />
+                    human or onboard computer.
+                </span>
+            </skoash.Component>
+        </skoash.Screen>
+    );
+}
