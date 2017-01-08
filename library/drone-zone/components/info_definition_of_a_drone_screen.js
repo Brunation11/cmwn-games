f6e92d0102f67787b247642a76c83b9f784aec2c
bunation11 <bzatta@gmail.com>
2017-01-07 17:00:17 -0700

    add new screen

---
+++ library/drone-zone/components/info_definition_of_a_drone_screen.js
@@ -0,0 +1,26 @@
+export default function (props, ref, key) {
+    return (
+        <skoash.Screen
+            {...props}
+            ref={ref}
+            key={key}
+            id="info-definition-of-a-drone"
+        >
+            <skoash.Audio
+                type="voiceOver"
+                // src={`${MEDIA.VO}ADroneIs.mp3`}
+            />
+
+            <skoash.Component className="frame">
+                <h1>DEFINITION OF A DRONE</h1>
+                <span className="copy">
+                    A drone is an
+                    <br />
+                    unmanned aerial vehicle
+                    <br />
+                    (UAV).
+                </span>
+            </skoash.Component>
+        </skoash.Screen>
+    );
+}
