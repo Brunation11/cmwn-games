f35876f1b89a3a2439be93e890d6e511a70f1fad
Adam Walzer <adamwalzer@gmail.com>
2017-01-09 11:25:32 -0500

    starting green team

---
+++ library/green-team-challenge/components/flip_screen.js
@@ -0,0 +1,11 @@
+export default function (props, ref, key) {
+    return (
+        <skoash.Screen
+            {...props}
+            ref={ref}
+            key={key}
+            id="flip"
+        >
+        </skoash.Screen>
+    );
+}
---
+++ library/green-team-challenge/index.js
@@ -0,0 +1,38 @@
+import config from './config';
+
+import Loader from 'shared/components/loader/0.1';
+
+import iOSScreen from 'shared/components/ios_splash_screen/0.1';
+
+import TitleScreen from './components/title_screen';
+import FlipScreen from './components/flip_screen';
+import QuitScreen from './components/quit_screen';
+
+skoash.start(
+    <skoash.Game
+        config={config}
+        loader={<Loader />}
+        screens={[
+            iOSScreen,
+            TitleScreen,
+            FlipScreen,
+        ]}
+        menus={{
+            quit: QuitScreen,
+        }}
+        assets={[
+            <div className="background title" />,
+            <div className="background flip" />,
+        ]}
+        getBackgroundIndex={(index, id) => {
+            switch (id) {
+                case 'ios-splash': return;
+                case 'title':
+                case 'flip':
+                    return;
+            }
+        }}
+    />
+);
+
+if (module.hot) module.hot.accept();
