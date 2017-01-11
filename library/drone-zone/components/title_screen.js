84eb94af900303817e4d1df93e57ab7f19c90447
bunation11 <bzatta@gmail.com>
2017-01-10 21:00:10 -0700

    add title screen assets

--- library/drone-zone/components/title_screen.js
+++ library/drone-zone/components/title_screen.js
@@ -5,6 +5,10 @@ export default function (props, ref, key) {
             ref={ref}
             key={key}
             id="title"
-        />
+        >
+            <skoash.Image className="radar" src={`${MEDIA.IMAGE}title.radar.png`} />
+            <skoash.Image className="drone" src={`${MEDIA.IMAGE}title.drone.png`} />
+            <skoash.Image className="name" src={`${MEDIA.IMAGE}title.name.png`} />
+        </skoash.Screen>
     );
 }
