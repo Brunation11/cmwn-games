d25e92e95875475073c233f2392300962fde063a
Annalise Mariottini <annaliseirene@msn.com>
2017-01-20 12:31:37 -0500

    index + title starting move to media server

--- library/drought-out/components/title_screen.js
+++ library/drought-out/components/title_screen.js
@@ -8,8 +8,10 @@ export default function (props, ref, key) {
             checkComplete={false}
             completeDelay={3000}
             completeOnStart={true}
+            playOnStart="bird-wing-flap"
         >
-            <skoash.Image className="animated" src="media/S_1/img_1.1.png" />
+            <skoash.Audio ref="bird-wing-flap" type="sfx" delay={2000} src={`${ENVIRONMENT.MEDIA_GAME}SoundAssets/effects/BirdWingFlap.mp3`} />
+            <skoash.Image className="animated" src={`${ENVIRONMENT.MEDIA_GAME}ImageAssets/img_1.1.png`} />
         </skoash.Screen>
     );
 }
