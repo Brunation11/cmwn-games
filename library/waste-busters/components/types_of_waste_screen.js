9b0551da85a3e9aa2c78d0484831e3aa3f97135d
Adam Walzer <adamwalzer@gmail.com>
2017-02-03 14:46:30 -0500

    types of waste text CORE-3004

--- library/waste-busters/components/types_of_waste_screen.js
+++ library/waste-busters/components/types_of_waste_screen.js
@@ -46,15 +46,37 @@ export default function (props, ref, key) {
                 selectClass="HIGHLIGHTED"
                 list={[
                     <skoash.Component type="li" correct={true} message="compost">
-                        <div className="side b"><div/></div>
+                        <div className="side b">
+                            <div>
+                                Discarded organic matter<br/>
+                                such as plant scraps and<br/>
+                                egg shells can all be turned<br/>
+                                into mineral-rich soil.
+                            </div>
+                        </div>
                         <div className="side a"><div/></div>
                     </skoash.Component>,
                     <skoash.Component type="li" correct={true} message="recycle">
-                        <div className="side b"><div/></div>
-                        <div className="side a"><div/></div>
+                        <div className="side b">
+                            <div>
+                                Metal, glass, and certain<br/>
+                                types of paper and plastic<br/>
+                                can be recycled.
+                            </div>
+                        </div>
+                        <div className="side a"></div>
                     </skoash.Component>,
                     <skoash.Component type="li" correct={true} message="landfill">
-                        <div className="side b"><div/></div>
+                        <div className="side b">
+                            <div>
+                                Alas, not all waste can be<br/>
+                                recycled or composted.<br/>
+                                Soiled tissues, plastic<br/>
+                                diapers, and more are just<br/>
+                                some of the times that<br/>
+                                might end up in the landfill!
+                            </div>
+                        </div>
                         <div className="side a"><div/></div>
                     </skoash.Component>,
                 ]}
