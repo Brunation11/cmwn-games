85538e1d1ff80b8477b60992c8293b912269f0f2
Adam Walzer <adamwalzer@gmail.com>
2017-02-08 18:13:41 -0500

    merge fixes

--- library/shared/js/make_media_globals.js
+++ library/shared/js/make_media_globals.js
@@ -4,22 +4,6 @@
         BASE: ENVIRONMENT.MEDIA
     };
 
-<<<<<<< HEAD
-    MEDIA.GAME = MEDIA.BASE + 'Games/' + gameName + '/';
-    MEDIA.EFFECT = MEDIA.GAME + 'SoundAssets/effects/';
-    MEDIA.VO = MEDIA.GAME + 'SoundAssets/vos/';
-    MEDIA.IMAGE = MEDIA.GAME + 'ImageAssets/';
-    MEDIA.SPRITE = MEDIA.GAME + 'SpritesAnimations/';
-    MEDIA.FRAME = MEDIA.GAME + 'Frames/';
-
-    MEDIA.MOCK = {};
-    MEDIA.MOCK.GAME = MEDIA.BASE + 'Games/mock-game/';
-    MEDIA.MOCK.EFFECT = MEDIA.MOCK.GAME + 'SoundAssets/effects/';
-    MEDIA.MOCK.VO = MEDIA.MOCK.GAME + 'SoundAssets/vos/';
-    MEDIA.MOCK.IMAGE = MEDIA.MOCK.GAME + 'ImageAssets/';
-    MEDIA.MOCK.SPRITE = MEDIA.MOCK.GAME + 'SpritesAnimations/';
-    MEDIA.MOCK.FRAME = MEDIA.MOCK.GAME + 'Frames/';
-=======
     const GAME = 'games/';
     const EFFECT = 'sound-assets/effects/';
     const VO = 'sound-assets/vos/';
@@ -49,7 +33,6 @@
     MEDIA.MOCK.SPRITE = MEDIA.MOCK.GAME + SPRITE;
     MEDIA.MOCK.FRAME = MEDIA.MOCK.GAME + FRAME;
     MEDIA.MOCK.FONT = MEDIA.MOCK.GAME + FONT;
->>>>>>> 2288e39852236075335e8ba1f0309d8c8fa3c636
 
     window.CMWN.MEDIA = MEDIA;
 }(window.CMWN.gameFolder));
