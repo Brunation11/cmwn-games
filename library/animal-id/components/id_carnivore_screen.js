e46b2a777b99e28ad1d1d943840cd78918b13023
Adam Walzer <adamwalzer@gmail.com>
2017-02-14 14:53:26 -0500

    id screens CORE-3232

--- library/animal-id/components/id_carnivore_screen.js
+++ library/animal-id/components/id_carnivore_screen.js
@@ -1,37 +1,18 @@
-import SelectableReveal from 'shared/components/selectable_reveal/0.1';
+import IDScreenComponent from './id_screen_component';
 
 export default function (props, ref, key) {
-    return (
-        <skoash.Screen
-            {...props}
-            ref={ref}
-            key={key}
-            id="id-carnivore"
-        >
-            <skoash.Audio ref="vo" type="voiceOver" src="media/audio/VO_3-1.mp3" />
-            <skoash.Image ref="title" className="title animated" src="media/images/img_3.1.png" />
-            <SelectableReveal
-                ref="selectable-reveal"
-                answers={['lion']}
-                assets={[
-                    <skoash.Audio ref="correct" type="sfx" src="media/audio/S_3.2.mp3" />,
-                    <skoash.Audio ref="incorrect" type="sfx" src="media/audio/id-incorrect.mp3" complete />
-                ]}
-                selectableList={[
-                    <skoash.ListItem className="beaver animated" />,
-                    <skoash.ListItem className="gorilla animated" />,
-                    <skoash.ListItem data-ref="lion" className="lion animated" />,
-                    <skoash.ListItem className="giraffe animated" />
-                ]}
-                revealList={[
-                    <skoash.ListItem data-ref="lion">
-                        <h3>A carnivore is an animal <br /> that eats only meat.</h3>
-                    </skoash.ListItem>
-                ]}
-                revealAssets={[
-                    <skoash.Audio ref="lion" type="voiceOver" src="media/audio/VO_3-2.mp3" delay={2000} />
-                ]}
-            />
-        </skoash.Screen>
-    );
+    return IDScreenComponent(props, ref, key, {
+        id: 'id-carnivore',
+        vo: 'media/audio/VO_3-1.mp3',
+        title: 'media/images/img_3.1.png',
+        correct: 'media/audio/S_3.2.mp3',
+        revealVO: 'media/audio/VO_3-2.mp3',
+        revealContent: <h3>A carnivore is an animal <br /> that eats only meat.</h3>,
+        selectables: {
+            beaver: 'incorrect',
+            gorilla: 'incorrect',
+            lion: 'correct',
+            giraffe: 'incorrect',
+        }
+    });
 }
--- library/animal-id/components/id_herbivore_screen.js
+++ library/animal-id/components/id_herbivore_screen.js
@@ -1,37 +1,18 @@
-import SelectableReveal from 'shared/components/selectable_reveal/0.1';
+import IDScreenComponent from './id_screen_component';
 
 export default function (props, ref, key) {
-    return (
-        <skoash.Screen
-            {...props}
-            ref={ref}
-            key={key}
-            id="id-herbivore"
-        >
-            <skoash.Audio ref="vo" type="voiceOver" src="media/audio/VO_10-1.mp3" />
-            <skoash.Image ref="title" className="title animated" src="media/images/img_10.1.png" />
-            <SelectableReveal
-                ref="selectable-reveal"
-                answers={['deer']}
-                assets={[
-                    <skoash.Audio ref="correct" type="sfx" src="media/audio/drag-correct.mp3" />,
-                    <skoash.Audio ref="incorrect" type="sfx" src="media/audio/id-incorrect.mp3" complete />
-                ]}
-                selectableList={[
-                    <skoash.ListItem data-ref="deer" className="deer animated" />,
-                    <skoash.ListItem className="possum animated" />,
-                    <skoash.ListItem className="tiger animated" />,
-                    <skoash.ListItem className="skunk animated" />
-                ]}
-                revealList={[
-                    <skoash.ListItem data-ref="deer">
-                        <h3>An herbivore is an animal<br/>that eats only plants.</h3>
-                    </skoash.ListItem>
-                ]}
-                revealAssets={[
-                    <skoash.Audio ref="deer" type="voiceOver" src="media/audio/VO_10-2.mp3" />
-                ]}
-            />
-        </skoash.Screen>
-    );
+    return IDScreenComponent(props, ref, key, {
+        id: 'id-herbivore',
+        vo: 'media/audio/VO_10-1.mp3',
+        title: 'media/images/img_10.1.png',
+        correct: 'media/audio/drag-correct.mp3',
+        revealVO: 'media/audio/VO_10-2.mp3',
+        revealContent: <h3>An herbivore is an animal<br/>that eats only plants.</h3>,
+        selectables: {
+            deer: 'correct',
+            possum: 'incorrect',
+            tiger: 'incorrect',
+            skunk: 'incorrect',
+        }
+    });
 }
