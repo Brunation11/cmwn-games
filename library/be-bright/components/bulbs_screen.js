8c73aa486a6b1c9ed959b695312bd9f2863bde29
Adam Walzer <adamwalzer@gmail.com>
2017-02-22 12:34:07 -0500

    skoash update and bulb screen fix

--- library/be-bright/components/bulbs_screen.js
+++ library/be-bright/components/bulbs_screen.js
@@ -1,5 +1,3 @@
-import SelectableReveal from 'shared/components/selectable_reveal/0.1';
-
 export default function (props, ref, key) {
     return (
         <skoash.Screen
@@ -22,16 +20,20 @@ export default function (props, ref, key) {
                     Why does Mr. Eco want you to turn off the lights?
                 </div>
             </div>
-            <SelectableReveal
-                ref="selectable-reveal"
-                selectableList={[
-                    <li className="animated" correct={true}></li>,
-                    <li className="animated" correct={true}></li>,
-                    <li className="animated" correct={true}></li>,
-                    <li className="animated" correct={true}></li>
-                ]}
-                selectableSelectClass="HIGHLIGHTED"
-                revealList={[
+            {skoash.mixins.SelectableReveal(props, {
+                media: [
+                    <skoash.Audio type="voiceOver" ref="0" src="media/S_3/VO_3.2.mp3" delay={2000} />,
+                    <skoash.Audio type="voiceOver" ref="1" src="media/S_3/VO_3.3.mp3" delay={2000} />,
+                    <skoash.Audio type="voiceOver" ref="2" src="media/S_3/VO_3.4.mp3" delay={2000} />,
+                    <skoash.Audio type="voiceOver" ref="3" src="media/S_3/VO_3.5.mp3" delay={2000} />,
+                ],
+                selectables: [
+                    <skoash.ListItem className="animated" correct={true}/>,
+                    <skoash.ListItem className="animated" correct={true}/>,
+                    <skoash.ListItem className="animated" correct={true}/>,
+                    <skoash.ListItem className="animated" correct={true}/>,
+                ],
+                reveals: [
                     <li>
                         <p>Light bulbs burn electricity!</p>
                     </li>,
@@ -69,15 +71,16 @@ export default function (props, ref, key) {
                             <skoash.Image src="media/_images/dollar.sign.png" />
                         </div>
                     </li>
-                ]}
-                revealAssets={[
-                    <skoash.Audio type="voiceOver" src="media/S_3/VO_3.2.mp3" delay={2000} />,
-                    <skoash.Audio type="voiceOver" src="media/S_3/VO_3.3.mp3" delay={2000} />,
-                    <skoash.Audio type="voiceOver" src="media/S_3/VO_3.4.mp3" delay={2000} />,
-                    <skoash.Audio type="voiceOver" src="media/S_3/VO_3.5.mp3" delay={2000} />,
-                    <skoash.Audio data-ref="open-sound" type="sfx" src="media/_Buttons/S_BU_3.mp3" />
-                ]}
-            />
+                ],
+                SelectableProps: {
+                    selectClass: 'HIGHLIGHTED',
+                },
+                RevealProps: {
+                    assets: [
+                        <skoash.Audio data-ref="open-sound" type="sfx" src="media/_Buttons/S_BU_3.mp3" />
+                    ],
+                },
+            })}
         </skoash.Screen>
     );
 }
