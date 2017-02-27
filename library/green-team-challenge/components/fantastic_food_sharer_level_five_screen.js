22372721a778d31de4a256ce24737db00ec2c381
Adam Walzer <adamwalzer@gmail.com>
2017-02-27 12:07:40 -0500

    removing GTC from this repo

--- library/green-team-challenge/components/fantastic_food_sharer_level_five_screen.js
+++
@@ -1,9 +0,0 @@
-import DropperGameComponent from './dropper_game_component';
-import defaultOpts from './default_fantastic_food_sharer_opts';
-
-export default function (props, ref, key) {
-    return DropperGameComponent(props, ref, key, _.defaults({
-        level: 5,
-        scoreToWin: 300,
-    }, defaultOpts));
-}
--- library/green-team-challenge/components/recycling_champion_level_two_screen.js
+++
@@ -1,9 +0,0 @@
-import DropperGameComponent from './dropper_game_component';
-import defaultOpts from './default_recycling_champion_opts';
-
-export default function (props, ref, key) {
-    return DropperGameComponent(props, ref, key, _.defaults({
-        level: 2,
-        scoreToWin: 760,
-    }, defaultOpts));
-}
